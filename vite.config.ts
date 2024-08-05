f3e5e258d2a6aa6cd4b3bf0905c3ba1ac37e2ee4
adrien charrier <adrien.charrier@outlook.fr>
2024-08-05 10:26:41 +0200

    chore: Remove vite-plugin-dts from package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
 import { resolve } from 'node:path'
-import dts from 'vite-plugin-dts'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    // dts({
-    //   rollupTypes: true,
-    // }),
     vue(),
   ],
   resolve: {
