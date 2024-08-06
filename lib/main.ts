bd1af03e3d6c02b57eeb3381c7fb5941e3bf3695
virginieVachet <vachetvirginie@gmail.com>
2024-08-06 09:55:07 +0200

    first build

--- lib/main.ts
+++ lib/main.ts
@@ -1,10 +1,1 @@
-// lib/main.ts
-import { createApp } from 'vue';
-import { vuetify } from '../lib/composables/useVuetify.js';
 import './assets/main.scss';
-
-const app = createApp({});
-app.use(vuetify);
-app.mount('#app');
-
-export default app;
