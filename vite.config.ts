b274fd956f27941de1efa5b8bb9323f80859bad6
adrien charrier <adrien.charrier@outlook.fr>
2024-08-08 11:04:29 +0200

    chore: Update TypeScript configuration for declaration and source maps

--- vite.config.ts
+++ vite.config.ts
@@ -2,32 +2,37 @@ import { fileURLToPath, URL } from 'node:url'
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@tests': fileURLToPath(new URL('./tests', import.meta.url)),
-    },
-  },
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/main.ts'),
-      name: 'DesignSystemV3',
-      fileName: 'design-system-v3',
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
-        },
-      },
-      
-    },
-  },
+	plugins: [
+		dts({
+			rollupTypes: true,
+			insertTypesEntry: true,
+			tsconfigPath: './tsconfig.app.json',
+		}),
+		vue(),
+	],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+			'@tests': fileURLToPath(new URL('./tests', import.meta.url)),
+		},
+	},
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/main.ts'),
+			name: 'DesignSystemV3',
+			fileName: 'design-system-v3',
+		},
+		rollupOptions: {
+			external: ['vue'],
+			output: {
+				globals: {
+					vue: 'Vue',
+				},
+			},
+		},
+	},
 })
