d48f7e21b72201c419b7bb3744a90f553516f5a5
VachetVirginie <vachetvirginie@gmail.com>
2024-08-22 10:56:20 +0200

    Feat/customizable mixin migration (#39)
    * customizable mixins migration
    
    * refactor: Update customizableOptions component to use TypeScript and improve props definition
    
    * lockfile
    
    * lockfile
    
    ---------
    
    Co-authored-by: adrien charrier <adrien.charrier@outlook.fr>

---
+++ src/composables/useCustomizableOptions.ts
@@ -0,0 +1,23 @@
+import { computed, type ComputedRef } from 'vue'
+import deepmerge from 'deepmerge'
+
+type PropsList = Record<string, unknown>
+type ComponentsProps = Record<string, PropsList>
+export interface CustomizableOptions {
+	vuetifyOptions?: ComponentsProps
+}
+
+export default function useCustomizableOptions<T1 extends ComponentsProps, T2 extends ComponentsProps>(
+	defaultOptions: T1,
+	props: { vuetifyOptions?: T2 },
+): ComputedRef<T1 & T2>
+export default function useCustomizableOptions<T extends ComponentsProps>(
+	defaultOptions: Partial<T>,
+	props: { vuetifyOptions?: Partial<T> },
+): ComputedRef<T>
+export default function useCustomizableOptions(
+	defaultOptions: ComponentsProps,
+	props: CustomizableOptions,
+) {
+	return computed(() => deepmerge(defaultOptions, props.vuetifyOptions ?? {}))
+}
