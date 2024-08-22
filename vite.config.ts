86564ec008fb2387749264c9ca154d04392ed31d
virginieVachet <vachetvirginie@gmail.com>
2024-08-22 14:39:52 +0200

    change type any

--- vite.config.ts
+++ vite.config.ts
@@ -7,62 +7,62 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        dts({
-            rollupTypes: true,
-            insertTypesEntry: true,
-            tsconfigPath: './tsconfig.app.json',
-        }),
-        vue(),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-            '@tests': fileURLToPath(new URL('./tests', import.meta.url)),
-        },
-    },
-    build: {
-        lib: {
-            entry: resolve(__dirname, 'src/main.ts'),
-            name: 'DesignSystemV3',
-            fileName: 'design-system-v3',
-        },
-        rollupOptions: {
-            external: ['vue'],
-            output: {
-                globals: {
-                    vue: 'Vue',
-                },
-            },
-        },
-    },
-    test: {
-        environment: 'happy-dom',
-        server: {
-            deps: {
-                inline: ['vuetify'],
-            },
-        },
-        coverage: {
-            enabled: true,
-            provider: 'v8',
-            reportsDirectory: './tests/unit/coverage',
-            include: [
-                'src/**/*.{js,vue,ts}',
-            ],
-            exclude: [
-                'src/**/*.spec.{js,vue,ts}',
-                'src/**/*.stories.*',
-                'src/main.ts',
-                'src/components/index.ts',
-                ...coverageConfigDefaults.exclude,
-            ],
-            /*thresholds: {
-                branches: 80,
-                functions: 80,
-                lines: 80,
-                statements: 80,
-            },*/
-        },
-    },
+	plugins: [
+		dts({
+			rollupTypes: true,
+			insertTypesEntry: true,
+			tsconfigPath: './tsconfig.app.json',
+		}),
+		vue(),
+	],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+			'@tests': fileURLToPath(new URL('./tests', import.meta.url)),
+		},
+	},
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/main.ts'),
+			name: 'DesignSystemV3',
+			fileName: 'design-system-v3',
+		},
+		rollupOptions: {
+			external: ['vue'],
+			output: {
+				globals: {
+					vue: 'Vue',
+				},
+			},
+		},
+	},
+	test: {
+		environment: 'happy-dom',
+		server: {
+			deps: {
+				inline: ['vuetify'],
+			},
+		},
+		coverage: {
+			enabled: true,
+			provider: 'v8',
+			reportsDirectory: './tests/unit/coverage',
+			include: [
+				'src/**/*.{js,vue,ts}',
+			],
+			exclude: [
+				'src/**/*.spec.{js,vue,ts}',
+				'src/**/*.stories.*',
+				'src/main.ts',
+				'src/components/index.ts',
+				...coverageConfigDefaults.exclude,
+			],
+			/* thresholds: {
+				branches: 80,
+				functions: 80,
+				lines: 80,
+				statements: 80,
+			}, */
+		},
+	},
 })
