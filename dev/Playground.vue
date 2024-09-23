44020554e97a6679ca7fa3c7af213dfe3cd160ad
David FYON <davidfyon@gmail.com>
2024-09-23 15:34:50 +0200

    update gitignore

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,25 +1,11 @@
 <script setup lang="ts">
-import PageContainer from '@/components/PageContainer/PageContainer.vue'
+
 </script>
 
 <template>
-  <VApp>
-    <main>
-      <PageContainer size="s" color="blue">
-        <h1>Playground</h1>
-      </PageContainer>
-      <PageContainer size="m" color="red">
-        <h1>Playground</h1>
-      </PageContainer>
-      <PageContainer size="l" color="orange">
-        <h1>Playground</h1>
-      </PageContainer>
-      <PageContainer size="xl" color="pink">
-        <h1>Playground</h1>
-      </PageContainer>
-      <PageContainer color="yellow" spacing="sm">
-        <h1>Playground</h1>
-      </PageContainer>
-    </main>
-  </VApp>
+  <div>
+
+  </div>
 </template>
+
+<style lang="scss"></style>
