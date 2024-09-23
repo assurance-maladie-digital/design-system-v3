41a823101a38a2593f9e00e0bf37365d407d8258
David FYON <davidfyon@gmail.com>
2024-09-23 14:43:58 +0200

    improve stories + tests

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -49,6 +49,7 @@ const preview: Preview = {
 				color: /(background|color)$/i,
 				date: /Date$/i,
 			},
+			disableSaveFromUI: true,
 		},
 		backgrounds: {
 			default: 'main',
--- src/components/PageContainer/PageContainer.stories.ts
+++ src/components/PageContainer/PageContainer.stories.ts
@@ -50,31 +50,51 @@ export const Default: Story = {
 	},
 }
 
-export const Card: Story = {
+export const Size: Story = {
 	args: {
 		default: 'Contenu de la page',
+		size: 's',
 	},
 	render: (args) => {
 		return {
-			components: { PageContainer, VCard },
+			components: { PageContainer },
 			setup() {
 				return { args }
 			},
 			template: `
 				<PageContainer :size="args.size" :spacing="args.spacing" :color="args.color">
-					<VCard class="pa-8">
-						{{ args.default }}
-					</VCard>
+					{{ args.default }}
 				</PageContainer>
 			`,
 		}
 	},
 }
 
-export const Size: Story = {
+export const Color: Story = {
 	args: {
 		default: 'Contenu de la page',
-		size: 's',
+		color: 'primary',
+	},
+	render: (args) => {
+		return {
+			components: { PageContainer },
+			setup() {
+				return { args }
+			},
+			template: `
+				<PageContainer :size="args.size" :spacing="args.spacing" :color="args.color">
+					<div class="pa-8">
+						{{ args.default }}
+					</div>
+				</PageContainer>
+			`,
+		}
+	},
+}
+
+export const Card: Story = {
+	args: {
+		default: 'Contenu de la page',
 	},
 	render: (args) => {
 		return {
