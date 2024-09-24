6a0f85a7b417b73e31a2f01a0f5ad55a931a7b51
David FYON <davidfyon@gmail.com>
2024-09-24 16:28:44 +0200

    add component + tests

---
+++ src/components/CopyBtn/CopyBtn.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { mdiContentCopy } from '@mdi/js'
+
+import useCustomizableOptions, { type CustomizableOptions } from '@/composables/useCustomizableOptions'
+import { copyToClipboard } from '@/utils/functions/copyToClipboard'
+
+import { locales } from './locales'
+import { config } from './config'
+
+const props = withDefaults(defineProps<CustomizableOptions & {
+  label: string
+  textToCopy: (() => string) | string
+  hideTooltip?: boolean
+  tooltipDuration?: number
+}>(), {
+  hideTooltip: false,
+  tooltipDuration: 2500,
+})
+
+const options = useCustomizableOptions(config, props)
+
+const tooltip = ref(false)
+const copyIcon = mdiContentCopy
+
+function copy(): void {
+  const contentToCopy =
+      typeof props.textToCopy === 'function'
+          ? props.textToCopy()
+          : props.textToCopy
+
+  copyToClipboard(contentToCopy)
+
+  if (props.hideTooltip) {
+    return
+  }
+
+  setTimeout(() => {
+    tooltip.value = false
+  }, props.tooltipDuration)
+}
+
+defineExpose({ copy })
+</script>
+
+<template>
+  <div class="vd-copy-btn">
+    <VMenu
+        v-model="tooltip"
+        v-bind="options.menu"
+        :disabled="props.hideTooltip"
+        transition="fade-transition"
+    >
+      <template #activator="{ props: menuProps }">
+        <VBtn
+            v-bind="{ ...menuProps, ...options.btn }"
+            :aria-label="props.label"
+            data-test-id="copy-btn"
+            @click="copy"
+        >
+          <slot name="icon">
+            <VIcon v-bind="options.icon">
+              {{ copyIcon }}
+            </VIcon>
+          </slot>
+        </VBtn>
+      </template>
+
+      <slot name="tooltip">
+        {{ locales.tooltip }}
+      </slot>
+    </VMenu>
+  </div>
+</template>
+
+<style lang="scss">
+// Make the tooltip menu look like a tooltip
+.vd-copy-tooltip-menu {
+  padding: 6px 16px;
+  box-shadow: none;
+  margin-top: 2px;
+  background: rgba(97, 97, 97, 0.9);
+  color: white;
+}
+</style>
