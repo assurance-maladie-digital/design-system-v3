5f796c4fc3edbaea4d740bdcd99f4c7196b2fbff
David FYON <davidfyon@gmail.com>
2024-09-30 15:43:46 +0200

    fix index exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
-export { default as TestComponent } from './TestComponent/TestComponent.vue'
-export { default as TestDTComponent } from './TestDesignTokensComponent/TestDTComponent.vue'
+export { default as PageContainer } from './PageContainer/PageContainer.vue'
+export { default as Alert } from './Alert/Alert.vue'
+export { default as CopyBtn } from './CopyBtn/CopyBtn.vue'
 export { default as SkipLink } from './SkipLink/SkipLink.vue'
