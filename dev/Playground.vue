477f11d095b3b9c7d35c8fc5773ad3109bb5d675
David FYON <davidfyon@gmail.com>
2024-10-02 23:00:11 +0200

    add ex on FranceConectBtn (#112)

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -3,45 +3,43 @@
 </script>
 
 <template>
-	<VApp>
-		<main>
-			<div class="pa-10">
-				<v-row>
-					<v-col>
-						<FranceConnectBtn href="/" />
-					</v-col>
-					<v-col>
-						<FranceConnectBtn
-							href="/"
-							is-connect-plus
-						/>
-					</v-col>
-				</v-row>
-			</div>
+	<main>
+		<div class="pa-10">
+			<v-row>
+				<v-col>
+					<FranceConnectBtn href="/" />
+				</v-col>
+				<v-col>
+					<FranceConnectBtn
+						href="/"
+						is-connect-plus
+					/>
+				</v-col>
+			</v-row>
+		</div>
 
-			<br>
+		<br>
 
-			<v-theme-provider
-				class="pa-10"
-				theme="dark"
-				with-background
-			>
-				<v-row>
-					<v-col>
-						<FranceConnectBtn
-							href="/"
-							disabled
-						/>
-					</v-col>
-					<v-col>
-						<FranceConnectBtn
-							href="/"
-							is-connect-plus
-							disabled
-						/>
-					</v-col>
-				</v-row>
-			</v-theme-provider>
-		</main>
-	</VApp>
+		<v-theme-provider
+			class="pa-10"
+			theme="dark"
+			with-background
+		>
+			<v-row>
+				<v-col>
+					<FranceConnectBtn
+						href="/"
+						disabled
+					/>
+				</v-col>
+				<v-col>
+					<FranceConnectBtn
+						href="/"
+						is-connect-plus
+						disabled
+					/>
+				</v-col>
+			</v-row>
+		</v-theme-provider>
+	</main>
 </template>
