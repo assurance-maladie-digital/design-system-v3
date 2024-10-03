8b98165bc9fc0f68ed7c7fe6959ab7d9e9ef0b89
virginieVachet <vachetvirginie@gmail.com>
2024-10-03 09:44:42 +0200

    add vuetify types

---
+++ src/types/vuetifyTypes.ts
@@ -0,0 +1,3 @@
+export type VariantType = 'text' | 'flat' | 'elevated' | 'tonal' | 'outlined' | 'plain'
+export type DensityType = 'comfortable' | 'compact'
+export type locationType = 'start' | 'end' | 'top' | 'bottom' | 'start center' | 'end center' | 'top center' | 'bottom center'
