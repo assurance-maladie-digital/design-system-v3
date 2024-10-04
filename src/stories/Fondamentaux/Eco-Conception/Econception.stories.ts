ec1609d2e750da821ae439ccf31f8fedb76a4ece
virginieVachet <vachetvirginie@gmail.com>
2024-10-04 16:59:33 +0200

    add ecoconception

---
+++ src/stories/Fondamentaux/Eco-Conception/Econception.stories.ts
@@ -0,0 +1,30 @@
+import { VExpansionPanels, VExpansionPanel } from 'vuetify/components'
+import type { StoryObj } from '@storybook/vue3'
+import { ecoDesignItems } from '@/stories/Fondamentaux/Eco-Conception/ecoDesignItems'
+
+export default {
+	title: 'Fondamentaux/EcoConception',
+}
+
+export const EcoPanel: StoryObj = {
+	render: () => {
+		return {
+			components: { VExpansionPanels, VExpansionPanel },
+			setup() {
+				return { ecoDesignItems }
+			},
+			template: `
+				<v-expansion-panels>
+					<v-expansion-panel
+						v-for="(item, index) in ecoDesignItems"
+						:key="index"
+						:text="item.description"
+						:title="item.title"
+						variant="accordion"
+					></v-expansion-panel>
+				</v-expansion-panels>
+			`,
+		}
+	},
+	tags: ['!dev'],
+}
