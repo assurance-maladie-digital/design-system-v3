b5dc51c66fe39c97907e6bd926cf47e46e475a11
David FYON <davidfyon@gmail.com>
2024-10-07 13:43:39 +0200

    fix warn on tests

--- src/components/BackBtn/tests/BackBtn.spec.ts
+++ src/components/BackBtn/tests/BackBtn.spec.ts
@@ -1,8 +1,8 @@
 import { mount } from '@vue/test-utils'
 import { expect, describe, it } from 'vitest'
 import BackBtn from '../BackBtn.vue'
-import { mdiArrowLeft } from '@mdi/js'
 import { vuetify } from '@tests/unit/setup'
+import { VIcon, VBtn } from 'vuetify/components'
 
 describe('BackBtn', () => {
 	it('render correctly', async () => {
@@ -10,26 +10,24 @@ describe('BackBtn', () => {
 			global: {
 				plugins: [vuetify],
 			},
+			component: {
+				VIcon,
+				VBtn,
+			},
 		})
 
 		expect(wrapper.html()).toMatchSnapshot()
 	})
-	it('renders with default props', () => {
-		const wrapper = mount(BackBtn)
-		expect(wrapper.find('.vd-back-btn').exists()).toBe(true)
-		expect(wrapper.find('VIcon').text()).toBe(mdiArrowLeft)
-		expect(wrapper.find('VIcon').attributes('color')).toBe('primary')
-	})
-
-	it('renders with dark theme', () => {
-		const wrapper = mount(BackBtn, {
-			props: { dark: true },
-		})
-		expect(wrapper.find('VIcon').attributes('color')).toBe('white')
-	})
 
 	it('hides back icon when hideBackIcon is true', () => {
 		const wrapper = mount(BackBtn, {
+			global: {
+				plugins: [vuetify],
+			},
+			component: {
+				VIcon,
+				VBtn,
+			},
 			props: { hideBackIcon: true },
 		})
 		expect(wrapper.find('VIcon').exists()).toBe(false)
@@ -37,6 +35,13 @@ describe('BackBtn', () => {
 
 	it('applies correct button classes based on props', () => {
 		const wrapper = mount(BackBtn, {
+			global: {
+				plugins: [vuetify],
+			},
+			component: {
+				VIcon,
+				VBtn,
+			},
 			props: { dark: false, hideBackIcon: false },
 		})
 		expect(wrapper.find('.vd-back-btn').classes()).toContain('pr-1')
@@ -44,17 +49,39 @@ describe('BackBtn', () => {
 
 	it('applies correct button classes when dark is true and hideBackIcon is false', () => {
 		const wrapper = mount(BackBtn, {
+			global: {
+				plugins: [vuetify],
+			},
+			component: {
+				VIcon,
+				VBtn,
+			},
 			props: { dark: true, hideBackIcon: false },
 		})
 		expect(wrapper.find('.vd-back-btn').classes()).not.toContain('pr-1')
 	})
 	it('returns false when dark prop is not provided', () => {
-		const wrapper = mount(BackBtn)
+		const wrapper = mount(BackBtn, {
+			global: {
+				plugins: [vuetify],
+			},
+			component: {
+				VIcon,
+				VBtn,
+			},
+		})
 		expect(wrapper.vm.dark).toBe(false)
 	})
 
 	it('returns true when dark prop is true', () => {
 		const wrapper = mount(BackBtn, {
+			global: {
+				plugins: [vuetify],
+			},
+			component: {
+				VIcon,
+				VBtn,
+			},
 			props: { dark: true },
 		})
 		expect(wrapper.vm.dark).toBe(true)
@@ -62,6 +89,13 @@ describe('BackBtn', () => {
 
 	it('returns false when dark prop is false', () => {
 		const wrapper = mount(BackBtn, {
+			global: {
+				plugins: [vuetify],
+			},
+			component: {
+				VIcon,
+				VBtn,
+			},
 			props: { dark: false },
 		})
 		expect(wrapper.vm.dark).toBe(false)
