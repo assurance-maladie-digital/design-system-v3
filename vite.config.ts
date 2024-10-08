d2cfb09108eeb8334f94b9f510e9e6a762e84fc9
adrien charrier <adrien.charrier@outlook.fr>
2024-10-08 09:56:26 +0200

    chore: eslint

--- vite.config.ts
+++ vite.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
 	css: {
 		preprocessorOptions: {
 			scss: {
-				silenceDeprecations: ["legacy-js-api"],
+				silenceDeprecations: ['legacy-js-api'],
 			},
 		},
 	},
