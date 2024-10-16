300b7933b27473d3c9b7fa9a34b6ba73b84aaec9
David FYON <davidfyon@gmail.com>
2024-10-16 12:12:00 +0200

    add component

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,2 +1,34 @@
+<script setup lang="ts">
+	import CollapsibleList from '@/components/CollapsibleList/CollapsibleList.vue'
+	import { ListItem } from '@/components/CollapsibleList/types'
+
+	const remboursementItems = [
+		{
+			text: 'Remboursement',
+			href: '/remboursement',
+			ariaLabel: 'Remboursement',
+		},
+		{
+			text: 'Remboursement',
+			href: '/remboursement',
+			ariaLabel: 'Remboursement',
+		},
+		{
+			text: 'Remboursement',
+			href: '/remboursement',
+			ariaLabel: 'Remboursement',
+		},
+		{
+			text: 'Remboursement',
+			href: '/remboursement',
+			ariaLabel: 'Remboursement',
+		},
+	] as ListItem[]
+</script>
+
 <template>
+	<CollapsibleList
+		:items="remboursementItems"
+		list-title="Remboursement"
+	/>
 </template>
