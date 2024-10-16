43a96509faea52aaa8e27ec09f43b5457302a9e3
virginieVachet <vachetvirginie@gmail.com>
2024-10-16 09:31:15 +0200

    add a accessible v-select

---
+++ src/components/Customs/CustomSelect/CustomSelect.stories.ts
@@ -0,0 +1,122 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import CustomSelect from '@/components/Customs/CustomSelect/customSelect.vue'
+import { VBtn, VMenu, VList, VListItem, VListItemTitle } from 'vuetify/components'
+import { ref } from 'vue'
+
+const meta = {
+	title: 'Components/CustomSelect',
+	component: CustomSelect,
+	parameters: {
+		layout: 'fullscreen',
+	},
+	argTypes: {
+		selectedValue: { control: 'text' },
+		items: { control: 'object' },
+		errorMessages: { control: 'object' },
+	},
+} as Meta<typeof CustomSelect>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+export const Default: Story = {
+	args: {
+		items: [
+			{ text: 'Option 1', value: '1' },
+			{ text: 'Option 2', value: '2' },
+		],
+	},
+	render: (args) => {
+		return {
+			components: { CustomSelect, VBtn, VMenu, VList, VListItem, VListItemTitle },
+			setup() {
+				return { args }
+			},
+			template: `
+				<CustomSelect
+					v-bind="args"
+				/>
+			`,
+		}
+	},
+}
+
+export const Outlined: Story = {
+	args: {
+		items: [
+			{ text: 'Option 1', value: '1' },
+			{ text: 'Option 2', value: '2' },
+		],
+	},
+	render: (args) => {
+		return {
+			components: { CustomSelect, VBtn, VMenu, VList, VListItem, VListItemTitle },
+			setup() {
+				return { args }
+			},
+			template: `
+				<CustomSelect
+					v-bind="args"
+					outlined
+				/>
+			`,
+		}
+	},
+}
+
+export const withError: Story = {
+	args: {
+		items: [
+			{ text: 'Option 1', value: '1' },
+			{ text: 'Option 2', value: '2' },
+		],
+	},
+	render: (args) => {
+		return {
+			components: { CustomSelect, VBtn, VMenu, VList, VListItem, VListItemTitle },
+			setup() {
+				const errorMessages = ref([])
+				const triggerError = () => {
+					// @ts-expect-error test error message
+					errorMessages.value = ['This is a test error message']
+				}
+				return { args, errorMessages, triggerError }
+			},
+			template: `
+				<div>
+					<CustomSelect
+						v-bind="args"
+						:error-messages="errorMessages"
+					/>
+					<VBtn @click="triggerError">
+						Trigger Error
+					</VBtn>
+				</div>
+			`,
+		}
+	},
+}
+
+export const withCustomKey: Story = {
+	args: {
+		items: [
+			{ customKey: 'Option 1', value: '1' },
+			{ customKey: 'Option 2', value: '2' },
+		],
+	},
+	render: (args) => {
+		return {
+			components: { CustomSelect, VBtn, VMenu, VList, VListItem, VListItemTitle },
+			setup() {
+				return { args }
+			},
+			template: `
+				<CustomSelect
+					v-bind="args"
+					outlined
+					text-key="customKey"
+				/>
+			`,
+		}
+	},
+}
---
+++ src/components/Customs/CustomSelect/customSelect.vue
@@ -0,0 +1,165 @@
+<script setup lang="ts">
+	import { mdiMenuDown } from '@mdi/js'
+	import { ref, watch, computed, type PropType } from 'vue'
+	import { VIcon, VList, VListItem, VListItemTitle } from 'vuetify/components'
+
+	const props = defineProps({
+		modelValue: {
+			type: [Object, String],
+			default: null,
+		},
+		items: {
+			type: Array,
+			default: () => [],
+		},
+		label: {
+			type: String,
+			default: '',
+		},
+		errorMessages: {
+			type: [String, Array] as PropType<string | readonly string[]>,
+			default: () => [],
+		},
+		required: {
+			type: Boolean,
+			default: false,
+		},
+		menuId: {
+			type: String,
+			default: 'custom-select-menu',
+		},
+		outlined: {
+			type: Boolean,
+			default: false,
+		},
+		textKey: {
+			type: String,
+			default: 'text',
+		},
+		valueKey: {
+			type: String,
+			default: 'value',
+		},
+	})
+
+	const emit = defineEmits(['update:modelValue'])
+
+	const isOpen = ref(false)
+	const selectedItem = ref<Record<string, unknown > | string | null>(props.modelValue)
+
+	const toggleMenu = () => {
+		isOpen.value = !isOpen.value
+	}
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any -- This is a generic type
+	const selectItem = (item: any) => {
+		selectedItem.value = item
+		emit('update:modelValue', item)
+		isOpen.value = false
+	}
+
+	const getItemText = (item: unknown) => {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any -- This is a generic type
+		return (item as Record<string, any>)[props.textKey]
+	}
+
+	const selectedItemText = computed(() => {
+		if (selectedItem.value && typeof selectedItem.value === 'object') {
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any -- This is a generic type
+			return (selectedItem.value as Record<string, any>)[props.textKey]
+		}
+		return 'Sélectionnez une option'
+	})
+
+	watch(() => props.modelValue, (newValue) => {
+		selectedItem.value = newValue
+	})
+
+	const buttonClass = computed(() => {
+		return props.outlined ? 'v-btn v-btn--density-default v-btn--size-default v-btn--variant-outlined' : ''
+	})
+
+	const formattedItems = computed(() => {
+		return props.items.map((item) => {
+			if (typeof item === 'string') {
+				return { [props.textKey]: item, [props.valueKey]: item }
+			}
+			return item
+		})
+	})
+</script>
+
+<template>
+	<v-input
+		v-model="selectedItem"
+		:label="label"
+		role="menu"
+		:error-messages="errorMessages"
+		:required="required"
+	>
+		<template #default>
+			<div
+				ref="menu"
+				:class="['custom-select', buttonClass, 'primary']"
+				role="menu"
+				tabindex="0"
+				style="position: relative;"
+				@click="toggleMenu"
+				@keydown.enter.prevent="toggleMenu"
+				@keydown.space.prevent="toggleMenu"
+			>
+				<span>{{ selectedItemText }}</span>
+				<VIcon> {{ mdiMenuDown }}</VIcon>
+			</div>
+			<VList
+				v-if="isOpen"
+				class="v-list"
+				:style="`position: relative; left: -${$refs.menu ? $refs.menu.getBoundingClientRect().width : 0}px;`"
+				@keydown.esc.prevent="isOpen = false"
+			>
+				<VListItem
+					v-for="(item, index) in formattedItems"
+					:key="index"
+					:ref="'options-' + index"
+					role="option"
+					class="v-list-item"
+					:aria-selected="selectedItem === item"
+					:tabindex="index + 1"
+					@click="selectItem(item)"
+				>
+					<VListItemTitle>
+						{{ getItemText(item) }}
+					</VListItemTitle>
+				</VListItem>
+			</VList>
+		</template>
+	</v-input>
+</template>
+<style scoped lang="scss">
+@import '../../../assets/tokens';
+
+.v-list {
+  position: absolute;
+  top: 26px;
+  width: 100%;
+  z-index: 1;
+  background-color: white;
+  max-width: 150px;
+  padding: 8px 0;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.12), 0 2px 10px rgba(0, 0, 0, 0.08);
+  border-radius: 4px;
+  overflow-y: auto;
+  max-height: 300px;
+}
+
+.v-list-item:hover {
+  background-color: rgba(0, 0, 0, 0.04);
+}
+
+.v-list-item[aria-selected='true'] {
+  background-color: rgba(0, 0, 0, 0.08);
+}
+
+.v-btn {
+  color: $blue-base;
+}
+</style>
