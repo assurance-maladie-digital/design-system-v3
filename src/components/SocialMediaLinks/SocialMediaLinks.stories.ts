6385fbd0cd72af7b67a4ac072b01c9ace4dd9da3
virginieVachet <vachetvirginie@gmail.com>
2024-10-17 16:24:44 +0200

    add tests ans story

---
+++ src/components/SocialMediaLinks/SocialMediaLinks.stories.ts
@@ -0,0 +1,101 @@
+import SocialMediaLinks from './SocialMediaLinks.vue'
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { mdiFacebook, mdiLinkedin, mdiTwitter } from '@mdi/js'
+
+const meta = {
+	title: 'Components/SocialMediaLinks',
+	component: SocialMediaLinks,
+	parameters: {
+		layout: 'centered',
+	},
+	args: {
+		socialMediaLinks: [
+			{
+				icon: mdiLinkedin,
+				href: 'https://www.linkedin.com/company/assurance-maladie/',
+			},
+			{
+				icon: mdiFacebook,
+				href: 'https://twitter.com/Assur_Maladie',
+			},
+			{
+				icon: mdiTwitter,
+				href: 'https://twitter.com/Assur_Maladie',
+			},
+		],
+	},
+	argTypes: {
+		socialMediaLinks: {
+			control: { type: 'object' },
+		},
+	},
+} as Meta<typeof SocialMediaLinks>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Desktop: Story = {
+	args: {
+		socialMediaLinks: [
+			{
+				icon: mdiLinkedin,
+				href: 'https://www.linkedin.com/company/assurance-maladie/',
+			},
+			{
+				icon: mdiFacebook,
+				href: 'https://twitter.com/Assur_Maladie',
+			},
+			{
+				icon: mdiTwitter,
+				href: 'https://twitter.com/Assur_Maladie',
+			},
+		],
+	},
+	render: (args) => {
+		return {
+			components: { SocialMediaLinks },
+			setup() {
+				return { args }
+			},
+			template: `
+                <SocialMediaLinks :links="args.socialMediaLinks" />
+            `,
+		}
+	},
+}
+
+export const Mobile: Story = {
+	parameters: {
+		viewport: {
+			defaultViewport: 'mobile1',
+		},
+	},
+	args: {
+		socialMediaLinks: [
+			{
+				icon: mdiLinkedin,
+				href: 'https://www.linkedin.com/company/assurance-maladie/',
+			},
+			{
+				icon: mdiFacebook,
+				href: 'https://twitter.com/Assur_Maladie',
+			},
+			{
+				icon: mdiTwitter,
+				href: 'https://twitter.com/Assur_Maladie',
+			},
+		],
+	},
+	render: (args) => {
+		return {
+			components: { SocialMediaLinks },
+			setup() {
+				return { args }
+			},
+			template: `
+				<SocialMediaLinks :links="args.socialMediaLinks" />
+			`,
+		}
+	},
+}
---
+++ src/components/SocialMediaLinks/tests/socialMediaLinks.spec.ts
@@ -0,0 +1,66 @@
+import { mount, VueWrapper } from '@vue/test-utils'
+import SocialMediaLinks from '../SocialMediaLinks.vue'
+import { describe, it, expect, afterEach } from 'vitest'
+
+describe('SocialMediaLinks.vue', () => {
+	let wrapper: VueWrapper
+
+	afterEach(() => {
+		if (wrapper) {
+			wrapper.unmount()
+		}
+	})
+
+	it('renders correctly with default props', () => {
+		wrapper = mount(SocialMediaLinks, {
+			props: {
+				links: null,
+			},
+		})
+		expect(wrapper.html()).toMatchSnapshot()
+	})
+
+	it('renders correctly with provided links', () => {
+		const links = [
+			{ href: 'https://twitter.com', icon: 'mdi-twitter' },
+			{ href: 'https://facebook.com', icon: 'mdi-facebook' },
+		]
+		wrapper = mount(SocialMediaLinks, {
+			props: {
+				links,
+			},
+		})
+		expect(wrapper.html()).toMatchSnapshot()
+	})
+
+	it('renders the correct number of social media links', () => {
+		const links = [
+			{ href: 'https://twitter.com', icon: 'mdi-twitter' },
+			{ href: 'https://facebook.com', icon: 'mdi-facebook' },
+		]
+		wrapper = mount(SocialMediaLinks, {
+			props: {
+				links,
+			},
+		})
+		expect(wrapper.findAll('li').length).toBe(links.length)
+	})
+
+	it('renders no links when links prop is empty array', () => {
+		wrapper = mount(SocialMediaLinks, {
+			props: {
+				links: [],
+			},
+		})
+		expect(wrapper.findAll('li').length).toBe(0)
+	})
+
+	it('renders no links when links prop is null', () => {
+		wrapper = mount(SocialMediaLinks, {
+			props: {
+				links: null,
+			},
+		})
+		expect(wrapper.findAll('li').length).toBe(0)
+	})
+})
