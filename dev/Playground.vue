fe8e038fcb5ba195478f2d88751cada74194a60a
David FYON <davidfyon@gmail.com>
2024-10-18 14:08:54 +0200

    delete playground content

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,24 +1,2 @@
-<script setup lang="ts">
-	import LangBtn from '@/components/LangBtn/LangBtn.vue'
-	import CustomInputSelect from '@/components/Customs/CustomInputSelect/CustomInputSelect.vue'
-	import { ref } from 'vue'
-
-	const value = ref('fr')
-	const selectedValue2 = ref(undefined)
-	const items = ['fr', 'en', 'co']
-	const items2 = ref(['1', '2', '3', '4', '5'])
-	const ariaLabel = 'Choix de la langue'
-</script>
-
 <template>
-	<main>
-		<div class="mt-12 ml-12">
-     <CustomInputSelect
-       v-model="value"
-       :items="items"
-       label="Choix de la langue"
-       ariaLabel="Choix de la langue"
-      />
-		</div>
-	</main>
 </template>
