e61a891cc41ca1532e2469eb2679f7f3bafa3f01
David FYON <davidfyon@gmail.com>
2024-10-21 14:56:51 +0200

    fix stories

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,26 +1,2 @@
-<script setup lang="ts">
-	import { ref } from 'vue'
-	import CustomSelect from '@/components/Customs/CustomSelect/CustomSelect.vue'
-
-	const selectedValue = ref(undefined)
-
-	const items = ref([
-		{ text: 'Option 1', value: '1' },
-		{ text: 'Option 2', value: '2' },
-	])
-</script>
-
 <template>
-	<main class="ma-12">
-		SelectedValue: {{ selectedValue }}
-		<div class="d-flex flex-wrap align-center">
-			<custom-select
-				v-model="selectedValue"
-				:items="items"
-				text-key="text"
-				outlined
-				required
-			/>
-		</div>
-	</main>
 </template>
