a8a8c4fe431ff50888a01b78d019b672d27631dc
virginieVachet <vachetvirginie@gmail.com>
2024-10-21 10:00:59 +0200

    fix playground

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,16 +1,2 @@
-<script setup lang="ts">
-	import Logo from '@/components/Logo/LOGO.vue'
-</script>
-
 <template>
-	<main class="ma-12">
-		<Logo />
-		<Logo hide-signature />
-		<Logo risque-pro />
-		<Logo avatar />
-		<Logo
-			avatar
-			size="small"
-		/>
-	</main>
 </template>
