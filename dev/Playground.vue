dcec467f0947e539853f739895dcc9acc3d9c237
David FYON <davidfyon@gmail.com>
2024-10-21 09:11:35 +0200

    fix min width

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,2 +1,21 @@
+<script setup lang="ts">
+	import CustomInputSelect from '@/components/Customs/CustomInputSelect/CustomInputSelect.vue'
+	import { ref } from 'vue'
+
+	const value = ref('fr')
+	const items = ['fr', 'en', 'co']
+	const ariaLabel = 'Choix de la langue'
+</script>
+
 <template>
+	<main>
+		<div class="mt-12 ml-12">
+			<CustomInputSelect
+				v-model="value"
+				:items="items"
+				:label="ariaLabel"
+				aria-label="Choix de la langue"
+			/>
+		</div>
+	</main>
 </template>
