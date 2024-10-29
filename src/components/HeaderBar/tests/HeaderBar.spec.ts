78142bf3a39f6fff273ea66daddbf33b4dee1a7d
adrien charrier <adrien.charrier@outlook.fr>
2024-10-29 14:39:33 +0100

    improve tests

--- src/components/HeaderBar/tests/HeaderBar.spec.ts
+++ src/components/HeaderBar/tests/HeaderBar.spec.ts
@@ -20,7 +20,7 @@ describe('HeaderBar', () => {
 			},
 			attachTo: document.body,
 		})
-		window.dispatchEvent(new CustomEvent('scroll', { detail: 0 }))
+		window.dispatchEvent(new CustomEvent('scroll'))
 
 		expect(wrapper.html()).toMatchSnapshot()
 
@@ -30,7 +30,7 @@ describe('HeaderBar', () => {
 		vi.spyOn(header.element, 'getBoundingClientRect').mockReturnValue({
 			top: -2000,
 		} as DOMRect)
-		window.dispatchEvent(new CustomEvent('scroll', { detail: 2000 }))
+		window.dispatchEvent(new CustomEvent('scroll'))
 
 		await wrapper.vm.$nextTick()
 
@@ -70,6 +70,58 @@ describe('HeaderBar', () => {
 		wrapper.unmount()
 	})
 
+
+	const TestMenu = defineComponent({
+		setup() {
+			const menu = ref(false)
+			const registerHeaderMenu = inject<(r: Ref<boolean>) => void>('registerHeaderMenu')
+
+			onMounted(() => {
+				if (registerHeaderMenu) {
+					registerHeaderMenu(menu)
+				}
+			})
+
+			return {
+				menu,
+			}
+		},
+		template: '<button @click="menu = true">Menu</button>',
+	})
+
+	it('should render the menu slot', async () => {
+		const wrapper = mount({
+			components: {
+				HeaderBar,
+				TestMenu,
+			},
+			template: `
+				<HeaderBar>
+					<template #menu>
+						<TestMenu />
+					</template>
+					<template #logo="{menuOpen}">
+						<div>{{menuOpen ? 'the menu is open' : 'the menu is closed'}}</div>
+					</template>
+				</HeaderBar>`,
+		}, {
+			global: {
+				plugins: [vuetify],
+			},
+			attachTo: document.body,
+		})
+		const text = wrapper.text()
+		expect(text).toContain('Menu')
+		expect(text).toContain('the menu is closed')
+
+		const button = wrapper.find('button')
+		await button.trigger('click')
+
+		expect(wrapper.text()).toContain('the menu is open')
+
+		wrapper.unmount()
+	})
+
 	it('should render in sticky mode', async () => {
 		const wrapper = mount(HeaderBar, {
 			props: {
@@ -80,7 +132,7 @@ describe('HeaderBar', () => {
 			},
 			attachTo: document.body,
 		})
-		window.dispatchEvent(new CustomEvent('scroll', { detail: 0 }))
+		window.dispatchEvent(new CustomEvent('scroll'))
 
 		// mock scroll related values
 		vi.spyOn(window, 'scrollY', 'get').mockReturnValue(2000)
@@ -88,7 +140,7 @@ describe('HeaderBar', () => {
 		vi.spyOn(header.element, 'getBoundingClientRect').mockReturnValue({
 			top: -2000,
 		} as DOMRect)
-		window.dispatchEvent(new CustomEvent('scroll', { detail: 2000 }))
+		window.dispatchEvent(new CustomEvent('scroll'))
 
 		await wrapper.vm.$nextTick()
 
@@ -112,20 +164,21 @@ describe('HeaderBar', () => {
 			},
 			attachTo: document.body,
 		})
-		window.dispatchEvent(new CustomEvent('scroll', { detail: 0 }))
+		window.dispatchEvent(new CustomEvent('scroll'))
 
 		const header = wrapper.find('.header')
+		const mockHeaderRect = vi.spyOn(header.element, 'getBoundingClientRect')
 
 		// mock scroll related values
-		vi.spyOn(window, 'scrollY', 'get').mockReturnValue(2000)
-
-		vi.spyOn(header.element, 'getBoundingClientRect').mockReturnValue({
+		const mockScrollY = vi.spyOn(window, 'scrollY', 'get')
+		const mockScrollTop = vi.spyOn(document.documentElement, 'scrollTop', 'get')
+		mockScrollY.mockReturnValue(2000)
+		mockScrollTop.mockReturnValue(2000)
+		mockHeaderRect.mockReturnValue({
 			top: -2000,
 			height: 77,
 		} as DOMRect)
-		vi.spyOn(document.documentElement, 'scrollTop', 'get').mockReturnValue(2000)
-		window.dispatchEvent(new CustomEvent('scroll', { detail: 2000 }))
-
+		window.dispatchEvent(new CustomEvent('scroll'))
 		await wrapper.vm.$nextTick()
 
 		const stickyHeader = wrapper.find('.sticky-header')
@@ -136,14 +189,13 @@ describe('HeaderBar', () => {
 		expect(stickyHeaderStyle).toContain('top: 0px;')
 		expect(stickyHeaderStyle).toContain('transform: translateY(-100%);')
 
-		vi.spyOn(window, 'scrollY', 'get').mockReturnValue(800)
-		vi.spyOn(header.element, 'getBoundingClientRect').mockReturnValue({
+		mockScrollY.mockReturnValue(800)
+		mockScrollTop.mockReturnValue(800)
+		mockHeaderRect.mockReturnValue({
 			top: -800,
 			height: 77,
 		} as DOMRect)
-		vi.spyOn(document.documentElement, 'scrollTop', 'get').mockReturnValue(800)
-		window.dispatchEvent(new CustomEvent('scroll', { detail: 800 }))
-
+		window.dispatchEvent(new CustomEvent('scroll'))
 		await wrapper.vm.$nextTick()
 
 		stickyHeaderStyle = stickyHeader.attributes('style')
@@ -152,60 +204,6 @@ describe('HeaderBar', () => {
 		expect(stickyHeaderStyle).toContain('position: fixed;')
 		expect(stickyHeaderStyle).toContain('top: 0px;')
 		expect(stickyHeaderStyle).toContain('transform: none;')
-
-		wrapper.unmount()
-	})
-
-	const testMenu = defineComponent({
-		setup() {
-			const menu = ref(false)
-			const registerHeaderMenu = inject<(r: Ref<boolean>) => void>('registerHeaderMenu')
-
-			onMounted(() => {
-				if (registerHeaderMenu) {
-					registerHeaderMenu(menu)
-				}
-			})
-
-			return {
-				menu,
-			}
-		},
-		template: '<button @click="menu = true">Menu</button>',
-	})
-
-	it('should render the menu slot', async () => {
-		const wrapper = mount({
-			components: {
-				HeaderBar,
-				Menu: testMenu,
-			},
-			template: `
-				<HeaderBar>
-					<template #menu>
-						<Menu />
-					</template>
-					<template #logo="{menuOpen}">
-						<div>{{menuOpen ? 'the menu is open' : 'the menu is closed'}}</div>
-					</template>
-				</HeaderBar>`,
-		}, {
-			global: {
-				plugins: [vuetify],
-			},
-			attachTo: document.body,
-			slots: {
-				menu: testMenu,
-			},
-		})
-		const text = wrapper.text()
-		expect(text).toContain('Menu')
-		expect(text).toContain('the menu is closed')
-
-		const button = wrapper.find('button')
-		await button.trigger('click')
-
-		expect(wrapper.text()).toContain('the menu is open')
 
 		wrapper.unmount()
 	})
