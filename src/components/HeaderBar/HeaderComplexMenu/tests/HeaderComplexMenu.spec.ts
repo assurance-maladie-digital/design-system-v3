a8b28742e3c1d99a4b68e9c79f07f115627bbc3e
adrien charrier <adrien.charrier@outlook.fr>
2024-10-30 18:26:15 +0100

    fix tests

--- src/components/HeaderBar/HeaderComplexMenu/tests/HeaderComplexMenu.spec.ts
+++ src/components/HeaderBar/HeaderComplexMenu/tests/HeaderComplexMenu.spec.ts
@@ -1,8 +1,8 @@
-import { describe, it, expect } from 'vitest'
-
-import HeaderComplexMenu from '../HeaderComplexMenu.vue'
-import { mount } from '@vue/test-utils'
 import { vuetify } from '@tests/unit/setup'
+import { mount } from '@vue/test-utils'
+import { describe, expect, it, vi } from 'vitest'
+import HeaderComplexMenu from '../HeaderComplexMenu.vue'
+import { afterEach } from 'node:test'
 
 describe('HeaderComplexMenu', () => {
 	const BtnTestComponent = {
@@ -13,10 +13,18 @@ describe('HeaderComplexMenu', () => {
 		template: `<button @click="$emit('update:modelValue', !open)">Test</button>`,
 	}
 
+	afterEach(() => {
+		vi.resetAllMocks()
+		document.body.innerHTML = ''
+	})
+
 	it('should render the component', async () => {
 		const wrapper = mount(HeaderComplexMenu, {
 			global: {
 				plugins: [vuetify],
+				provide: {
+					registerHeaderMenu: () => {},
+				},
 			},
 			slots: {
 				default: '<div>Default slot</div>',
@@ -24,14 +32,11 @@ describe('HeaderComplexMenu', () => {
 			stubs: {
 				HeaderMenuBtn: BtnTestComponent,
 			},
-			inject: {
-				registerHeaderMenu: () => {},
-			},
 		})
 
 		expect(wrapper.html()).toMatchSnapshot()
 
-		const menu = wrapper.find('.menu')
+		const menu = wrapper.find('.overlay')
 		expect(menu.attributes('style')).toContain('display: none;')
 
 		const btn = wrapper.find('.header-menu-btn')
@@ -43,9 +48,9 @@ describe('HeaderComplexMenu', () => {
 		const wrapper = mount(HeaderComplexMenu, {
 			global: {
 				plugins: [vuetify],
-			},
-			inject: {
-				registerHeaderMenu: () => {},
+				provide: {
+					registerHeaderMenu: () => {},
+				},
 			},
 			stubs: {
 				HeaderMenuBtn: BtnTestComponent,
@@ -53,25 +58,28 @@ describe('HeaderComplexMenu', () => {
 			slots: {
 				default: '<div>Default slot</div>',
 			},
+			attachTo: document.body,
 		})
 
-		const menu = wrapper.find('.menu')
+		const overlay = wrapper.find('.overlay')
 		const btn = wrapper.find('.header-menu-btn')
+
 		await btn.trigger('click')
-		expect(menu.attributes('style')).toBeUndefined()
+		expect(overlay.attributes('style')).toBeUndefined()
 
-		await wrapper.find('.overlay').trigger('click')
+		await overlay.trigger('click')
+		expect(overlay.attributes('style')).toContain('display: none;')
 
-		expect(menu.attributes('style')).toContain('display: none;')
+		wrapper.unmount()
 	})
 
 	it('should not close the menu when clicking inside', async () => {
 		const wrapper = mount(HeaderComplexMenu, {
 			global: {
 				plugins: [vuetify],
-			},
-			inject: {
-				registerHeaderMenu: () => {},
+				provide: {
+					registerHeaderMenu: () => {},
+				},
 			},
 			stubs: {
 				HeaderMenuBtn: BtnTestComponent,
@@ -79,24 +87,27 @@ describe('HeaderComplexMenu', () => {
 			slots: {
 				default: '<div>Default slot</div>',
 			},
+			attachTo: document.body,
 		})
 
-		const menu = wrapper.find('.menu')
+		const menu = wrapper.find('.overlay')
 		const btn = wrapper.find('button')
 		await btn.trigger('click')
 		expect(menu.attributes('style')).toBeUndefined()
 
 		await wrapper.find('.header-menu').trigger('click')
 		expect(menu.attributes('style')).toBeUndefined()
+
+		wrapper.unmount()
 	})
 
 	it('should listen to the button to open and close the menu', async () => {
 		const wrapper = mount(HeaderComplexMenu, {
 			global: {
 				plugins: [vuetify],
-			},
-			inject: {
-				registerHeaderMenu: () => {},
+				provide: {
+					registerHeaderMenu: () => {},
+				},
 			},
 			stubs: {
 				HeaderMenuBtn: BtnTestComponent,
@@ -106,7 +117,7 @@ describe('HeaderComplexMenu', () => {
 			},
 		})
 
-		const menu = wrapper.find('.menu')
+		const menu = wrapper.find('.overlay')
 		const btn = wrapper.find('button')
 		await btn.trigger('click')
 		expect(menu.attributes('style')).toBeUndefined()
