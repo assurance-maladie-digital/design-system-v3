18ecc2b76b19aed68f9f8c42f1cf67bb4bd5c8c2
David FYON <davidfyon@gmail.com>
2024-11-21 11:07:09 +0100

    improve notifications

--- src/stories/Fondamentaux/Accessibilite/Accessibilite.stories.ts
+++ src/stories/Fondamentaux/Accessibilite/Accessibilite.stories.ts
@@ -3,7 +3,7 @@ import type { StoryObj } from '@storybook/vue3'
 import { AccessibiliteItems } from './AccessibiliteItems'
 
 export default {
-	title: 'Fondamentaux/Accessibilite',
+	title: 'Fondamentaux/Accessibilité',
 }
 
 export const AccessibilitePanel: StoryObj = {
