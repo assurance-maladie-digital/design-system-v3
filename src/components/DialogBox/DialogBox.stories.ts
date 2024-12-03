7e2cb53f6f47829d101cfd76733e6c3aa972befe
Adrien <65124433+4dr1en@users.noreply.github.com>
2024-12-03 11:41:41 +0100

    Feat: add DialogBox component (#238)
    * Feat: add DialogBox component
    
    * requested changes
    
    * fix regression
    
    * fix stories
    
    * fix stories
    
    ---------
    
    Co-authored-by: David FYON <davidfyon@gmail.com>

---
+++ src/components/DialogBox/DialogBox.stories.ts
@@ -0,0 +1,798 @@
+import { type Meta, type StoryObj } from '@storybook/vue3'
+import { VBtn } from 'vuetify/components'
+import DialogBox from './DialogBox.vue'
+import { fn } from '@storybook/test'
+
+const meta = {
+	title: 'Composants/Feedback/DialogBox',
+	component: DialogBox,
+	parameters: {
+		layout: 'fullscreen',
+		docs: {
+			controls: {
+				exclude: ['cancel', 'confirm', 'update:modelValue'],
+			},
+		},
+	},
+	argTypes: {
+		'modelValue': {
+			control: 'boolean',
+			description: 'Afficher la boîte de dialogue, a utiliser avec `v-model`',
+			table: {
+				category: 'props',
+			},
+		},
+		'title': {
+			control: 'text',
+			description: 'Titre de la boîte de dialogue, utilisable en prop ou slot',
+			table: {
+				type: {
+					summary: 'string',
+				},
+			},
+		},
+		'default': {
+			control: 'text',
+			description: 'Contenu de la boîte de dialogue',
+			table: {
+				type: {
+					summary: '{}',
+				},
+			},
+		},
+		'actions': {
+			control: 'text',
+			description: 'Contenu des actions de la boîte de dialogue',
+			table: {
+				type: {
+					summary: '{}',
+				},
+			},
+		},
+		'width': {
+			control: 'text',
+			description: 'Largeur max de la boîte de dialogue',
+			table: {
+				type: {
+					summary: 'string',
+				},
+			},
+		},
+		'cancelBtnText': {
+			control: 'text',
+			description: 'Texte du bouton d\'annulation',
+			table: {
+				type: {
+					summary: 'string',
+				},
+				defaultValue: {
+					summary: `'Annuler'`,
+				},
+			},
+		},
+		'confirmBtnText': {
+			control: 'text',
+			description: 'Texte du bouton de confirmation',
+			table: {
+				type: {
+					summary: 'string',
+				},
+				defaultValue: {
+					summary: `'Confirmer'`,
+				},
+			},
+		},
+		'hideActions': {
+			control: 'boolean',
+			description: 'Masquer les actions',
+			table: {
+				type: {
+					summary: 'boolean',
+				},
+			},
+		},
+		'persistent': {
+			control: 'boolean',
+			description: 'Masquer le bouton de fermeture',
+			table: {
+				type: {
+					summary: 'boolean',
+				},
+			},
+		},
+		'vuetifyOptions': {
+			control: 'object',
+			description: 'Personnalisation des composants Vuetify internes',
+			table: {
+				category: 'props',
+				defaultValue: {
+					detail: `
+					{
+						card: {
+							class: 'pa-6',
+						},
+						cardTitle: {
+							class: 'd-flex align-start flex-nowrap pa-0 mb-6 mr-6',
+						},
+						closeBtn: {
+							icon: true,
+							elevation: 0,
+							width: '32px',
+							height: '32px',
+							class: 'mt-n2 mr-n2 ml-4',
+						},
+						actionsCtn: {
+							class: 'd-flex flex-wrap mt-6',
+						},
+						cancelBtn: {
+							color: 'primary',
+							variant: 'text',
+						},
+						confirmBtn: {
+							color: 'primary',
+						},
+					}`,
+				},
+				type: {
+					summary: 'Record<string, any>',
+					detail: `
+					{
+	card?: Record<string, any>,
+	cardTitle?: Record<string, any>,
+	closeBtn?: Record<string, any>,
+	actionsCtn?: Record<string, any>,
+	cancelBtn?: Record<string, any>,
+	confirmBtn?: Record<string, any>,
+}
+					`,
+				},
+			},
+		},
+		'onCancel': {
+			description: 'Callback sur annulation',
+			table: {
+				category: 'events',
+			},
+		},
+		'onConfirm': {
+			description: 'Callback sur confirmation',
+			table: {
+				category: 'events',
+			},
+		},
+		'onUpdate:modelValue': {
+			description: 'Callback sur mise à jour de la valeur',
+			table: {
+				category: 'events',
+			},
+		},
+	},
+} satisfies Meta<typeof DialogBox>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+	args: {
+		'modelValue': false,
+		'title': 'DialogBox title',
+		'default': 'DialogBox content',
+		'onCancel': fn(),
+		'onConfirm': fn(),
+		'onUpdate:modelValue': fn(),
+	},
+	render: (args) => {
+		return {
+			components: { DialogBox, VBtn },
+			setup() {
+				return { args }
+			},
+			template: `
+				<div class="pa-4">
+					<VBtn
+						@click="args.modelValue = !args.modelValue"
+						color="primary"
+					>Toggle DialogBox</VBtn>
+					<DialogBox
+						v-bind="args"
+						@confirm="args.modelValue = false"
+						@cancel="args.modelValue = false"
+						@update:modelValue="args.modelValue = $event"
+					>
+						{{ args.default }}
+					</DialogBox>
+				</div>
+			`,
+		}
+	},
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `
+				<template>
+					<VBtn
+						color="primary"
+						@click="dialogOpen = !dialogOpen"
+					>Toggle DialogBox</VBtn>
+					<DialogBox
+						v-model="dialogOpen"
+						title="DialogBox title"
+						@confirm="dialogOpen = false"
+						@cancel="dialogOpen = false"
+					>
+						DialogBox content
+					</DialogBox>
+				</template>
+				`,
+			},
+			{
+				name: 'Script',
+				code: `
+				<script setup lang="ts">
+					import { DialogBox } from '@cnamts/synapse'
+					import { ref } from 'vue'
+
+					const dialogOpen = ref(false)
+				</script>
+				`,
+			},
+		],
+	},
+}
+
+export const ButtonTexts: Story = {
+	args: {
+		'modelValue': false,
+		'title': 'DialogBox title',
+		'default': 'DialogBox content',
+		'cancelBtnText': 'Retour',
+		'confirmBtnText': 'Valider',
+		'onCancel': fn(),
+		'onConfirm': fn(),
+		'onUpdate:modelValue': fn(),
+	},
+	render: (args) => {
+		return {
+			components: { DialogBox, VBtn },
+			setup() {
+				return { args }
+			},
+			template: `
+				<div class="pa-4">
+					<VBtn
+						@click="args.modelValue = !args.modelValue"
+						color="primary"
+					>Toggle DialogBox</VBtn>
+					<DialogBox
+						v-bind="args"
+						@update:modelValue="args.modelValue = $event"
+						@confirm="args.modelValue = false"
+						@cancel="args.modelValue = false"
+					>
+						{{ args.default }}
+					</DialogBox>
+				</div>
+			`,
+		}
+	},
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `
+				<template>
+					<VBtn
+						color="primary"
+						@click="dialogOpen = !dialogOpen"
+					>Toggle DialogBox</VBtn>
+					<DialogBox
+						v-model="dialogOpen"
+						title="DialogBox title"
+						cancelBtnText="Retour"
+						confirmBtnText="Valider"
+						@confirm="dialogOpen = false"
+						@cancel="dialogOpen = false"
+					>
+						DialogBox content
+					</DialogBox>
+				</template>
+				`,
+			},
+			{
+				name: 'Script',
+				code: `
+				<script setup lang="ts">
+					import { DialogBox } from '@cnamts/synapse'
+					import { ref } from 'vue'
+
+					const dialogOpen = ref(false)
+				</script>
+				`,
+			},
+		],
+	},
+}
+
+export const HideActions: Story = {
+	args: {
+		'modelValue': false,
+		'title': 'DialogBox title',
+		'default': 'DialogBox content',
+		'hideActions': true,
+		'onCancel': fn(),
+		'onConfirm': fn(),
+		'onUpdate:modelValue': fn(),
+	},
+	render: (args) => {
+		return {
+			components: { DialogBox, VBtn },
+			setup() {
+				return { args }
+			},
+			template: `
+				<div class="pa-4">
+					<VBtn
+						@click="args.modelValue = !args.modelValue"
+						color="primary"
+					>Toggle DialogBox</VBtn>
+					<DialogBox
+						v-bind="args"
+						@update:modelValue="args.modelValue = $event"
+					>
+						{{ args.default }}
+					</DialogBox>
+				</div>
+			`,
+		}
+	},
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `
+				<template>
+					<VBtn
+						color="primary"
+						@click="dialogOpen = !dialogOpen"
+					>Toggle DialogBox</VBtn>
+					<DialogBox
+						v-model="dialogOpen"
+						title="DialogBox title"
+						hideActions
+					>
+						DialogBox content
+					</DialogBox>
+				</template>
+				`,
+			},
+			{
+				name: 'Script',
+				code: `
+				<script setup lang="ts">
+					import { DialogBox } from '@cnamts/synapse'
+					import { ref } from 'vue'
+
+					const dialogOpen = ref(false)
+				</script>
+				`,
+			},
+		],
+	},
+}
+
+export const Persistent: Story = {
+	args: {
+		'modelValue': false,
+		'title': 'DialogBox title',
+		'default': 'DialogBox content',
+		'persistent': true,
+		'onCancel': fn(),
+		'onConfirm': fn(),
+		'onUpdate:modelValue': fn(),
+	},
+	render: (args) => {
+		return {
+			components: { DialogBox, VBtn },
+			setup() {
+				return { args }
+			},
+			template: `
+				<div class="pa-4">
+					<VBtn
+						@click="args.modelValue = !args.modelValue"
+						color="primary"
+					>Toggle DialogBox</VBtn>
+					<DialogBox
+						v-bind="args"
+						@update:modelValue="args.modelValue = $event"
+						@confirm="args.modelValue = false"
+						@cancel="args.modelValue = false"
+					>
+						{{ args.default }}
+					</DialogBox>
+				</div>
+			`,
+		}
+	},
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `
+				<template>
+					<VBtn
+						color="primary"
+						@click="dialogOpen = !dialogOpen"
+					>Toggle DialogBox</VBtn>
+					<DialogBox
+						v-model="dialogOpen"
+						title="DialogBox title"
+						Persistent
+						@confirm="dialogOpen = false"
+						@cancel="dialogOpen = false"
+					>
+						DialogBox content
+					</DialogBox>
+				</template>
+				`,
+			},
+			{
+				name: 'Script',
+				code: `
+				<script setup lang="ts">
+					import { DialogBox } from '@cnamts/synapse'
+					import { ref } from 'vue'
+
+					const dialogOpen = ref(false)
+				</script>
+				`,
+			},
+		],
+	},
+}
+
+export const ActionsSlot: Story = {
+	args: {
+		'modelValue': false,
+		'title': 'DialogBox title',
+		'default': 'DialogBox content',
+		'onCancel': fn(),
+		'onConfirm': fn(),
+		'onUpdate:modelValue': fn(),
+	},
+	render: (args) => {
+		return {
+			components: { DialogBox, VBtn },
+			setup() {
+				return { args }
+			},
+			template: `
+				<div class="pa-4">
+					<VBtn
+						@click="args.modelValue = !args.modelValue"
+						color="primary"
+					>Toggle DialogBox</VBtn>
+					<DialogBox
+						v-bind="args"
+						@update:modelValue="args.modelValue = $event"
+					>
+						<template #actions>
+							<VBtn
+								color="primary"
+								@click="args.modelValue = false"
+							>Action</VBtn>
+						</template>
+						{{ args.default }}
+					</DialogBox>
+				</div>
+			`,
+		}
+	},
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `
+				<template>
+					<VBtn
+						color="primary"
+						@click="dialogOpen = !dialogOpen"
+					>Toggle DialogBox</VBtn>
+					<DialogBox
+						v-model="dialogOpen"
+						title="DialogBox title"
+					>
+						<template #actions>
+							<VBtn
+								color="primary"
+								@click="dialogOpen = false"
+							>Action</VBtn>
+						</template>
+						DialogBox content
+					</DialogBox>
+				</template>
+				`,
+			},
+			{
+				name: 'Script',
+				code: `
+				<script setup lang="ts">
+					import { DialogBox } from '@cnamts/synapse'
+					import { ref } from 'vue'
+
+					const dialogOpen = ref(false)
+				</script>
+				`,
+			},
+		],
+	},
+}
+
+export const TitleSlot: Story = {
+	args: {
+		'modelValue': false,
+		'default': 'DialogBox content',
+		'onCancel': fn(),
+		'onConfirm': fn(),
+		'onUpdate:modelValue': fn(),
+	},
+	render: (args) => {
+		return {
+			components: { DialogBox, VBtn },
+			setup() {
+				return { args }
+			},
+			template: `
+				<div class="pa-4">
+					<VBtn
+						@click="args.modelValue = !args.modelValue"
+						color="primary"
+					>Toggle DialogBox</VBtn>
+					<DialogBox
+						v-bind="args"
+						@update:modelValue="args.modelValue = $event"
+						@confirm="args.modelValue = false"
+						@cancel="args.modelValue = false"
+					>
+						<template #title>
+							<VBtn
+								color="primary"
+								@click="args.modelValue = false"
+							>Title slot</VBtn>
+						</template>
+						{{ args.default }}
+					</DialogBox>
+				</div>
+			`,
+		}
+	},
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `
+				<template>
+					<VBtn
+						color="primary"
+						@click="dialogOpen = !dialogOpen"
+					>Toggle DialogBox</VBtn>
+					<DialogBox
+						v-model="dialogOpen"
+					>
+						<template #title>
+							<VBtn 
+								color="primary"
+								@click="dialogOpen = false"
+							>Title slot</VBtn>
+						</template>
+						DialogBox content
+					</DialogBox>
+				</template>
+				`,
+			},
+			{
+				name: 'Script',
+				code: `
+				<script setup lang="ts">
+					import { DialogBox } from '@cnamts/synapse'
+					import { ref } from 'vue'
+
+					const dialogOpen = ref(false)
+				</script>
+				`,
+			},
+		],
+	},
+}
+
+export const Width: Story = {
+	args: {
+		'modelValue': false,
+		'title': 'DialogBox title',
+		'default': 'DialogBox content',
+		'width': '500px',
+		'onCancel': fn(),
+		'onConfirm': fn(),
+		'onUpdate:modelValue': fn(),
+	},
+	render: (args) => {
+		return {
+			components: { DialogBox, VBtn },
+			setup() {
+				return { args }
+			},
+			template: `
+				<div class="pa-4">
+					<VBtn
+						@click="args.modelValue = !args.modelValue"
+						color="primary"
+					>Toggle DialogBox</VBtn>
+					<DialogBox
+						v-bind="args"
+						@update:modelValue="args.modelValue = $event"
+						@confirm="args.modelValue = false"
+						@cancel="args.modelValue = false"
+					>
+						{{ args.default }}
+					</DialogBox>
+				</div>
+				`,
+		}
+	},
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `
+					<template>
+						<VBtn
+							color="primary"
+							@click="dialogOpen = !dialogOpen"
+						>Toggle DialogBox</VBtn>
+						<DialogBox
+							v-model="dialogOpen"
+							title="DialogBox title"
+							width="500px"
+							@confirm="dialogOpen = false"
+							@cancel="dialogOpen = false"
+						>
+							DialogBox content
+						</DialogBox>
+					</template>
+					`,
+			},
+			{
+				name: 'Script',
+				code: `
+					<script setup lang="ts">
+						import { DialogBox } from '@cnamts/synapse'
+						import { ref } from 'vue'
+	
+						const dialogOpen = ref(false)
+					</script>
+					`,
+			},
+		],
+	},
+}
+
+export const VuetifyOptions: Story = {
+	args: {
+		'modelValue': false,
+		'title': 'DialogBox title',
+		'default': 'DialogBox content',
+		'vuetifyOptions': {
+			card: {
+				rounded: 'xl',
+			},
+			cardTitle: {
+				class: 'pa-0 mb-4 accent--text',
+			},
+			closeBtn: {
+				class: {
+					'd-none': true,
+				},
+			},
+			icon: {
+				color: 'accent',
+			},
+			cancelBtn: {
+				color: 'accent',
+				text: false,
+			},
+			confirmBtn: {
+				color: 'accent',
+				text: true,
+			},
+		},
+		'onCancel': fn(),
+		'onConfirm': fn(),
+		'onUpdate:modelValue': fn(),
+	},
+	render: (args) => {
+		return {
+			components: { DialogBox, VBtn },
+			setup() {
+				return { args }
+			},
+			template: `
+				<div class="pa-4">
+					<VBtn
+						@click="args.modelValue = !args.modelValue"
+						color="primary"
+					>Toggle DialogBox</VBtn>
+					<DialogBox
+						v-bind="args"
+						@update:modelValue="args.modelValue = $event"
+						@confirm="args.modelValue = false"
+						@cancel="args.modelValue = false"
+					>
+						{{ args.default }}
+					</DialogBox>
+				</div>
+			`,
+		}
+	},
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `
+				<template>
+					<VBtn
+						color="primary"
+						@click="dialogOpen = !dialogOpen"
+					>Toggle DialogBox</VBtn>
+					<DialogBox
+						v-model="dialogOpen"
+						title="DialogBox title"
+						:vuetify-options="vuetifyOptions"
+						@confirm="dialogOpen = false"
+						@cancel="dialogOpen = false"
+					>
+						DialogBox content
+					</DialogBox>
+				</template>
+				`,
+			},
+			{
+				name: 'Script',
+				code: `
+				<script setup lang="ts">
+					import { DialogBox } from '@cnamts/synapse'
+					import { ref } from 'vue'
+
+					const dialogOpen = ref(false)
+					
+					const vuetifyOptions = {
+						card: {
+							rounded: 'xl',
+						},
+						cardTitle: {
+							class: 'pa-0 mb-4 accent--text',
+						},
+						closeBtn: {
+							class: {
+								'd-none': true,
+							},
+						},
+						icon: {
+							color: 'accent',
+						},
+						cancelBtn: {
+							color: 'accent',
+							text: false,
+						},
+						confirmBtn: {
+							color: 'accent',
+							text: true,
+						},
+					}
+				</script>
+				`,
+			},
+		],
+	},
+}
---
+++ src/components/DialogBox/tests/DialogBox.spec.ts
@@ -0,0 +1,329 @@
+/* eslint-disable vue/one-component-per-file */
+import { describe, it, expect } from 'vitest'
+import { mount, shallowMount } from '@vue/test-utils'
+import { vuetify } from '@tests/unit/setup'
+
+import { VCard } from 'vuetify/components'
+import { defineComponent } from 'vue'
+import DialogBox from '../DialogBox.vue'
+
+const defaultProps = {
+	modelValue: true,
+	title: 'Test title',
+	width: '600px',
+	cancelBtnText: 'Cancel',
+	confirmBtnText: 'Confirm',
+	hideActions: false,
+	persistent: false,
+}
+
+describe('DialogBox', () => {
+	describe('rendering and props', () => {
+		it('renders correctly with props', () => {
+			const wrapper = mount(DialogBox, {
+				props: defaultProps,
+				global: {
+					plugins: [vuetify],
+					stubs: {
+						VDialog: {
+							template: '<div><slot></slot></div>',
+						},
+					},
+				},
+			})
+
+			expect(wrapper.html()).toMatchSnapshot()
+		})
+
+		it('is closed when model value is false', async () => {
+			const wrapper = mount(DialogBox, {
+				props: {
+					...defaultProps,
+					modelValue: false,
+				},
+				global: {
+					plugins: [vuetify],
+				},
+			})
+
+			expect(wrapper.html()).toBe('')
+		})
+
+		it('becomes visible when the model value is updated', async () => {
+			const wrapper = mount(DialogBox, {
+				props: defaultProps,
+				global: {
+					plugins: [vuetify],
+				},
+			})
+
+			const card = wrapper.getComponent(VCard)
+			expect(card.isVisible()).toBe(true)
+
+			await wrapper.setProps({ modelValue: false })
+			expect(card.isVisible()).toBe(false)
+
+			await wrapper.setProps({ modelValue: true })
+			expect(card.isVisible()).toBe(true)
+		})
+
+		it('renders the title slot', async () => {
+			const wrapper = mount(DialogBox, {
+				slots: {
+					title: '<h2>Test title</h2>',
+				},
+				props: defaultProps,
+				global: {
+					plugins: [vuetify],
+				},
+			})
+
+			const modal = wrapper.getComponent(VCard)
+			const title = modal.find<HTMLElement>('h2').text()
+
+			await modal.vm.$nextTick()
+
+			expect(title).toBe('Test title')
+		})
+	})
+
+	describe('focusable elements and tab navigation', () => {
+		it('gets the correct focusable elements', async () => {
+			const wrapper = mount(DialogBox, {
+				slots: {
+					default: `
+						<button id="first">First</button>
+						<button id="second" disabled>Second</button>
+						<button id="third">third</button>
+						<a href="https://www.ameli.fr/" id="link">ameli.fr</a>
+					`,
+				},
+				props: {
+					...defaultProps,
+					hideActions: true,
+					persistent: true,
+				},
+				global: {
+					plugins: [vuetify],
+				},
+			})
+
+			const modal = wrapper.getComponent(VCard)
+
+			const firstBtn = modal.find<HTMLElement>('#first')
+			const thirdBtn = modal.find<HTMLElement>('#third')
+			const theLink = modal.find<HTMLElement>('#link')
+
+			await modal.vm.$nextTick()
+
+			// @ts-expect-error - Testing private method
+			expect(await wrapper.vm.getSelectableElements()).toEqual([
+				firstBtn.element,
+				thirdBtn.element,
+				theLink.element,
+			])
+		})
+
+		it('handles the internal tab navigation', async () => {
+			const wrapper = mount(DialogBox, {
+				slots: {
+					default: `
+						<button id="first">First</button>
+						<button id="second" disabled>Second</button>
+						<button id="third">third</button>
+						<a href="https://www.ameli.fr/" id="link">ameli.fr</a>
+					`,
+				},
+				props: {
+					...defaultProps,
+					hideActions: true,
+					persistent: true,
+				},
+				global: {
+					plugins: [vuetify],
+				},
+			})
+
+			async function triggerTab() {
+				await modal.find(':focus').trigger('keydown', {
+					keyCode: 9,
+					key: 'Tab',
+					code: 'Tab',
+				})
+			}
+
+			async function triggerShiftTab() {
+				await modal.find(':focus').trigger('keydown', {
+					keyCode: 9,
+					key: 'Tab',
+					code: 'Tab',
+					shiftKey: true,
+				})
+			}
+
+			const modal = wrapper.getComponent(VCard)
+
+			const firstBtn = modal.find<HTMLElement>('#first')
+			const link = modal.find<HTMLElement>('#link')
+			await modal.vm.$nextTick()
+
+			firstBtn.element.focus()
+			await modal.vm.$nextTick()
+
+			await triggerShiftTab()
+			expect(link.element).toEqual(document.activeElement)
+
+			await triggerTab()
+			expect(firstBtn.element).toEqual(document.activeElement)
+		})
+
+		it('return to the first focusable element', async () => {
+			const testComponent = defineComponent({
+				components: { DialogBox },
+				setup() {
+					return {
+						dialog: true,
+
+					}
+				},
+				template: `
+				<DialogBox v-model="dialog" title="Test title" :hide-actions="true">
+					<button id="first">First</button>
+					<button id="second" disabled>Second</button>
+					<button id="third">third</button>
+					<a href="https://www.ameli.fr/" id="link">ameli.fr</a>
+				</DialogBox>
+				<button id="external">External</button>
+				`,
+			})
+			const wrapper = mount(testComponent, {
+				global: {
+					plugins: [vuetify],
+				},
+			})
+
+			const externalBtn = wrapper.find<HTMLButtonElement>('#external')
+			externalBtn.element.focus()
+			await wrapper.vm.$nextTick()
+
+			await externalBtn.trigger('keydown', {
+				keyCode: 9,
+				key: 'Tab',
+				code: 'Tab',
+			})
+
+			const modal = wrapper.getComponent(VCard)
+
+			const firstBtn = modal.find<HTMLElement>('#first')
+			expect(firstBtn.element).toEqual(document.activeElement)
+		})
+
+		it('return the last focusable element', async () => {
+			const testComponent = defineComponent({
+				components: { DialogBox },
+				setup() {
+					return {
+						dialog: true,
+					}
+				},
+				template: `
+				<button id="external">External</button>
+				<DialogBox v-model="dialog" title="Test title" :hide-actions="true">
+					<button id="first">First</button>
+					<button id="second" disabled>Second</button>
+					<button id="third">third</button>
+					<a href="https://www.ameli.fr/" id="link">ameli.fr</a>
+				</DialogBox>
+				`,
+			})
+			const wrapper = mount(testComponent, {
+				global: {
+					plugins: [vuetify],
+				},
+			})
+
+			const external = wrapper.find<HTMLElement>('#external')
+			const modal = wrapper.getComponent(VCard)
+			external.element.focus()
+			await wrapper.vm.$nextTick()
+
+			await modal.trigger('keydown', {
+				keyCode: 9,
+				key: 'Tab',
+				code: 'Tab',
+				shiftKey: true,
+			})
+
+			const link = modal.find<HTMLButtonElement>('#link')
+			expect(link.element).toEqual(document.activeElement)
+		})
+	})
+
+	describe('event emissions', () => {
+		it('emits an event when close button is clicked', async () => {
+			const wrapper = mount(DialogBox, {
+				props: {
+					modelValue: true,
+				},
+				global: {
+					plugins: [vuetify],
+				},
+			})
+
+			const modal = wrapper.getComponent(VCard)
+
+			expect(modal.isVisible()).toBe(true)
+
+			const closeBtn = modal.find('button')
+			await closeBtn.trigger('click')
+
+			expect(wrapper.emitted('update:modelValue')).toBeTruthy()
+		})
+
+		it('emits a cancel event when cancel button is clicked', async () => {
+			const wrapper = mount(DialogBox, {
+				props: defaultProps,
+				global: {
+					plugins: [vuetify],
+				},
+			})
+
+			const modal = wrapper.getComponent(VCard)
+
+			const cancelBtn = modal.find('.vd-dialog-box-actions-ctn button')
+			await cancelBtn.trigger('click')
+
+			expect(wrapper.emitted('cancel')).toBeTruthy()
+		})
+
+		it('emits a confirm event when confirm button is clicked', async () => {
+			const wrapper = mount(DialogBox, {
+				props: defaultProps,
+				global: {
+					plugins: [vuetify],
+				},
+			})
+
+			const modal = wrapper.getComponent(VCard)
+
+			const confirmBtn = modal.find('[data-test-id="confirm-btn"]')
+			await confirmBtn.trigger('click')
+
+			expect(wrapper.emitted('confirm')).toBeTruthy()
+		})
+	})
+
+	describe('Test methods', () => {
+		it('getSelectableElements if this.$refs.dialogContent.$el is undefined', async () => {
+			const wrapper = shallowMount(DialogBox, {
+				props: defaultProps,
+				global: {
+					plugins: [vuetify],
+				},
+			})
+			// @ts-expect-error - Testing private method
+			const result = await wrapper.vm.getSelectableElements()
+			expect(result).toEqual([])
+		})
+	})
+})
