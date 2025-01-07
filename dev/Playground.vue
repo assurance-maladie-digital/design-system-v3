3cc6d349f12cf3ff4af7aca573cc6bfe6d1c6868
David FYON <davidfyon@gmail.com>
2025-01-07 14:47:35 +0100

    reset Playground

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,25 +1,2 @@
-<script lang="ts" setup>
-	import ExternalLinks from '@/components/ExternalLinks/ExternalLinks.vue'
-
-	const items = [
-		{
-			text: 'ameli.fr',
-			href: 'https://www.ameli.fr',
-		},
-		{
-			text: 'Github',
-			href: 'https://www.github.com',
-		},
-		{
-			text: 'Twitter',
-			href: 'https://www.twitter.com',
-		},
-	]
-</script>
-
 <template>
-	<ExternalLinks
-		:items="items"
-		position="top left"
-	/>
 </template>
