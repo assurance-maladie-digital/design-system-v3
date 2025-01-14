35dd7baa3db5ae855b0de7e9478305713c331a63
David FYON <davidfyon@gmail.com>
2025-01-14 11:13:32 +0100

    fix stories code

--- src/components/NotificationBar/NotificationBar.stories.ts
+++ src/components/NotificationBar/NotificationBar.stories.ts
@@ -28,339 +28,107 @@ export default meta
 
 type Story = StoryFn<StoryArgs>
 
-const Template: Story = args => ({
-	components: { NotificationBar, VBtn },
-	setup() {
-		const { addNotification } = useNotificationService()
-		const showNotification = ref(false)
-		const { closeBtnText, bottom, rounded } = toRefs(args)
-		const typeRef = ref(args.type)
+export const Default: Story = {
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `
+				<div class="d-flex flex-wrap align-center justify-center">
+					<NotificationBar
+						v-model="showNotification"
+						:close-btn-text="closeBtnText"
+						:bottom="bottom"
+						:rounded="rounded"
+					>
+						<template #default>This is a {{ typeRef }} notification</template>
+					</NotificationBar>
+					<VBtn
+						color="primary"
+						@click="envoyerNotification('This is a ' + typeRef + ' notification')"
+						class="ma-6"
+					>
+						Afficher la notification
+					</VBtn>
+				</div>
+				`,
+			}, {
+				name: 'Script',
+				code: `
+				<script setup lang="ts">
+					import { VBtn } from 'vuetify/components'
+					import { NotificationBar } from '@cnamts/synapse'
+					
+					const { addNotification } = useNotificationService()
+					const showNotification = ref(false)
+					const { type: typeRef, closeBtnText } = toRefs(args)
 
-		watch(
-			() => args,
-			(newArgs) => {
-				typeRef.value = newArgs.type
-				closeBtnText.value = newArgs.closeBtnText
-				bottom.value = newArgs.bottom
-				rounded.value = newArgs.rounded
+					const envoyerNotification = (message: string) => {
+						const notification: Notification = {
+						id: Date.now().toString(),
+						message,
+						type: typeRef.value,
+						timeout: -1,
+					}
+					addNotification(notification)
+					showNotification.value = true
+				}
+				</script>
+				`,
 			},
-			{ deep: true },
-		)
-
-		const envoyerNotification = (message: string) => {
-			const notification: Notification = {
-				id: Date.now().toString(),
-				message,
-				type: typeRef.value,
-				timeout: -1,
-			}
-			addNotification(notification)
-			showNotification.value = true
-		}
-
+		],
+	},
+	args: {
+		closeBtnText: 'Fermer',
+		type: 'info',
+		bottom: false,
+		rounded: 'rounded',
+	},
+	render: (args) => {
 		return {
-			showNotification,
-			typeRef,
-			closeBtnText,
-			bottom,
-			rounded,
-			envoyerNotification,
+			components: { NotificationBar, VBtn },
+			setup() {
+				const { addNotification } = useNotificationService()
+				const showNotification = ref(false)
+				const { type: typeRef, closeBtnText } = toRefs(args)
+
+				const envoyerNotification = (message: string) => {
+					const notification: Notification = {
+						id: Date.now().toString(),
+						message,
+						type: typeRef.value,
+						timeout: -1,
+					}
+					addNotification(notification)
+					showNotification.value = true
+				}
+
+				return {
+					showNotification,
+					typeRef,
+					closeBtnText,
+					envoyerNotification,
+				}
+			},
+			template: `
+				<div class="d-flex flex-wrap align-center justify-center">
+					<NotificationBar
+						v-model="showNotification"
+						:close-btn-text="closeBtnText"
+						:bottom="bottom"
+						:rounded="rounded"
+					>
+						<template #default>This is a {{ typeRef }} notification</template>
+					</NotificationBar>
+					<VBtn
+						color="primary"
+						@click="envoyerNotification('This is a ' + typeRef + ' notification')"
+						class="ma-6"
+					>
+						Afficher la notification
+					</VBtn>
+				</div>
+			`,
 		}
-	},
-	template: `
-		<div class="d-flex flex-wrap align-center justify-center">
-			<NotificationBar
-				v-model="showNotification"
-				:close-btn-text="closeBtnText"
-				:bottom="bottom"
-				:rounded="rounded"
-			>
-				<template #default>This is a {{ typeRef }} notification</template>
-			</NotificationBar>
-			<VBtn
-				color="primary"
-				@click="envoyerNotification('This is a ' + typeRef + ' notification')"
-				class="ma-6"
-			>
-				Afficher la notification
-			</VBtn>
-		</div>
-	`,
-})
-
-export const Success = Template.bind({})
-Success.args = { type: 'success', closeBtnText: 'Fermer', bottom: false, rounded: 'lg' }
-export const Info = Template.bind({})
-Info.args = { type: 'info', closeBtnText: 'Fermer', bottom: false, rounded: 'lg' }
-export const Warning = Template.bind({})
-Warning.args = { type: 'warning', closeBtnText: 'Fermer', bottom: false, rounded: 'lg' }
-export const Error = Template.bind({})
-Error.args = { type: 'error', closeBtnText: 'Fermer', bottom: false, rounded: 'lg' }
-
-export const WithAction: Story = args => ({
-	components: { NotificationBar, VBtn },
-	setup() {
-		const { addNotification } = useNotificationService()
-		const showNotification = ref(false)
-		const { type: typeRef, closeBtnText } = toRefs(args)
-
-		const envoyerNotification = (message: string) => {
-			const notification: Notification = {
-				id: Date.now().toString(),
-				message,
-				type: typeRef.value,
-				timeout: -1,
-			}
-			addNotification(notification)
-			showNotification.value = true
-		}
-
-		return {
-			showNotification,
-			typeRef,
-			closeBtnText,
-			envoyerNotification,
-		}
-	},
-	template: `
-    <div class="d-flex flex-wrap align-center justify-center">
-      <NotificationBar
-        v-model="showNotification"
-        :close-btn-text="closeBtnText"
-      >
-        <template #default>This is a {{ typeRef }} notification</template>
-        <template #action>
-          <VBtn variant="outlined">Action</VBtn>
-        </template>
-      </NotificationBar>
-      <VBtn
-        color="primary"
-        @click="envoyerNotification('This is a ' + typeRef + ' notification')"
-        class="ma-6"
-      >
-        Afficher la notification
-      </VBtn>
-    </div>
-  `,
-})
-WithAction.args = {
-	type: 'info',
-	closeBtnText: 'Fermer',
-}
-
-export const WithTimeout: Story = args => ({
-	components: { NotificationBar, VBtn },
-	setup() {
-		const { addNotification } = useNotificationService()
-		const showNotification = ref(false)
-
-		// Ajouter une valeur par défaut si `args.type` n'est pas défini
-		const typeRef = ref(args.type || 'info')
-		const { closeBtnText } = toRefs(args)
-
-		const envoyerNotification = (message: string) => {
-			const notification: Notification = {
-				id: Date.now().toString(),
-				message,
-				type: typeRef.value,
-				timeout: 5000, // Timeout de 5 secondes
-			}
-			addNotification(notification)
-			showNotification.value = true
-		}
-
-		return {
-			showNotification,
-			typeRef, // Utilisation de `typeRef`
-			closeBtnText,
-			envoyerNotification,
-		}
-	},
-	template: `
-		<div class="d-flex flex-wrap align-center justify-center">
-			<NotificationBar
-				v-model="showNotification"
-				:close-btn-text="closeBtnText"
-			>
-				<template #default>This is a {{ typeRef }} notification</template>
-			</NotificationBar>
-			<VBtn
-				color="primary"
-				@click="envoyerNotification('This is a ' + typeRef + ' notification')"
-				class="ma-6"
-			>
-				Afficher la notification
-			</VBtn>
-		</div>
-	`,
-})
-WithTimeout.args = {
-	type: 'info', // Valeur par défaut pour `type`
-	closeBtnText: 'Fermer',
-}
-
-export const WithLongText: Story = args => ({
-	components: { NotificationBar, VBtn },
-	setup() {
-		const { addNotification } = useNotificationService()
-		const showNotification = ref(false)
-
-		// Ajouter une valeur par défaut pour `type`
-		const typeRef = ref(args.type || 'info')
-		const { closeBtnText } = toRefs(args)
-
-		const envoyerNotification = (message: string) => {
-			const notification: Notification = {
-				id: Date.now().toString(),
-				message,
-				type: typeRef.value,
-				timeout: -1, // Notification persistante
-			}
-			addNotification(notification)
-			showNotification.value = true
-		}
-
-		return {
-			showNotification,
-			typeRef, // Utilisation de `typeRef`
-			closeBtnText,
-			envoyerNotification,
-		}
-	},
-	template: `
-		<div class="d-flex flex-wrap align-center justify-center">
-			<NotificationBar
-				v-model="showNotification"
-				:close-btn-text="closeBtnText"
-			>
-				<template #default>
-					<div>
-						<p>This is a {{ typeRef }} notification</p>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-					</div>
-				</template>
-			</NotificationBar>
-			<VBtn
-				color="primary"
-				@click="envoyerNotification('This is a ' + typeRef + ' lorem ipsum dolor sit amet, consectetur adipiscing elit...')"
-				class="ma-6"
-			>
-				Afficher la notification
-			</VBtn>
-		</div>
-	`,
-})
-WithLongText.args = {
-	type: 'info',
-	closeBtnText: 'Fermer',
-}
-
-export const forMobile: Story = args => ({
-	components: { NotificationBar, VBtn },
-	setup() {
-		const { addNotification } = useNotificationService()
-		const showNotification = ref(false)
-
-		const typeRef = ref(args.type || 'info')
-		const { closeBtnText } = toRefs(args)
-
-		const envoyerNotification = (message: string) => {
-			const notification: Notification = {
-				id: Date.now().toString(),
-				message,
-				type: typeRef.value,
-				timeout: -1,
-			}
-			addNotification(notification)
-			showNotification.value = true
-		}
-
-		return {
-			showNotification,
-			typeRef,
-			closeBtnText,
-			envoyerNotification,
-		}
-	},
-	template: `
-		<div class="d-flex flex-wrap align-center justify-center">
-			<NotificationBar
-				v-model="showNotification"
-				:close-btn-text="closeBtnText"
-			>
-				<template #default>This is a {{ typeRef }} notification</template>
-			</NotificationBar>
-			<VBtn
-				color="primary"
-				@click="envoyerNotification('This is a ' + typeRef + ' notification ')"
-				class="ma-6"
-			>
-				Afficher la notification
-			</VBtn>
-		</div>
-	`,
-})
-forMobile.args = {
-	type: 'info',
-	closeBtnText: 'Fermer',
-}
-forMobile.parameters = {
-	viewport: {
-		defaultViewport: 'mobile2',
-	},
-}
-
-export const forMobileWithLongText: Story = args => ({
-	components: { NotificationBar, VBtn },
-	setup() {
-		const { addNotification } = useNotificationService()
-		const showNotification = ref(false)
-
-		const typeRef = ref(args.type || 'info')
-		const { closeBtnText } = toRefs(args)
-
-		const envoyerNotification = (message: string) => {
-			const notification: Notification = {
-				id: Date.now().toString(),
-				message,
-				type: typeRef.value,
-				timeout: -1,
-			}
-			addNotification(notification)
-			showNotification.value = true
-		}
-
-		return {
-			showNotification,
-			typeRef,
-			closeBtnText,
-			envoyerNotification,
-		}
-	},
-	template: `
-		<div class="d-flex flex-wrap align-center justify-center">
-			<NotificationBar
-				v-model="showNotification"
-				:close-btn-text="closeBtnText"
-			>
-				<template #default>This is a {{ typeRef }} notification</template>
-			</NotificationBar>
-			<VBtn
-				color="primary"
-				@click="envoyerNotification('This is a ' + typeRef + ' lorem ipsum dolor sit amet, consectetur adipiscing elit...')"
-				class="ma-6"
-			>
-				Afficher la notification
-			</VBtn>
-		</div>
-	`,
-})
-forMobileWithLongText.args = {
-	type: 'info',
-	closeBtnText: 'Fermer',
-}
-forMobileWithLongText.parameters = {
-	viewport: {
-		defaultViewport: 'mobile2',
 	},
 }
