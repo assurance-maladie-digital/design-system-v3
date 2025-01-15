74c6178367da2aed1742481e4d6dc77d2ed0137d
virginieVachet <vachetvirginie@gmail.com>
2025-01-15 13:42:02 +0100

    add page for issue in doc

--- src/stories/GuideDuDev/components.stories.ts
+++ src/stories/GuideDuDev/components.stories.ts
@@ -1,7 +1,7 @@
 import SyAlert from '@/components/SyAlert/SyAlert.vue'
 
 export default {
-	title: 'Guide Du Dev/Rapport de bug',
+	title: 'Guide Du Dev/Comment créer une issue',
 	component: SyAlert,
 }
 export const Info = {
@@ -9,14 +9,16 @@ export const Info = {
 		return {
 			components: { SyAlert },
 			setup() {
-				return { }
+				return {}
 			},
 			template: `
-				<SyAlert type="info" variant="tonal" :closable="false">
-					<template #default>Lorsque vous créez une reproduction minimale, supprimez tous les éléments, propriétés, variables, données et autres qui ne sont pas nécessaires pour reproduire le bug. Cela facilitera le traitement du rapport et le temps qu’il faudra pour identifier puis résoudre le bug.
-					</template>
-				</SyAlert>
-			`,
+              <SyAlert type="info" variant="tonal" :closable="false">
+                <template #default>Lorsque vous créez une reproduction minimale, supprimez tous les éléments,
+                  propriétés, variables, données et autres qui ne sont pas nécessaires pour reproduire le bug. Cela
+                  facilitera le traitement du rapport et le temps qu’il faudra pour identifier puis résoudre le bug.
+                </template>
+              </SyAlert>
+            `,
 		}
 	},
 	tags: ['!dev'],
