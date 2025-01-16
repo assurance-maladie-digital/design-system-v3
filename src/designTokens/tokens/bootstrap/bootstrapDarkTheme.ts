4c18801edde8cad6b68d468ffb4a4144b58e63f9
David FYON <davidfyon@gmail.com>
2025-01-16 16:30:02 +0100

    Tokens: add bootstrap colors (#345)
    * add colors
    
    * fix

---
+++ src/designTokens/tokens/bootstrap/bootstrapDarkTheme.ts
@@ -0,0 +1,7 @@
+// import { bootstrapColorsTokens } from './bootstrapColors'
+import { cnamColorsTokens } from '../cnam/cnamColors'
+
+// Pour l'utiilser cf : dev/main.ts -> a ajouter a la place des cnamColorsTokens
+export const bootstrapLightTheme = {
+	backgroundSurface: cnamColorsTokens.parma.darken60,
+}
