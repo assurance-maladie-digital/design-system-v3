1acb66e536104aeef626e2e28b2980979d657a81
adrien charrier <adrien.charrier@outlook.fr>
2025-01-22 16:49:03 +0100

    refactor: remove unused SySlider components and related tests

--- src/components/Customs/SySlider/types.ts
+++
@@ -1,9 +0,0 @@
-import { type MaybeRef } from 'vue'
-
-export interface Range {
-	selectedMin: MaybeRef<number>
-	selectedMax: MaybeRef<number>
-	rangeMin: MaybeRef<number>
-	rangeMax: MaybeRef<number>
-	step: MaybeRef<number>
-}
