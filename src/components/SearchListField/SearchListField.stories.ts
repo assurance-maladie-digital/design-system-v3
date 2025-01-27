f63021bb1a5e37bfe5896bca20a3bf6d32cc912b
David FYON <davidfyon@gmail.com>
2025-01-27 09:58:20 +0100

    SearchListField (#351)
    * add component + tests + stories
    
    * tests
    
    * improve Tanaguru
    
    * fix tanaguru
    
    * fix tests + clean playground
    
    * fix build
    
    * fix lint style

---
+++ src/components/SearchListField/SearchListField.stories.ts
@@ -0,0 +1,126 @@
+import type { StoryObj, Meta } from '@storybook/vue3'
+import SearchListField from './SearchListField.vue'
+
+const meta = {
+	title: 'Composants/Formulaires/SearchListField',
+	component: SearchListField,
+	parameters: {
+		layout: 'fullscreen',
+		controls: { exclude: ['filteredItems', 'search', 'emitChangeEvent'] },
+	},
+	decorators: [
+		() => ({
+			template: '<div style="padding: 20px;"><story/></div>',
+		}),
+	],
+	argTypes: {
+		modelValue: {
+			default: '[]',
+		},
+		items: { control: { type: 'object' } },
+	},
+} satisfies Meta<typeof SearchListField>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+/**
+ * Story par défaut
+ */
+export const Default: Story = {
+	args: {
+		modelValue: [],
+		items: [
+			{
+				label: 'Chirurgien-dentiste',
+				value: 'chirurgien-dentiste',
+			},
+			{
+				label: 'Infirmier',
+				value: 'infirmier',
+			},
+			{
+				label: 'Orthophoniste',
+				value: 'orthophoniste',
+			},
+			{
+				label: 'Orthoptiste',
+				value: 'orthoptiste',
+			},
+			{
+				label: 'Pédicure-podologue',
+				value: 'pedicure-podologue',
+			},
+			{
+				label: 'Pharmacien',
+				value: 'pharmacien',
+			},
+		],
+	},
+	render: (args) => {
+		return {
+			components: { SearchListField },
+			setup() {
+				return { args }
+			},
+			template: `
+                <SearchListField v-bind="args" v-model="args.modelValue"/>
+            `,
+		}
+	},
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `
+<template>
+  <SearchListField
+   	v-model="modelValue"
+	:items="items"
+  />
+  {{ modelValue }}
+</template>
+        `,
+			},
+			{
+				name: 'Script',
+				code: `
+<script setup lang="ts">
+import { ref } from 'vue'
+import SearchListField from '@cnamts/synapse'
+
+const modelValue = ref('')
+
+const items = [
+  {
+	label: 'Chirurgien-dentiste',
+	value: 'chirurgien-dentiste',
+  },
+  {
+	label: 'Infirmier',
+	value: 'infirmier',
+  },
+  {
+	label: 'Orthophoniste',
+	value: 'orthophoniste',
+  },
+  {
+	label: 'Orthoptiste',
+	value: 'orthoptiste',
+  },
+  {
+	label: 'Pédicure-podologue',
+	value: 'pedicure-podologue',
+  },
+  {
+	label: 'Pharmacien',
+	value: 'pharmacien',
+  },
+]
+</script>
+        `,
+			},
+		],
+	},
+}
---
+++ src/components/SearchListField/SearchListField.vue
@@ -0,0 +1,194 @@
+<script lang="ts" setup>
+	import { ref, computed, watch } from 'vue'
+	import { mdiMagnify } from '@mdi/js'
+	import type { PropType } from 'vue'
+	import type { SearchListItem } from './types'
+	import { locales } from './locales'
+
+	import { SyTextField } from '@/components'
+
+	const props = defineProps({
+		modelValue: {
+			type: Array as PropType<unknown[]>,
+			default: () => [],
+		},
+		items: {
+			type: Array as PropType<SearchListItem[]>,
+			default: () => [],
+		},
+		outlined: {
+			type: Boolean,
+			default: true,
+		},
+	})
+
+	const emit = defineEmits(['update:modelValue'])
+
+	const search = ref<string | null>(null)
+	const internalValue = ref<unknown[]>(props.modelValue)
+	const searchIcon = mdiMagnify
+
+	watch(
+		() => props.modelValue,
+		(newValue) => {
+			internalValue.value = newValue
+		},
+	)
+
+	const filteredItems = computed(() => {
+		if (search.value === null) {
+			return props.items
+		}
+		const searchLower = search.value.toLowerCase()
+		return props.items.filter(item =>
+			item.label.toLowerCase().includes(searchLower),
+		)
+	})
+
+	const emitChangeEvent = (value: unknown[]) => {
+		emit('update:modelValue', value)
+	}
+
+	defineExpose({
+		filteredItems,
+		search,
+		emitChangeEvent,
+	})
+</script>
+
+<template>
+	<div class="vd-search-list">
+		<SyTextField
+			v-model="search"
+			:label="locales.search"
+			aria-describedby="search-description"
+			aria-labelledby="search-label"
+			hide-details
+			color="primary"
+			:variant="outlined ? 'outlined' : 'underlined'"
+			clearable
+			tabindex="0"
+		>
+			<template #prepend-inner>
+				<VIcon class="mr-1">
+					{{ searchIcon }}
+				</VIcon>
+			</template>
+		</SyTextField>
+
+		<!-- eslint-disable-next-line  vuejs-accessibility/label-has-for -->
+		<label
+			id="search-label"
+			class="d-sr-only"
+		>
+			{{ locales.search }}
+		</label>
+		<span
+			id="search-description"
+			class="d-sr-only"
+		>
+			{{ locales.search }}
+		</span>
+
+		<VList
+			id="search-list"
+			v-model:selected="internalValue"
+			title="search-list"
+			select-strategy="classic"
+			class="pb-0"
+			role="listbox"
+			aria-labelledby="search-list-title"
+			@update:selected="emitChangeEvent"
+		>
+			<h2
+				id="search-list-title"
+				class="d-sr-only"
+			>
+				{{ locales.searchListTitle }}
+			</h2>
+			<VListItem
+				v-for="(item, index) in filteredItems"
+				:id="`search-list-item-${index}`"
+				:key="index"
+				:value="item.value"
+				role="option"
+				:aria-selected="internalValue.includes(item.value)"
+				:aria-labelledby="`search-list-item-${index}`"
+				:tabindex="0"
+				active-class="text-primary"
+				class="d-flex align-center justify-start mx-4"
+			>
+				<span
+					:id="`search-list-item-label-${index}`"
+					class="d-sr-only"
+				>
+					{{ item.label }}
+				</span>
+				<template #prepend="{ isActive }">
+					<VListItemAction start>
+						<input
+							:id="`checkbox-${index}`"
+							type="checkbox"
+							role="option"
+							:checked="isActive"
+							:aria-selected="isActive"
+							:aria-label="`${locales.checkboxLabel} ${item.label}`"
+							:aria-labelledby="`checkbox-${index}`"
+							:title="`${locales.checkboxLabel} ${item.label}`"
+							class="custom-checkbox ml-2"
+						>
+						<!-- eslint-disable-next-line  vuejs-accessibility/label-has-for -->
+						<label
+							:for="`checkbox-${index}`"
+							class="d-sr-only"
+						>
+							{{ locales.checkboxLabel }}
+						</label>
+					</VListItemAction>
+
+					<VListItemTitle>{{ item.label }}</VListItemTitle>
+				</template>
+			</VListItem>
+		</VList>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/tokens';
+
+.v-list {
+	background: transparent;
+}
+
+.vd-search-list .v-list-item--active::before {
+	opacity: 0;
+}
+
+.custom-checkbox {
+	appearance: none;
+	width: 20px;
+	height: 20px;
+	border: 2px solid rgb(0 0 0 / 50%);
+	border-radius: 2px;
+	outline: none;
+	cursor: pointer;
+	transition: all 0.3s ease;
+}
+
+.custom-checkbox:checked {
+	background-color: tokens.$primary-base !important;
+	border-color: tokens.$primary-base !important;
+
+	&::before {
+		content: '\2713';
+		display: block;
+		text-align: center;
+		line-height: 15px;
+		color: #fff;
+	}
+}
+
+.custom-checkbox:hover {
+	border-color: tokens.$primary-darker !important;
+}
+</style>
