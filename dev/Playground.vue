48564d4ac8508365ef7bfdee3cb1015eae2bef32
verushkarossi <verushkarossi@gmail.com>
2025-01-27 15:47:36 +0100

    debut dev

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,2 +1,25 @@
+<script lang="ts" setup>
+	import ExternalLinks from '@/components/ExternalLinks/ExternalLinks.vue'
+
+	const items = [
+		{
+			text: 'ameli.fr',
+			href: 'https://www.ameli.fr',
+		},
+		{
+			text: 'Github',
+			href: 'https://www.github.com',
+		},
+		{
+			text: 'Twitter',
+			href: 'https://www.twitter.com',
+		},
+	]
+</script>
+
 <template>
+	<ExternalLinks
+		position="top left"
+		:items="items"
+	/>
 </template>
