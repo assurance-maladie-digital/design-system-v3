7446ce832b947feab8e80f710e0d00459fe3fec6
verushkarossi <verushkarossi@gmail.com>
2025-01-29 10:49:33 +0100

    accessibilite ok pr package 4

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,14 +1,2 @@
-<script lang="ts" setup>
-	import PasswordField from '@/components/PasswordField/PasswordField.vue'
-	const customRules = []
-
-</script>
-
 <template>
-	<PasswordField
-		:custom-rules="customRules"
-		is-validate-on-blur
-		outlined
-		:required="false"
-	/>
 </template>
