717907a49c28f287d6570c76fe14ea47df3943b3
David FYON <davidfyon@gmail.com>
2025-02-03 14:19:55 +0100

    fix imports

--- src/designTokens/index.ts
+++ src/designTokens/index.ts
@@ -3,9 +3,9 @@ import { cnamContextualTokens } from './tokens/cnam/cnamContextual'
 import { cnamColorsTokens } from './tokens/cnam/cnamColors'
 import { cnamLightTheme } from './tokens/cnam/cnamLightTheme'
 import { cnamDarkTheme } from './tokens/cnam/cnamDarkTheme'
-import { paColorsTokens } from '@/designTokens/tokens/pa/paColors'
-import { paLightTheme } from '@/designTokens/tokens/pa/paLightTheme'
-import { paDarkTheme } from '@/designTokens/tokens/pa/paDarkTheme'
+import { paColorsTokens } from './tokens/pa/paColors'
+import { paLightTheme } from './tokens/pa/paLightTheme'
+import { paDarkTheme } from './tokens/pa/paDarkTheme'
 
 export {
 	cnamColorsTokens,
