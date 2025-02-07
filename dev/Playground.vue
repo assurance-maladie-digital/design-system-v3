2e0918bba2395c8757f8b487258a9c22bd8dc1b7
David FYON <davidfyon@gmail.com>
2025-02-07 10:24:37 +0100

    add click event

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,2 +1,28 @@
+<script setup lang="ts">
+	import { SyAlert } from '@/components'
+	import { ref } from 'vue'
+
+	const showAlert = ref(true)
+</script>
+
 <template>
+	<SyAlert
+		v-model="showAlert"
+		type="success"
+		variant="tonal"
+		closable
+		@click:close="console.log('close')"
+	>
+		<template #default>
+			This is a success alert
+		</template>
+	</SyAlert>
+
+	<VBtn
+		v-if="!showAlert"
+		color="primary"
+		@click="showAlert = true"
+	>
+		Réinitialiser
+	</VBtn>
 </template>
--- src/components/SyAlert/SyAlert.vue
+++ src/components/SyAlert/SyAlert.vue
@@ -50,12 +50,12 @@
 <template>
 	<VAlert
 		v-model="show"
-		:type
-		:closable
-		:variant
-		:class="`alert alert--${type}`"
-		:color="type"
-		:border="variant === 'tonal' ? 'start' : false"
+		:type="props.type"
+		:closable="props.closable"
+		:variant="props.variant"
+		:class="`alert alert--${props.type}`"
+		:color="props.type"
+		:border="props.variant === 'tonal' ? 'start' : false"
 	>
 		<template #prepend>
 			<VIcon
@@ -73,11 +73,11 @@
 		</template>
 
 		<template
-			v-if="closable"
+			v-if="props.closable"
 			#close
 		>
 			<VBtn
-				:color="variant === 'outlined' ? undefined : 'primary'"
+				:color="props.variant === 'outlined' ? undefined : 'primary'"
 				:ripple="false"
 				variant="text"
 				width="auto"
@@ -104,157 +104,156 @@
 @use 'sass:map';
 
 .alert {
-	padding: tokens.$padding-4;
+  padding: tokens.$padding-4;
 }
 
 .alert-icon {
-	border-radius: 50%;
+  border-radius: 50%;
 }
 
 :deep(.v-alert__underlay) {
-	display: none;
+  display: none;
 }
 
 :deep(.v-alert__close) {
-	align-self: center;
+  align-self: center;
 }
 
 :deep(.v-btn--variant-text .v-btn__overlay) {
-	background: transparent !important;
+  background: transparent !important;
 }
 
 .alert-icon :deep(.v-icon__svg) {
-	height: 1.5rem !important;
+  height: 1.5rem !important;
 }
 
 .alert-close-btn {
-	cursor: pointer;
-	line-height: 0;
+  cursor: pointer;
+  line-height: 0;
 
-	.v-btn__overlay {
-		display: none;
-	}
+  .v-btn__overlay {
+    display: none;
+  }
 }
 
 .v-btn {
-	text-transform: none;
-	font-weight: bold;
-	font-size: 0.75rem;
-	letter-spacing: normal;
+  text-transform: none;
+  font-weight: bold;
+  font-size: 0.75rem;
+  letter-spacing: normal;
 }
 
 @media screen and (width <= 440px) {
-	.alert {
-		display: flex;
-		flex-direction: column;
-		background-color: tokens.$white-base;
+  .alert {
+    display: flex;
+    flex-direction: column;
+    background-color: tokens.$white-base;
 
-		:deep(.v-alert__content) {
-			align-self: flex-start !important;
-			margin-top: tokens.$padding-6;
-		}
+    :deep(.v-alert__content) {
+      align-self: flex-start !important;
+      margin-top: tokens.$padding-6;
+    }
 
-		:deep(.v-alert__close) {
-			margin-top: tokens.$padding-4;
-			align-self: flex-end;
-		}
+    :deep(.v-alert__close) {
+      margin-top: tokens.$padding-4;
+      align-self: flex-end;
+    }
 
-		.v-alert__prepend > .v-icon {
-			background: none !important;
-		}
-	}
+    .v-alert__prepend > .v-icon {
+      background: none !important;
+    }
+  }
 }
 
 @media screen and (width >= 441px) {
-	.alert {
-		.alert-icon {
-			width: 3.5rem !important;
-			height: 3.5rem !important;
-			display: grid;
-			place-items: center;
-		}
-	}
+  .alert {
+    .alert-icon {
+      width: 3.5rem !important;
+      height: 3.5rem !important;
+      display: grid;
+      place-items: center;
+    }
+  }
 }
 
 @mixin redesign($type, $map) {
-	&.alert--#{$type}.v-alert--variant-tonal {
-		background: map.get($map, 'background') !important;
-		color: tokens.$colors-text-base !important;
+  &.alert--#{$type}.v-alert--variant-tonal {
+    background: map.get($map, 'background') !important;
+    color: tokens.$colors-text-base !important;
 
-		:deep(.v-alert__border) {
-			border-color: map.get($map, 'border') !important;
-			opacity: 1 !important;
-			border-inline-start-width: 4px;
-		}
-	}
+    :deep(.v-alert__border) {
+      border-color: map.get($map, 'border') !important;
+      opacity: 1 !important;
+      border-inline-start-width: 4px;
+    }
+  }
 
-	&.alert--#{$type}.v-alert--variant-outlined {
-		background: transparent !important;
+  &.alert--#{$type}.v-alert--variant-outlined {
+    background: transparent !important;
 
-		:deep(.v-alert__border) {
-			border-color: map.get($map, 'border') !important;
-			opacity: 1 !important;
-		}
-	}
+    :deep(.v-alert__border) {
+      border-color: map.get($map, 'border') !important;
+      opacity: 1 !important;
+    }
+  }
 
-	&.alert--#{$type},
-	&.text-#{$type} {
-		.v-alert__prepend > .v-icon {
-			background: map.get($map, 'icon-bg');
+  &.alert--#{$type},
+  &.text-#{$type} {
+    .v-alert__prepend > .v-icon {
+      background: map.get($map, 'icon-bg');
 
-			:deep(svg) {
-				fill: map.get($map, 'accent');
-			}
-		}
-	}
+      :deep(svg) {
+        fill: map.get($map, 'accent');
+      }
+    }
+  }
 
-	&.text-#{$type} {
-		color: map.get($map, 'accent') !important;
-		border-color: map.get($map, 'accent') !important;
-	}
+  &.text-#{$type} {
+    color: map.get($map, 'accent') !important;
+    border-color: map.get($map, 'accent') !important;
+  }
 }
 
 .v-alert {
-	@include redesign(
-		'warning',
-		(
-			'background': tokens.$colors-background-warning,
-			'accent': tokens.$colors-border-warning,
-			'border': tokens.$colors-border-warning,
-			'icon':tokens.$colors-icon-warning,
-			'icon-bg': tokens.$colors-background-warning-subdued,
-		)
-	);
-	@include redesign(
-		'success',
-		(
-			'background': tokens.$colors-background-success,
-			'accent': tokens.$colors-border-success,
-			'border': tokens.$colors-border-success,
-			'icon':tokens.$colors-icon-success,
-			'icon-bg': tokens.$colors-background-success-subdued,
-		)
-	);
-	@include redesign(
-		'error',
-		(
-			'background': tokens.$colors-background-error,
-			'accent': tokens.$colors-border-error,
-			'border': tokens.$colors-border-error,
-			'icon':tokens.$colors-icon-error,
-			'icon-bg': tokens.$colors-background-error-subdued,
-		)
-	);
-	@include redesign(
-		'info',
-		(
-			'background': tokens.$colors-background-info,
-			'accent': tokens.$colors-border-info,
-			'border': tokens.$colors-border-info,
-			'icon':tokens.$colors-icon-info,
-			'icon-bg': tokens.$colors-background-info-subdued,
-		)
-	);
+  @include redesign(
+          'warning',
+          (
+              'background': tokens.$colors-background-warning,
+              'accent': tokens.$colors-border-warning,
+              'border': tokens.$colors-border-warning,
+              'icon':tokens.$colors-icon-warning,
+              'icon-bg': tokens.$colors-background-warning-subdued,
+          )
+  );
+  @include redesign(
+          'success',
+          (
+              'background': tokens.$colors-background-success,
+              'accent': tokens.$colors-border-success,
+              'border': tokens.$colors-border-success,
+              'icon':tokens.$colors-icon-success,
+              'icon-bg': tokens.$colors-background-success-subdued,
+          )
+  );
+  @include redesign(
+          'error',
+          (
+              'background': tokens.$colors-background-error,
+              'accent': tokens.$colors-border-error,
+              'border': tokens.$colors-border-error,
+              'icon':tokens.$colors-icon-error,
+              'icon-bg': tokens.$colors-background-error-subdued,
+          )
+  );
+  @include redesign(
+          'info',
+          (
+              'background': tokens.$colors-background-info,
+              'accent': tokens.$colors-border-info,
+              'border': tokens.$colors-border-info,
+              'icon':tokens.$colors-icon-info,
+              'icon-bg': tokens.$colors-background-info-subdued,
+          )
+  );
 }
-
 </style>
