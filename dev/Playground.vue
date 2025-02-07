29ebc91f77a9b6e0bab1939915b7d9e01baa44dd
David FYON <davidfyon@gmail.com>
2025-02-07 10:05:55 +0100

    reset playground

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,15 +1,2 @@
-<script setup lang="ts">
-	import { SyAlert } from '@/components'
-</script>
-
 <template>
-	<SyAlert
-		closable
-		model-value
-		type="success"
-		variant="tonal"
-		@click:close="console.log('close')"
-	>
-		Alert content
-	</SyAlert>
 </template>
