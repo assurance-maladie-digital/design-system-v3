ae60080eae2c3a1bd046d1b75642f6cf850bf714
David FYON <davidfyon@gmail.com>
2025-02-17 14:28:27 +0100

    fix doc

--- src/components/DatePicker/DatePicker.stories.ts
+++ src/components/DatePicker/DatePicker.stories.ts
@@ -12,7 +12,7 @@ const meta = {
 	],
 	parameters: {
 		layout: 'fullscreen',
-		controls: { exclude: ['modelValue'] },
+		controls: { exclude: ['modelValue', 'noCalendar'] },
 	},
 	argTypes: {
 		modelValue: {
