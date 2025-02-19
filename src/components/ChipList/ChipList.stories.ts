d2bcbebfd0688756808d5a643369ee728e1d684d
verushkarossi <verushkarossi@gmail.com>
2025-02-19 15:20:49 +0100

    composant chip list ok + accessibilite

--- src/components/ChipList/ChipList.stories.ts
+++ src/components/ChipList/ChipList.stories.ts
@@ -19,7 +19,7 @@ const meta = {
 			default: [],
 		},
 		overflowLimit: {
-			control: { type: 'number' },
+			control: { type: 'range' },
 			default: 4,
 		},
 	},
@@ -51,7 +51,99 @@ export const Default: Story = {
 		{
 			text: 'Email',
 			value: 'email',
-			state: 'info',
+			state: '',
+		},
+		{
+			text: 'Courrier',
+			value: 'courrier',
+			state: '',
+
+		},
+		{
+			text: 'SMS',
+			value: 'sms',
+			state: '',
+		},
+
+		{
+			text: 'Telephone',
+			value: 'telephone',
+			state: '',
+		},
+
+	]
+    
+</script>
+                `,
+			},
+		],
+	},
+	args: {
+		items: [
+			{
+				text: 'Email',
+				value: 'email',
+				state: '',
+			},
+			{
+				text: 'Courrier',
+				value: 'courrier',
+				state: '',
+
+			},
+			{
+				text: 'SMS',
+				value: 'sms',
+				state: '',
+			},
+
+			{
+				text: 'Telephone',
+				value: 'telephone',
+				state: '',
+			},
+		],
+		overflowLimit: 5,
+	},
+	render: (args) => {
+		return {
+			components: { ChipList },
+			setup() {
+				return { args }
+			},
+			template: `
+			<ChipList
+		:items="args.items"
+		:overflow-limit="args.overflowLimit"
+	/>
+            `,
+		}
+	},
+}
+
+export const Success: Story = {
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `
+				<template>
+	<ChipList
+		:items="args.items"
+		:overflow-limit="args.overflowLimit"
+	/>
+</template>
+                `,
+			},
+			{
+				name: 'Script',
+				code: `<script setup lang="ts">
+   import ChipList from '@/components/ChipList/ChipList.vue'
+	const items = [
+		{
+			text: 'Email',
+			value: 'email',
+			state: 'success',
 		},
 		{
 			text: 'Courrier',
@@ -62,7 +154,191 @@ export const Default: Story = {
 		{
 			text: 'SMS',
 			value: 'sms',
-			state: 'error',
+			state: 'success',
+		},
+
+		{
+			text: 'Telephone',
+			value: 'telephone',
+			state: 'success',
+		},
+
+	]
+    
+</script>
+                `,
+			},
+		],
+	},
+	args: {
+		items: [
+			{
+				text: 'Email',
+				value: 'email',
+				state: 'success',
+			},
+			{
+				text: 'Courrier',
+				value: 'courrier',
+				state: 'success',
+
+			},
+			{
+				text: 'SMS',
+				value: 'sms',
+				state: 'success',
+			},
+
+			{
+				text: 'Telephone',
+				value: 'telephone',
+				state: 'success',
+			},
+		],
+		overflowLimit: 5,
+	},
+	render: (args) => {
+		return {
+			components: { ChipList },
+			setup() {
+				return { args }
+			},
+			template: `
+			<ChipList
+		:items="args.items"
+		:overflow-limit="args.overflowLimit"
+	/>
+            `,
+		}
+	},
+}
+
+export const Info: Story = {
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `
+				<template>
+	<ChipList
+		:items="args.items"
+		:overflow-limit="args.overflowLimit"
+	/>
+</template>
+                `,
+			},
+			{
+				name: 'Script',
+				code: `<script setup lang="ts">
+   import ChipList from '@/components/ChipList/ChipList.vue'
+	const items = [
+		{
+			text: 'Email',
+			value: 'email',
+			state: 'info',
+		},
+		{
+			text: 'Courrier',
+			value: 'courrier',
+			state: 'info',
+
+		},
+		{
+			text: 'SMS',
+			value: 'sms',
+			state: 'info',
+		},
+
+		{
+			text: 'Telephone',
+			value: 'telephone',
+			state: 'info',
+		},
+
+	]
+    
+</script>
+                `,
+			},
+		],
+	},
+	args: {
+		items: [
+			{
+				text: 'Email',
+				value: 'email',
+				state: 'info',
+			},
+			{
+				text: 'Courrier',
+				value: 'courrier',
+				state: 'info',
+
+			},
+			{
+				text: 'SMS',
+				value: 'sms',
+				state: 'info',
+			},
+
+			{
+				text: 'Telephone',
+				value: 'telephone',
+				state: 'info',
+			},
+		],
+		overflowLimit: 5,
+	},
+	render: (args) => {
+		return {
+			components: { ChipList },
+			setup() {
+				return { args }
+			},
+			template: `
+			<ChipList
+		:items="args.items"
+		:overflow-limit="args.overflowLimit"
+	/>
+            `,
+		}
+	},
+}
+
+export const Warning: Story = {
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `
+				<template>
+	<ChipList
+		:items="args.items"
+		:overflow-limit="args.overflowLimit"
+	/>
+</template>
+                `,
+			},
+			{
+				name: 'Script',
+				code: `<script setup lang="ts">
+   import ChipList from '@/components/ChipList/ChipList.vue'
+	const items = [
+		{
+			text: 'Email',
+			value: 'email',
+			state: 'warning',
+		},
+		{
+			text: 'Courrier',
+			value: 'courrier',
+			state: 'warning',
+
+		},
+		{
+			text: 'SMS',
+			value: 'sms',
+			state: 'warning',
 		},
 
 		{
@@ -83,12 +359,104 @@ export const Default: Story = {
 			{
 				text: 'Email',
 				value: 'email',
-				state: 'info',
+				state: 'warning',
 			},
 			{
 				text: 'Courrier',
 				value: 'courrier',
-				state: 'success',
+				state: 'warning',
+
+			},
+			{
+				text: 'SMS',
+				value: 'sms',
+				state: 'warning',
+			},
+
+			{
+				text: 'Telephone',
+				value: 'telephone',
+				state: 'warning',
+			},
+		],
+		overflowLimit: 5,
+	},
+	render: (args) => {
+		return {
+			components: { ChipList },
+			setup() {
+				return { args }
+			},
+			template: `
+			<ChipList
+		:items="args.items"
+		:overflow-limit="args.overflowLimit"
+	/>
+            `,
+		}
+	},
+}
+
+export const Error: Story = {
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `
+				<template>
+	<ChipList
+		:items="args.items"
+		:overflow-limit="args.overflowLimit"
+	/>
+</template>
+                `,
+			},
+			{
+				name: 'Script',
+				code: `<script setup lang="ts">
+   import ChipList from '@/components/ChipList/ChipList.vue'
+	const items = [
+		{
+			text: 'Email',
+			value: 'email',
+			state: 'error',
+		},
+		{
+			text: 'Courrier',
+			value: 'courrier',
+			state: 'error',
+
+		},
+		{
+			text: 'SMS',
+			value: 'sms',
+			state: 'error',
+		},
+
+		{
+			text: 'Telephone',
+			value: 'telephone',
+			state: 'error',
+		},
+
+	]
+    
+</script>
+                `,
+			},
+		],
+	},
+	args: {
+		items: [
+			{
+				text: 'Email',
+				value: 'email',
+				state: 'error',
+			},
+			{
+				text: 'Courrier',
+				value: 'courrier',
+				state: 'error',
 
 			},
 			{
@@ -100,10 +468,10 @@ export const Default: Story = {
 			{
 				text: 'Telephone',
 				value: 'telephone',
-				state: 'warning',
+				state: 'error',
 			},
 		],
-		overflowLimit: 4,
+		overflowLimit: 5,
 	},
 	render: (args) => {
 		return {
