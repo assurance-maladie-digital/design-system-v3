5c52b1c56098094d107883769b5972566b05c0c4
verushkarossi <39561014+verushkarossi@users.noreply.github.com>
2025-02-24 17:30:15 +0100

    Feat/chip list (#394)
    * demarrage composant
    
    * composant chip list ok + accessibilite
    
    * composant chip list ok + accessibilite
    
    * wip fix
    
    * wip fix
    
    * wip fix
    
    * stles
    
    * fix tests
    
    * fix mdx
    
    * fix some stories
    
    * add options + fix display
    
    * add customization story
    
    * fix vuetifyOptions
    
    * fix vuetifyOptions
    
    * fix stories
    
    ---------
    
    Co-authored-by: virginieVachet <vachetvirginie@gmail.com>
    Co-authored-by: David FYON <davidfyon@gmail.com>

---
+++ src/components/ChipList/config.ts
@@ -0,0 +1,27 @@
+type VuetifyVariant = 'flat' | 'text' | 'elevated' | 'tonal' | 'outlined' | 'plain'
+
+type PropsList = {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any -- mock Axios headers
+	[key: string]: any
+}
+
+interface ComponentConfig {
+	[key: string]: PropsList
+}
+
+export const config: ComponentConfig = {
+	chip: {
+		color: 'primary',
+		size: 'small',
+		variant: 'flat' as VuetifyVariant,
+		class: 'ma-1',
+	},
+	btn: {
+		size: 'small',
+		density: 'compact',
+		variant: 'text' as VuetifyVariant,
+	},
+	icon: {
+		size: 'medium',
+	},
+}
