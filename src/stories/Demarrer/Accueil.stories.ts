94f81ed87030ffd8b348f9dcc64a1828c8fe345f
David FYON <davidfyon@gmail.com>
2025-02-26 14:44:27 +0100

    add homepage

--- src/stories/Demarrer/Accueil.stories.ts
+++ src/stories/Demarrer/Accueil.stories.ts
@@ -1,7 +1,7 @@
-import { VBtn, VIcon, VRow, VCol } from 'vuetify/components'
+import { VBtn, VIcon, VRow, VCol, VCard, VCardText, VCardTitle } from 'vuetify/components'
 import type { StoryObj } from '@storybook/vue3'
 
-import { mdiGithub } from '@mdi/js'
+import { mdiGithub, mdiArrowRight } from '@mdi/js'
 
 import { version } from '../../../package.json'
 
@@ -12,29 +12,139 @@ export default {
 export const Header: StoryObj = {
 	render: () => {
 		return {
-			components: { VBtn, VIcon },
+			components: { VBtn, VIcon, VCol, VRow },
 			setup() {
 				return {
+					arrowRight: mdiArrowRight,
 					githubIcon: mdiGithub,
 					version,
 				}
 			},
 			template: `
-              <div class="d-flex justify-space-between align-center">
-                  <h1 class="title font-weight-medium">Un Design System<br/>pour l’Assurance Maladie</h1>
-                  <VBtn
-                      :icon="githubIcon"
-                      aria-label="GitHub"
-                      href="https://github.com/assurance-maladie-digital/design-system-v3"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      class="d-xs-none d-sm-flex"
-                  />
+              <div>
+				  <span style="position: absolute; top:0; left:-1000px; background-color: rgba(12, 65, 154, 0.2); width: 1000%; height: 100%; z-index: 0" />
+				  <VRow cols="12" style="z-index: 1; position: relative">
+					  <VCol md="6" sm="12">
+						  <h1 class="font-weight-bold text-h4 mb-2">Synapse</h1>
+						  <img alt="line" src="/.storybook/public/home-line.svg" width="120" height="5" class="mb-5" />
+						  <h2 class="text-h4 mb-5">Documentation d'aide<br/> à la fabrication des IHM<br/>de la CNAM</h2>
+						  <VBtn color="primary" href="/?path=/docs/d%C3%A9marrer-introduction--docs">
+							  Démarrer
+							  <VIcon size="small" class="ml-2" right>{{arrowRight}}</VIcon>
+						  </VBtn>
+					  </VCol>
+					  <VCol md="6" sm="12" class="d-flex justify-center">
+						  <img alt="line" src="/.storybook/public/home-illustration.svg" width="auto" height="300"  />
+					  </VCol>
+				  </VRow>
+				  <div class="position-absolute right-0" style="bottom: -10px">
+					  <VBtn
+						  aria-label="GitHub"
+						  href="https://github.com/assurance-maladie-digital/design-system-v3"
+						  target="_blank"
+						  rel="noopener noreferrer"
+						  class="d-flex align-center justify-center text-lowercase"
+					  >
+						  <b>v{{ version }}</b>
+						  <VIcon size="large" class="ml-2">{{ githubIcon }}</VIcon>
+					  </VBtn>
+				  </div>
               </div>
-              <p class="mt-4 mb-8">
-                <b>v{{ version }}</b>
-              </p>
             `,
+		}
+	},
+	tags: ['!dev'],
+}
+
+export const Welcome: StoryObj = {
+	render: () => {
+		return {
+			template: `
+              <div class="mt-12 mb-12">
+                  <p>Bienvenue dans notre guide d'aide à la conception, pensé par et pour les équipes de la CNAM.</p>
+                  <p class="mt-4">Ce guide a été conçu pour accompagner chaque membre de l'équipe dans la création de plateformes web de qualité, alignées avec nos standards et nos valeurs.</p>
+                  <p class="mt-4">Vous y trouverez toutes les ressources essentielles pour vous guider tout au long du processus de conception :</p>
+              </div>
+            `,
+		}
+	},
+	tags: ['!dev'],
+}
+
+export const DesignSystem: StoryObj = {
+	render: () => {
+		return {
+			components: { VRow, VCol, VCard, VCardTitle, VCardText },
+			template: `
+              <div class="mb-12">
+                  <h3 class="text-h5 font-weight-bold mb-6 text-primary">Design System</h3>
+                  <VRow>
+                      <VCol cols="12" md="4">
+                          <VCard class="pa-0 card-hover" elevation="0" href="/?path=/docs/fondamentaux-introduction--docs">
+							  <img src="/.storybook/public/home-card-tokens.svg" alt="Design Tokens" class="w-100" />
+							  <VCardTitle><b>Design Tokens</b></VCardTitle>
+                              <VCardText>Les Design Tokens fournissent des recommandations claires et des bonnes pratiques pour assurer l'uniformité et la qualité des interfaces numériques.</VCardText>
+                          </VCard>
+                      </VCol>
+                      <VCol cols="12" md="4">
+						  <VCard class="pa-0 card-hover" elevation="0" href="/?path=/docs/composants-structure-headerbar--docs">
+							  <img src="/.storybook/public/home-card-components.svg" alt="Components" class="w-100" />
+                              <VCardTitle><b>Composants</b></VCardTitle>
+                              <VCardText>Les Composants assurent une expérience utilisateur homogène en offrant des éléments réutilisables et standardisés.</VCardText>
+                          </VCard>
+                      </VCol>
+                      <VCol cols="12" md="4">
+						  <VCard class="pa-0 card-hover" elevation="0" href="/?path=/docs/templates-errorpage--docs">
+							  <img src="/.storybook/public/home-card-templates.svg" alt="Templates" class="w-100" />
+                              <VCardTitle><b>Templates</b></VCardTitle>
+                              <VCardText>Les Templates facilitent la mise en page en proposant des structures prédéfinies garantissant cohérence et efficacité dans la conception des interfaces.</VCardText>
+                          </VCard>
+                      </VCol>
+                      <VCol cols="12" md="4">
+						  <VCard class="pa-0 card-hover" elevation="0" href="/?path=/docs/guide-du-dev-cr%C3%A9er-une-issue--creeruneissue">
+							  <img src="/.storybook/public/home-card-dev.svg" alt="Guide du dev" class="w-100" />
+                              <VCardTitle><b>Guides du dev</b></VCardTitle>
+                              <VCardText>Les Guides du Dev accompagnent les équipes techniques dans l'implémentation du Design System en garantissant une intégration fluide et efficace.</VCardText>
+                          </VCard>
+                      </VCol>
+                  </VRow>
+              </div>
+            `,
+		}
+	},
+	tags: ['!dev'],
+}
+
+export const Guidelines: StoryObj = {
+	render: () => {
+		return {
+			components: { VRow, VCol, VCard, VCardTitle, VCardText },
+			template: `
+				<div>
+					<h3 class="text-h5 font-weight-bold mb-6 text-primary">Guidelines</h3>
+					<VRow>
+						<VCol cols="12" md="4">
+							<VCard class="pa-0 card-hover" elevation="0" href="/?path=/docs/accessibilit%C3%A9-introduction--docs">
+								<img src="/.storybook/public/home-card-access.svg" alt="Accessibilité" class="w-100"/>
+								<VCardTitle><b>Accessibilité</b></VCardTitle>
+								<VCardText>L'Accessibilité permet de concevoir des interfaces inclusives respectant les
+									normes d'accessibilité pour une expérience optimale pour tous les utilisateurs.
+								</VCardText>
+							</VCard>
+						</VCol>
+						<VCol cols="12" md="4">
+							<VCard class="pa-0 card-hover" elevation="0" href="/?path=/docs/accessibilit%C3%A9-introduction--docs">
+								<img src="/.storybook/public/home-card-eco.svg" alt="Eco-conception" class="w-100"/>
+								<VCardTitle><b>Éco-conception</b></VCardTitle>
+								<VCardText>L'éco-conception vise à réduire l'impact environnemental des interfaces
+									numériques en incluant des pratiques optimales en termes de performance, de
+									consommation énergétique et d'accessibilité.
+								</VCardText>
+							</VCard>
+						</VCol>
+					</VRow>
+				</div>
+			`,
 		}
 	},
 	tags: ['!dev'],
