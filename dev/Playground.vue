8cf7ad70a5aaf70d70f247478ea636eb65615d2e
virginieVachet <vachetvirginie@gmail.com>
2025-03-03 11:52:35 +0100

    remove playground

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,13 +1,2 @@
-<script setup>
-import { ref } from 'vue'
-import NirField from '@/components/NirField/NirField.vue'
-const nirTest = ref('')
-</script>
-
 <template>
-<NirField
-	v-model="nirTest"
-	placeholder="Date de test"
-    width="40%"
-/>
 </template>
