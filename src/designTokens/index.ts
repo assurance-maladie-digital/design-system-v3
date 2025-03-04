5d5acc3cfef9d3d48bb40e2f84b7117a8fde04e2
David FYON <davidfyon@gmail.com>
2025-03-04 16:21:22 +0100

    add changes

--- src/designTokens/index.ts
+++ src/designTokens/index.ts
@@ -4,6 +4,8 @@ import { cnamColorsTokens } from './tokens/cnam/cnamColors'
 import { cnamLightTheme } from './tokens/cnam/cnamLightTheme'
 import { cnamDarkTheme } from './tokens/cnam/cnamDarkTheme'
 import { paColorsTokens } from './tokens/pa/paColors'
+import { paSemanticTokens } from './tokens/pa/paSemantic'
+import { paContextualTokens } from './tokens/pa/paContextual'
 import { paLightTheme } from './tokens/pa/paLightTheme'
 import { paDarkTheme } from './tokens/pa/paDarkTheme'
 
@@ -14,6 +16,8 @@ export {
 	cnamLightTheme,
 	cnamDarkTheme,
 	paColorsTokens,
+	paSemanticTokens,
+	paContextualTokens,
 	paLightTheme,
 	paDarkTheme,
 }
