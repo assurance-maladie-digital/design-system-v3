a9d792afe708d28406b80fc5f94f86cba2f20ea6
adrien charrier <adrien.charrier@outlook.fr>
2025-03-04 15:47:42 +0100

    test: add snapshots for active filters and locales in FilterInline component

--- src/components/FilterInline/tests/FilterInline.spec.ts
+++ src/components/FilterInline/tests/FilterInline.spec.ts
@@ -2,6 +2,7 @@ import { describe, it, expect } from 'vitest'
 import FilterInline from '../FilterInline.vue'
 import { mount } from '@vue/test-utils'
 import { vuetify } from '@tests/unit/setup'
+import { locales } from '../locales'
 
 describe('FilterInline', () => {
 	it('should render correctly', () => {
@@ -12,5 +13,56 @@ describe('FilterInline', () => {
 			},
 		})
 		expect(wrapper.html()).toMatchSnapshot()
+	})
+
+	it('renders correctly with an active filter', () => {
+		const wrapper = mount(FilterInline, {
+			global: {
+				plugins: [vuetify],
+			},
+			props: {
+				modelValue: [
+					{
+						name: 'name',
+						label: 'Nom',
+						value: 'John Doe',
+					},
+				],
+			},
+		})
+
+		expect(wrapper.html()).toMatchSnapshot()
+	})
+
+	it('renders correctly with multiple active filters', () => {
+		const wrapper = mount(FilterInline, {
+			global: {
+				plugins: [vuetify],
+			},
+			props: {
+				modelValue: [
+					{
+						name: 'name',
+						label: 'Nom',
+						value: 'John Doe',
+					},
+					{
+						name: 'profession',
+						label: 'Profession',
+						value: ['Infirmer', 'Pharmacien'],
+					},
+				],
+			},
+		})
+
+		expect(wrapper.html()).toMatchSnapshot()
+	})
+})
+
+describe('FiltersInline locales', () => {
+	it('should return the correct locale', () => {
+		expect(locales.badgeLabel(0)).toMatchSnapshot()
+		expect(locales.badgeLabel(1)).toMatchSnapshot()
+		expect(locales.badgeLabel(2)).toMatchSnapshot()
 	})
 })
