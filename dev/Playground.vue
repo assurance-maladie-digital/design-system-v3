077fcf2d773d81bb9acecf7cc8c1eb913efee864
verushkarossi <verushkarossi@gmail.com>
2025-03-05 13:55:26 +0100

    accessibilité ok + refacto pages usages ok

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,16 +1,2 @@
-<script lang="ts" setup>
-	import Usages from '@/components/Usages/Usages.vue'
-	const itemsToDo = [
-		'texteddddddddddddddddddddddddddddddddddddd2', 'texte2', 'texte2',
-	]
-
-	const itemsNotToDo = [
-		'texteddddddddddddddddddddddddddddddddddd', 'texte2', 'texte2', 'texte2',
-	]
-</script>
 <template>
-	<Usages
-		:items1="itemsToDo"
-		:items2="itemsNotToDo"
-	/>
 </template>
