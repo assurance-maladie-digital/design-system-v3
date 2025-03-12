fa4b72fae8d664e277b6f678ea4c6833238ed8a5
David FYON <davidfyon@gmail.com>
2025-03-12 11:24:56 +0100

    add to do fix code

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -9,12 +9,34 @@ const applyThemeClass = (theme) => {
 	rootElement.classList.add(`theme-${theme}`)
 }
 
+// TODO : FIX THIS
+const applyThemeSidebar = (theme) => {
+	document.addEventListener('DOMContentLoaded', () => {
+		console.log('loaded')
+		const sidebar = document.querySelector('.sidebar')
+		console.log(sidebar)
+		if (sidebar) {
+			const items = sidebar.querySelectorAll('.sidebar-item') as NodeListOf<HTMLElement>
+			console.log(theme)
+			items.forEach((item) => {
+				if (theme === 'pa') {
+					if (item.querySelector('a[href="#démarrer-accueil--docs"]')) {
+						console.log(item)
+						item.style.display = 'none'
+					}
+				}
+			})
+		}
+	})
+}
+
 // Get stored theme or default to CNAM
 const storedTheme = typeof window !== 'undefined' ? localStorage.getItem('storybook-theme') : 'cnam'
 
 // Apply initial theme
 if (typeof window !== 'undefined') {
 	applyThemeClass(storedTheme || 'cnam')
+	applyThemeSidebar(storedTheme || 'cnam')
 }
 
 addons.setConfig({
@@ -26,14 +48,17 @@ if (typeof window !== 'undefined') {
 	window.addEventListener('storage', (event) => {
 		if (event.key === 'storybook-theme') {
 			const newTheme = event.newValue || 'cnam'
-			
+
 			// Update Storybook theme
 			addons.setConfig({
 				theme: newTheme === 'pa' ? paTheme : cnamTheme,
 			})
-			
+
 			// Apply theme class to HTML root
 			applyThemeClass(newTheme)
+
+			// Apply theme menu sidebar
+			applyThemeSidebar(newTheme)
 		}
 	})
 }
