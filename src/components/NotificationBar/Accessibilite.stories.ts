a063066e5cc866cc3abe6757083cc00a4936724e
David FYON <davidfyon@gmail.com>
2025-03-18 14:33:09 +0100

    add xls for NotificationBar accessibility (#446)

--- src/components/NotificationBar/Accessibilite.stories.ts
+++ src/components/NotificationBar/Accessibilite.stories.ts
@@ -159,6 +159,10 @@ export const Legende: StoryObj = {
 				  </div>
 				</div>
 			  </div>
+			  <div class="mt-4">
+				  <p>Rapport d’audit manuel : <a href="/audits/NotificationBar.xlsx" style="color:#0C41BD;">Voir le rapport</a></p>
+				  <p style="color: grey; font-size: 14px">Correctifs en cours (<a href="https://github.com/assurance-maladie-digital/design-system/issues/3967" target="_blank" style="color:#0C41BD;">issue #3967</a>)</p>
+			  </div>
             `,
 		}
 	},
