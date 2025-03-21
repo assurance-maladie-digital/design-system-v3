f39f661b7eb9592ced081b3940402951d3057d8f
virginieVachet <vachetvirginie@gmail.com>
2025-03-21 12:25:34 +0100

    fix default story on AmeliProBtn

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -7,18 +7,18 @@
 
 <template>
 	<div>
-		<AmeliproBtn
-			label="Click me"
-			:text="true"
-			:bordered="true"
-			:disabled="false"
-			:color="'primary'"
-			:text-color="'white'"
-			:icon-name="'mdi-check'"
-			:icon-left="true"
-			@click="handleClick"
-		>
-			Coucou
-		</AmeliproBtn>
+    <AmeliproBtn
+        label="Click me"
+        :text="true"
+        :bordered="true"
+        :disabled="false"
+        :color="'primary'"
+        :text-color="'white'"
+        :icon-name="'mdi-check'"
+        :icon-left="true"
+        @click="handleClick"
+    >
+      My Button
+    </AmeliproBtn>
 	</div>
 </template>
