97ea6da95b033656e28f989cd023f70c4cd4481f
David FYON <davidfyon@gmail.com>
2025-03-24 11:10:39 +0100

    ﻿add logo

--- .storybook/ApTheme.ts
+++ .storybook/ApTheme.ts
@@ -2,28 +2,28 @@ import { create } from '@storybook/theming/create'
 
 export default create({
 	base: 'light',
-	brandTitle: 'CNAM-DS',
+	brandTitle: 'AmeliPro',
 	brandUrl: './',
-	brandImage: '/logos/logo-assurance-maladie.svg',
+	brandImage: '/logos/logo-amelipro.svg',
 	brandTarget: '_self',
 
 	colorPrimary: 'rgba(0, 81, 109, 0.7)',
 	colorSecondary: 'rgba(0, 81, 109, 1)',
 
 	// UI
-	appBg: 'rgba(0, 81, 109, 0.05)',
+	appBg: 'rgba(0, 81, 109, 0.08)',
 	appContentBg: 'white',
 	appBorderColor: 'grey',
 	appBorderRadius: 4,
 
 	// Text colors
-	textColor: 'rgba(0, 81, 109, 1)',
+	textColor: 'rgb(0,34,44)',
 	textInverseColor: 'rgba(255,255,255,0.9)',
 
 	// Toolbar default and active colors
 	barTextColor: 'rgba(0, 81, 109, 1)',
 	barSelectedColor: 'rgba(0, 81, 109, 1)',
-	barBg: 'rgba(244, 245, 241, 0.7)',
+	barBg: 'rgba(244, 245, 241, 0.9)',
 
 	// Form colors
 	inputBg: 'white',
