5e880f869e0b58e403ed98b54a88cce43624e37f
David FYON <davidfyon@gmail.com>
2025-03-25 11:27:46 +0100

    fix home

--- src/stories/Demarrer/Accueil.stories.ts
+++ src/stories/Demarrer/Accueil.stories.ts
@@ -34,9 +34,10 @@ export const Header: StoryObj = {
 						<VCol md="6" sm="12">
 							<h1 class="font-weight-bold text-h4 mb-2">Synapse</h1>
 							<img alt="line" src="/home-line.svg" width="120" height="5" class="mb-5"/>
-							<h2 class="text-h4 mb-5">Documentation d'aide<br/> à la fabrication des IHM<br/><span
-								v-if="theme === 'ap'">d'AmeliPro</span><span
-								v-if="theme === 'pa'">de Portail Agent</span><span v-else>de la CNAM</span></h2>
+							<h2 class="text-h4 mb-5">Documentation d'aide<br/> à la fabrication des IHM<br/>
+								<span v-if="theme === 'ap'">d'AmeliPro</span>
+								<span v-if="theme === 'pa'">de Portail Agent</span>
+								<span v-if="theme === 'cnam'">de la CNAM</span></h2>
 							<VBtn color="primary" href="/?path=/docs/d%C3%A9marrer-introduction--docs">
 								Démarrer
 								<VIcon size="small" class="ml-2" right>{{ arrowRight }}</VIcon>
@@ -84,12 +85,14 @@ export const Welcome: StoryObj = {
 				}
 			},
 			template: `
-              <div class="mt-12 mb-12">
-                  <p>Bienvenue dans notre guide d'aide à la conception, pensé par et pour les équipes <span v-if="theme === 'pa'">de Portail Agent</span><span v-if="theme === 'ap'">d'AmeliPro</span><span v-if="theme === 'cnam'">de la CNAM</span>.</p>
-                  <p class="mt-4">Ce guide a été conçu pour accompagner chaque membre de l'équipe dans la création de plateformes web de qualité, alignées avec nos standards et nos valeurs.</p>
-                  <p class="mt-4">Vous y trouverez toutes les ressources essentielles pour vous guider tout au long du processus de conception :</p>
-              </div>
-            `,
+				<div class="mt-12 mb-12">
+					<p>Bienvenue dans notre guide d'aide à la conception, pensé par et pour les équipes <span v-if="theme === 'ap'">d'AmeliPro</span><span v-if="theme === 'pa'">de Portail Agent</span><span v-else>de la CNAM</span>.</p>
+					<p class="mt-4">Ce guide a été conçu pour accompagner chaque membre de l'équipe dans la création de
+						plateformes web de qualité, alignées avec nos standards et nos valeurs.</p>
+					<p v-if="theme !== 'ap'" class="mt-4">Vous y trouverez toutes les ressources essentielles pour vous
+						guider tout au long du processus de conception :</p>
+				</div>
+			`,
 		}
 	},
 	tags: ['!dev'],
@@ -98,12 +101,12 @@ export const Welcome: StoryObj = {
 export const DesignSystem: StoryObj = {
 	render: () => {
 		return {
-            setup() {
-                const theme = typeof window !== 'undefined' ? localStorage.getItem('storybook-theme') : 'cnam'
-                return {
-                    theme,
-                }
-            },
+			setup() {
+				const theme = typeof window !== 'undefined' ? localStorage.getItem('storybook-theme') : 'cnam'
+				return {
+					theme,
+				}
+			},
 			components: { VRow, VCol, VCard, VCardTitle, VCardText },
 			template: `
               <div class="mb-12" v-if="theme !== 'ap'">
