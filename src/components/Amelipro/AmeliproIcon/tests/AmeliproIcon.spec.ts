e74370352c7a163d73a7269bcd4b1b13b6df4042
David FYON <davidfyon@gmail.com>
2025-03-26 11:04:42 +0100

    add tests

--- src/components/Amelipro/AmeliproIcon/tests/AmeliproIcon.spec.ts
+++ src/components/Amelipro/AmeliproIcon/tests/AmeliproIcon.spec.ts
@@ -1,20 +1,14 @@
 import { mount } from '@vue/test-utils'
 import { expect, describe, it } from 'vitest'
 import AmeliproIcon from '../AmeliproIcon.vue'
-import { vuetify } from '@tests/unit/setup'
 
 describe('AmeliproIcon', () => {
 	it('render correctly', async () => {
 		const wrapper = mount(AmeliproIcon, {
-			global: {
-				plugins: [vuetify],
-			},
 			props: {
-				icon: 'utilisateur',
-				iconBgColor: 'ap-blue-darken-1',
-				iconColor: 'ap-white',
-				uniqueId: 'amelipro-icon-id',
-				xLarge: true,
+				icon: 'ap-icone-ajouter',
+				size: 24,
+				color: 'ap-blue',
 			},
 		})
 
