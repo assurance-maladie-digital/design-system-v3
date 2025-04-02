29b77a00f44ed8c90b6cc365f97cec20df364899
David FYON <davidfyon@gmail.com>
2025-04-02 11:29:12 +0200

    SelectBtnField: fix margin (#490)
    * fix margin
    
    * fix
    
    * fix margin
    
    * update snap

--- src/components/SelectBtnField/config.ts
+++ src/components/SelectBtnField/config.ts
@@ -3,7 +3,7 @@ export const config = {
 		height: 'auto',
 		minHeight: '40px',
 		activeClass: 'text-white',
-		class: 'py-2 d-flex align-items-center',
+		class: 'py-2 mr-0 d-flex align-items-center',
 	},
 	icon: {
 		class: 'text-white flex-shrink-0 ml-1',
