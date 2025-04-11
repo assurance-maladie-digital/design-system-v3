bfa9763b4634cd27ac8010ecf5e81cbea9aac085
David FYON <davidfyon@gmail.com>
2025-04-11 10:52:13 +0200

    add changes

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,2 +1,16 @@
+<script lang="ts" setup>
+import CopyBtn from '@/components/CopyBtn/CopyBtn.vue'
+</script>
+
 <template>
+  <div class="d-flex flex-wrap align-center pa-4">
+    <p class="mb-0 mr-1">
+      Patient n°<b>1970756541</b>
+    </p>
+
+    <CopyBtn
+        label="Copier le numéro de patient"
+        text-to-copy="1970756541"
+    />
+  </div>
 </template>
