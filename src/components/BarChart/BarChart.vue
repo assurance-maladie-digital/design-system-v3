bd0260c6d68dd4e54491a6a405be88624ec1e6bb
David FYON <davidfyon@gmail.com>
2025-04-11 15:06:49 +0200

    add lib + component

---
+++ src/components/BarChart/BarChart.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+	import { Bar } from 'vue-chartjs'
+	import {
+		Chart as ChartJS,
+		Title,
+		Tooltip,
+		Legend,
+		BarElement,
+		CategoryScale,
+		LinearScale,
+	} from 'chart.js'
+
+	// Enregistrement des composants Chart.js
+	ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+	// Données du graphique
+	const chartData = {
+		labels: ['January', 'February', 'March'],
+		datasets: [
+			{
+				data: [40, 20, 12],
+				label: 'Sales',
+				backgroundColor: '#0070f3',
+			},
+		],
+	}
+
+	// Options de configuration du graphique
+	const chartOptions = {
+		responsive: true,
+		plugins: {
+			legend: {
+				display: true,
+			},
+			title: {
+				display: true,
+				text: 'Bar Chart',
+			},
+		},
+	}
+</script>
+<template>
+	<Bar
+		id="my-chart-id"
+		:options="chartOptions"
+		:data="chartData"
+	/>
+</template>
