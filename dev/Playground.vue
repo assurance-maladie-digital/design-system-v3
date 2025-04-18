88f7f2b4f3df5e71f931f76bd80aaac985935f41
David FYON <davidfyon@gmail.com>
2025-04-18 17:16:16 +0200

    fix props

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,9 +1,31 @@
 <script lang="ts" setup>
 	import BarChart from '@/components/BarChart/BarChart.vue'
+
+	const data = {
+		labels: ['April', 'May', 'June'],
+		datasets: [
+			{
+				label: 'Revenus',
+				data: [50, 30, 70],
+				backgroundColor: '#34a853',
+			},
+		],
+	}
+
+	const options = {
+		plugins: {
+			title: {
+				text: 'Graphique des revenus trimestriels',
+			},
+		},
+	}
 </script>
 
 <template>
 	<div class="d-flex flex-wrap align-center pa-4">
-		<BarChart />
+		<BarChart
+			:chart-data="data"
+			:chart-options="options"
+		/>
 	</div>
 </template>
