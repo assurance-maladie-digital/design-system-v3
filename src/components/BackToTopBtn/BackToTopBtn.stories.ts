1306df359d6b7dbf3cc8d66fc117cbc3b767e207
adrien <adrien.charrier@outlook.fr>
2025-04-23 13:49:19 +0200

    improve props documentation

--- src/components/BackToTopBtn/BackToTopBtn.stories.ts
+++ src/components/BackToTopBtn/BackToTopBtn.stories.ts
@@ -12,23 +12,68 @@ const meta = {
 	argTypes: {
 		threshold: {
 			control: { type: 'range', max: 2000 },
+			description: 'Distance par rapport au bas de la page à partir de laquelle le bouton apparaît',
+			table: {
+				type: { summary: 'number' },
+			},
 		},
 		nudgeRight: {
 			control: { type: 'range' },
+			description: 'Décalage du bouton par rapport au bord droit de la fenêtre',
 			default: 16,
+			table: {
+				type: { summary: 'number' },
+			},
 		},
 		nudgeBottom: {
 			control: { type: 'range' },
+			description: 'Décalage du bouton par rapport au bord inférieur de la fenêtre',
 			default: 16,
+			table: {
+				type: { summary: 'number' },
+			},
 		},
 		target: {
 			control: { type: 'text' },
+			description: 'ID du conteneur devant être scrollé',
+			table: {
+				type: { summary: 'string' },
+			},
 		},
 		default: {
+			description: 'Texte du bouton',
 			control: { type: 'text' },
 		},
 		icon: {
+			description: 'Icône du bouton',
 			control: { type: 'text' },
+		},
+		vuetifyOptions: {
+			control: { type: 'object' },
+			description: 'Options de personnalisation du bouton',
+			default: () => ({
+				btn: {
+					variant: 'outlined',
+					color: 'primary',
+					class: 'text-wrap px-0 px-md-4',
+				},
+				icon: {
+					color: 'primary',
+					size: 'small',
+					class: 'ml-0 ml-md-1',
+				},
+			}),
+			table: {
+				category: 'props',
+				type: {
+					summary: 'object',
+					detail: `
+{
+	btn: Record<string, unknown>,
+	icon: Record<string, unknown>,
+}`,
+				},
+			},
 		},
 	},
 	args: {
