369c2e7bf08a79dbbbcdb83196e5933a43f9f4d2
David FYON <davidfyon@gmail.com>
2025-05-05 09:56:59 +0200

    Doc:  avancement audit page (#575)
    * add avancement story
    
    * fix
    
    * fix

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -91,6 +91,8 @@ const preview: Preview = {
 						'Aculturation', ['Sensibilisation à l’accessibilité numérique'],
 						'Kit de pré-audit', ['Introduction', 'Échantillonnage', 'Pré-audit', 'Outils', ['Introduction', 'Tanaguru']],
 						'Audit', ['RGAA'],
+						'Avancement',
+						'Vuetify',
 					],
 					'Éco-conception',
 					[
