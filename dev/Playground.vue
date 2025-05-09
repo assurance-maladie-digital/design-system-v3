4da52cb068e48f36bae9022ff78b22c656b27174
David FYON <davidfyon@gmail.com>
2025-05-09 14:23:21 +0200

    add component

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,2 +1,36 @@
+<script setup>
+	import DiacritiquePicker from '@/components/DiacriticPicker/DiacriticPicker.vue'
+	import { ref } from 'vue'
+
+	const nom = ref('')
+	const adresse = ref('')
+</script>
+
 <template>
+	<div class="pa-4">
+		<DiacritiquePicker>
+			<v-text-field
+				id="diacritique-input"
+				v-model="nom"
+				label="Nom avec accents"
+				color="primary"
+				variant="outlined"
+			/>
+		</DiacritiquePicker>
+    {{ nom }}
+
+    <br/><br/>
+
+		<DiacritiquePicker>
+			<v-textarea
+				id="diacritique-input"
+				v-model="adresse"
+				label="Adresse"
+				color="primary"
+				variant="outlined"
+				auto-grow
+			/>
+		</DiacritiquePicker>
+    {{ adresse }}
+	</div>
 </template>
---
+++ src/components/DiacriticPicker/DiacriticPicker.vue
@@ -0,0 +1,186 @@
+<script setup>
+	import { ref, nextTick, computed, onMounted } from 'vue'
+
+	const wrapperRef = ref(null)
+	const dialog = ref(false)
+
+	const inputId = 'diacritique-input'
+	const buttonId = 'diacritique-button'
+	const dialogId = 'diacritique-dialog'
+
+	const inputMessageHeight = ref(0)
+
+	const dynamicStyles = computed(() => ({
+		marginTop: `-${inputMessageHeight.value}px`,
+	}))
+
+	function updateInputMessageHeight() {
+		const element = document.querySelector('#diacritique-input-messages')
+		if (element) {
+			inputMessageHeight.value = element.getBoundingClientRect().height
+		}
+	}
+
+	const diacriticsMap = {
+		e: ['e', 'é', 'è', 'ê', 'ë'],
+		a: ['a', 'à', 'â', 'ä', 'æ'],
+		i: ['i', 'î', 'ï'],
+		o: ['o', 'ô', 'ö', 'œ'],
+		u: ['u', 'ù', 'û', 'ü'],
+		y: ['y', 'ÿ'],
+		c: ['c', 'ç'],
+	}
+
+	const diacritics = {
+		lower: [...new Set(Object.values(diacriticsMap).flat())],
+		upper: [...new Set(Object.values(diacriticsMap).flat().map(c => c.toUpperCase()))],
+	}
+
+	function getNativeInput() {
+		return wrapperRef.value?.querySelector('input, textarea')
+	}
+
+	function insertChar(char) {
+		const el = getNativeInput()
+		if (!el) return
+
+		const pos = el.selectionStart
+		const value = el.value
+		const newValue = value.slice(0, pos) + char + value.slice(pos)
+
+		el.value = newValue
+		el.dispatchEvent(new Event('input')) // Synchronise avec v-model
+
+		nextTick(() => {
+			el.focus()
+			el.setSelectionRange(pos + 1, pos + 1)
+		})
+
+		dialog.value = false
+	}
+
+	function handleKeydown(event) {
+		if (event.key !== '=') return
+
+		const el = getNativeInput()
+		if (!el) return
+
+		const pos = el.selectionStart
+		const value = el.value
+		const prevChar = value[pos - 1]
+		if (!prevChar) return
+
+		const isUpper = prevChar === prevChar.toUpperCase()
+		const baseChar = prevChar.toLowerCase()
+		const list = diacriticsMap[baseChar]
+		if (!list) return
+
+		const currentIndex = list.findIndex(c =>
+			isUpper ? c.toUpperCase() === prevChar : c === prevChar,
+		)
+		if (currentIndex === -1) return
+
+		const nextChar = isUpper
+			? list[(currentIndex + 1) % list.length].toUpperCase()
+			: list[(currentIndex + 1) % list.length]
+
+		el.value = value.slice(0, pos - 1) + nextChar + value.slice(pos)
+
+		nextTick(() => {
+			el.setSelectionRange(pos, pos)
+		})
+
+		event.preventDefault()
+	}
+
+	onMounted(() => {
+		nextTick(() => {
+			updateInputMessageHeight()
+		})
+	})
+</script>
+
+<template>
+	<div class="diacritic-wrapper">
+		<div
+			ref="wrapperRef"
+      :id="inputId"
+			class="input-slot flex-grow-1"
+			role="textbox"
+			tabindex="0"
+			@keydown="handleKeydown"
+		>
+			<slot />
+		</div>
+
+		<VBtn
+			:id="buttonId"
+			icon
+			size="small"
+			variant="tonal"
+			color="primary"
+			:aria-controls="dialogId"
+			:aria-haspopup="'dialog'"
+			:aria-expanded="dialog.toString()"
+			aria-label="Caractères accentués"
+			class="ml-2"
+			:style="dynamicStyles"
+			@click="dialog = !dialog"
+		>
+			&Eacute;&eacute;
+		</VBtn>
+
+		<VDialog
+			v-model="dialog"
+			scrollable
+			:retain-focus="false"
+			aria-modal="true"
+			:aria-labelledby="buttonId"
+			role="dialog"
+		>
+			<VCard @click:outside="dialog = false">
+				<VCardTitle>Caractères diacritiques</VCardTitle>
+				<VCardText>
+					<div
+						class="d-flex flex-wrap"
+						role="group"
+						aria-label="Caractères minuscules"
+					>
+						<VBtn
+							v-for="char in diacritics.lower"
+							:key="'l-' + char"
+							size="small"
+							class="ma-1"
+							@click="insertChar(char)"
+						>
+							{{ char }}
+						</VBtn>
+					</div>
+					<div
+						class="d-flex flex-wrap mt-2"
+						role="group"
+						aria-label="Caractères majuscules"
+					>
+						<VBtn
+							v-for="char in diacritics.upper"
+							:key="'u-' + char"
+							size="small"
+							class="ma-1"
+							@click="insertChar(char)"
+						>
+							{{ char }}
+						</VBtn>
+					</div>
+				</VCardText>
+			</VCard>
+		</VDialog>
+	</div>
+</template>
+
+<style scoped>
+.diacritic-wrapper {
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+</style>
