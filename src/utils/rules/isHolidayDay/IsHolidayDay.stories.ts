842c0d558d763cc5210629bd2e6ba6ceb967be54
virginieVachet <vachetvirginie@gmail.com>
2025-05-13 09:00:35 +0200

    add source code

--- src/utils/rules/isHolidayDay/IsHolidayDay.stories.ts
+++ src/utils/rules/isHolidayDay/IsHolidayDay.stories.ts
@@ -70,4 +70,60 @@ export const Default: Story = {
 			</div>
 		`,
 	}),
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Script',
+				code: `
+				<script setup lang="ts">
+					import { isHolidayDay } from '@cnamts/synapse'
+					import { datePicker } from '@cnamts/synapse'
+					
+					const date = ref('')
+					const { getJoursFeries } = useHolidayDay()
+					const currentYear = new Date().getFullYear()
+					const holidays = Array.from(getJoursFeries(currentYear)).sort()
+					const holidayRules = [
+							{
+								type: 'isHolidayDay',
+								options: {
+								fieldName: 'La date',
+								message: 'Vous ne pouvez pas sélectionner un jour férié.',
+								successMessage: 'La date sélectionnée n\\'est pas un jour férié.',
+								},
+							},
+					]
+				</script>
+			`,
+			},
+			{
+				name: 'Template',
+				code: `
+				<template>
+				<div>
+				<DatePicker
+					v-model="date"
+					label="Date (pas de jour férié)"
+					:custom-rules="rules"
+					placeholder="Sélectionnez une date"
+				/>
+				
+				<div class="mt-4">
+					<p>Date sélectionnée : <strong>{{ date || 'Aucune' }}</strong></p>
+				</div>
+				
+				<div class="mt-4">
+					<h4>Jours fériés de l'année en cours :</h4>
+					<ul>
+						<li v-for="(holiday, index) in holidays" :key="index">
+							{{ holiday }}
+						</li>
+					</ul>
+				</div>
+			</div>
+				</template>
+				`,
+			},
+		],
+	},
 }
