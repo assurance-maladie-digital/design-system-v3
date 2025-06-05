0c627b4a80fd4bb0d7e444a799ba94c4f5b843d7
virginieVachet <vachetvirginie@gmail.com>
2025-06-05 21:08:02 +0200

    add logo audit

--- src/components/Logo/Accessibilite.stories.ts
+++ src/components/Logo/Accessibilite.stories.ts
@@ -216,6 +216,10 @@ export const Legende: StoryObj = {
 				  </div>
 				</div>
 			  </div>
+			  <div class="mt-4">
+				  <p>Rapport d’audit manuel : <a href="/audits/Logo.xlsx" style="color:#0C41BD;">Voir le rapport</a></p>
+				  <p style="color: grey; font-size: 14px">Aucune correction nécessaire.</p>
+			  </div>
             `,
 		}
 	},
