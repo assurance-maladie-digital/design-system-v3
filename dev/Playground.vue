d722d8752335c0e838fc62d6da4dffd768451252
David FYON <davidfyon@gmail.com>
2025-06-12 15:46:21 +0200

    reset playground

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,28 +1,2 @@
-<script setup lang="ts">
-	import { ref } from 'vue'
-	import SyTable from '../src/components/Tables/SyTable/SyTable.vue'
-
-	const headers = [
-		{ text: 'Column 1', value: 'col1' },
-		{ text: 'Column 2', value: 'col2' },
-	]
-
-	const items = [
-		{ name: 'col1', label: 'Column 1' },
-		{ name: 'col2', label: 'Column 2' },
-	]
-
-</script>
-
 <template>
-	<div>
-		<SyTable
-			:headers
-			:items
-			:resizable-columns="true"
-			:suffix="'.playground'"
-		/>
-	</div>
 </template>
-
-<style lang="scss" scoped></style>
