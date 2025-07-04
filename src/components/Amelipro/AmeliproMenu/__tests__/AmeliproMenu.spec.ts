1f32a19caa8f515d8d30d183383fa4cddbab70b0
Grégory Nicol <gregory.nicol@sogeti.com>
2025-07-04 17:59:33 +0200

    Add unit tests for AmeliproMenu

--- src/components/Amelipro/AmeliproMenu/__tests__/AmeliproMenu.spec.ts
+++ src/components/Amelipro/AmeliproMenu/__tests__/AmeliproMenu.spec.ts
@@ -1,87 +1,168 @@
-import { mount } from '@vue/test-utils'
-import { expect, describe, it } from 'vitest'
+import { VueWrapper, mount, shallowMount } from '@vue/test-utils'
+import { beforeEach, describe, expect, it } from 'vitest'
 import AmeliproMenu from '../AmeliproMenu.vue'
-import { vuetify } from '@tests/unit/setup'
+import type { AmeliproMenuItem } from '../types'
+import type { ComponentProps } from 'vue-component-type-helpers'
+import type { ExpectedPropOptions } from '@tests/types'
+import type { PropType } from 'vue'
+import type { RouteLocationRaw } from 'vue-router'
+import TestHelper from '@tests/helpers/TestHelper'
+import { attachToApp } from '@tests/helpers/utils'
+
+const expectedPropOptions: ExpectedPropOptions<typeof AmeliproMenu> = {
+	homeHref: {
+		type: String,
+		default: undefined,
+	},
+	homeTo: {
+		type: [Array, Object, String] as PropType<RouteLocationRaw>,
+		default: undefined,
+	},
+	items: {
+		type: Array as PropType<AmeliproMenuItem[]>,
+		default: () => [],
+	},
+	menuHeader: {
+		type: String,
+		default: 'Titre du service',
+	},
+	uniqueId: {
+		type: String,
+		required: true,
+	},
+}
+
+const requiredPropValues = (): ComponentProps<typeof AmeliproMenu> => ({ uniqueId: 'required-unique-id' })
+
+const modifiedPropValues = (): ComponentProps<typeof AmeliproMenu> => ({
+	homeHref: 'modified-home-href',
+	homeTo: 'modified-home-to',
+	items: [
+		{
+			actif: false,
+			children: [
+				{
+					actif: false,
+					children: [
+						{
+							id: 'modified-subsubmenu-1-1-1',
+							name: 'Modified SubSubMenu 1.1.1',
+							to: '/modified/test',
+						},
+					],
+					id: 'modified-submenu-1-1',
+					name: 'Modified SubMenu 1.1',
+				},
+			],
+			id: 'modified-menu-1',
+			name: 'Modified Menu 1',
+		},
+	],
+	menuHeader: 'Modified menu header',
+	uniqueId: 'modified-unique-id',
+})
+
+const testHelper = new TestHelper(AmeliproMenu)
+testHelper
+	.setExpectedPropOptions(expectedPropOptions)
+	.setRequiredPropValues(requiredPropValues)
+	.setModifiedPropValues(modifiedPropValues)
+	.setMountOptions({
+		global: {
+			stubs: {
+				VNavigationDrawer: { template: '<div><slot /></div>' },
+				VDivider: true,
+			},
+		},
+	})
 
 describe('AmeliproMenu', () => {
-	it('render correctly', async () => {
-		const wrapper = mount(AmeliproMenu, {
-			global: {
-				plugins: [vuetify],
-				stubs: {
-					VNavigationDrawer: {
-						template: '<div><slot></slot></div>',
+	describe('Snapshots', () => {
+		testHelper.snapshots()
+	})
+
+	describe('Properties', () => {
+		testHelper.properties()
+	})
+
+	describe('Setting props should update attributes of inner tags', () => {
+		let wrapper: VueWrapper<InstanceType<typeof AmeliproMenu>>
+
+		beforeEach(() => {
+			wrapper = shallowMount(AmeliproMenu, {
+				autoAttach: attachToApp(),
+				global: {
+					stubs: {
+						VNavigationDrawer: true,
+						VDivider: true,
 					},
 				},
-			},
-			props: {
-				items: [
-					{	actif: false,
-						children: [
-							{
-								actif: false,
-								children: [
-									{
-										id: '111',
-										name: 'SubSubMenu 1.1.1',
-										to: '/test',
-									},
-									{
-										id: '112',
-										name: 'SubSubMenu 1.1.2',
-										to: '/test2',
-									},
-								],
-								id: '11',
-								name: 'SubMenu 1.1',
-							},
-							{
-								actif: false,
-								id: '12',
-								name: 'SubMenu 1.2',
-								to: '/test3',
-							},
-						],
-						id: '1',
-						name: 'Menu 1',
-					},
-					{
-						actif: false,
-						children: [
-							{
-								id: '21',
-								name: 'SubMenu 2.1',
-							},
-							{
-								actif: false,
-								children: [
-									{
-										children: [],
-										id: '211',
-										name: 'SubSubMenu 2.1.1',
-									},
-									{
-										children: [],
-										id: '212',
-										name: 'SubSubMenu 2.1.2',
-									},
-								],
-								id: '22',
-								name: 'SubMenu 2.2',
-							},
-						],
-						id: '2',
-						name: 'Menu 2',
-					},
-					{
-						id: '3',
-						name: 'Menu 3',
-					},
-				],
-				uniqueId: 'amelipro-menu-id',
-			},
+				props: requiredPropValues(),
+			})
 		})
 
-		expect(wrapper.html()).toMatchSnapshot()
+		it('prop uniqueId sets attribute id on root container', async () => {
+			expect(wrapper.find('.amelipro-menu').attributes('id')).toBe(`${testHelper.default('uniqueId')}-container`)
+			const { uniqueId } = modifiedPropValues()
+			await wrapper.setProps({ uniqueId })
+			expect(wrapper.find('.amelipro-menu').attributes('id')).toBe(`${testHelper.modified('uniqueId')}-container`)
+		})
+	})
+
+	describe('Public functions', () => {
+		let wrapper: VueWrapper<InstanceType<typeof AmeliproMenu>>
+
+		describe('test setFocus', () => {
+			beforeEach(() => {
+				wrapper = mount(AmeliproMenu, {
+					autoAttach: attachToApp(),
+					global: {
+						stubs: {
+							VNavigationDrawer: { template: '<div><slot /></div>' },
+							VDivider: true,
+						},
+					},
+					props: modifiedPropValues(),
+				})
+			})
+
+			it('setFocus on close button', async () => {
+				await wrapper.find('.amelipro-menu__btn--open').trigger('click')
+				expect(wrapper.find(`#${testHelper.modified('uniqueId')}-close-menu-btn`).exists()).toBe(true)
+				await wrapper.find(`#${testHelper.modified('uniqueId')}-close-menu-btn`).trigger('keydown', { key: 'Tab', shiftKey: true })
+				expect(wrapper.find(`#${testHelper.modified('uniqueId')}-return-home-menu-btn`).exists()).toBe(true)
+			})
+
+			it('setFocus on home button', async () => {
+				await wrapper.find('.amelipro-menu__btn--open').trigger('click')
+				expect(wrapper.find(`#${testHelper.modified('uniqueId')}-return-home-menu-btn`).exists()).toBe(true)
+				await wrapper.find(`#${testHelper.modified('uniqueId')}-return-home-menu-btn`).trigger('keydown', { key: 'Tab' })
+				expect(wrapper.find(`#${testHelper.modified('uniqueId')}-close-menu-btn`).exists()).toBe(true)
+			})
+		})
+	})
+
+	describe('Events', () => {
+		let wrapper: VueWrapper<InstanceType<typeof AmeliproMenu>>
+
+		beforeEach(() => {
+			wrapper = mount(AmeliproMenu, {
+				autoAttach: true,
+				global: {
+					stubs: {
+						VNavigationDrawer: true,
+						VDivider: true,
+					},
+				},
+				props: modifiedPropValues(),
+			})
+		})
+
+		it('Escape key emits escape event', async () => {
+			await wrapper.find('.amelipro-menu__btn--open').trigger('click')
+			expect(wrapper.emitted('escape')).toBeUndefined()
+			await window.dispatchEvent(new KeyboardEvent('keydown', { code: 'Escape' }))
+			expect(wrapper.emitted('escape')).toStrictEqual([[]])
+		})
 	})
 })
