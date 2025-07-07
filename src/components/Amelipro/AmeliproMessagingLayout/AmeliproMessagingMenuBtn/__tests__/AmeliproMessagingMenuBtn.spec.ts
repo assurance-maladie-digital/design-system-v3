83df36d355032bdb4c94cf81ec6f2485add4e3a4
Grégory Nicol <gregory.nicol@sogeti.com>
2025-07-07 11:30:49 +0200

    Add unit tests for AmeliproMessagingMenuBtn

--- src/components/Amelipro/AmeliproMessagingLayout/AmeliproMessagingMenuBtn/__tests__/AmeliproMessagingMenuBtn.spec.ts
+++ src/components/Amelipro/AmeliproMessagingLayout/AmeliproMessagingMenuBtn/__tests__/AmeliproMessagingMenuBtn.spec.ts
@@ -1,20 +1,72 @@
-import { mount } from '@vue/test-utils'
-import { expect, describe, it } from 'vitest'
 import AmeliproMessagingMenuBtn from '../AmeliproMessagingMenuBtn.vue'
-import { vuetify } from '@tests/unit/setup'
+import type { ComponentProps } from 'vue-component-type-helpers'
+import type { ExpectedPropOptions } from '@tests/types'
+import type { PropType } from 'vue'
+import type { RouteLocationRaw } from 'vue-router'
+import TestHelper from '@tests/helpers/TestHelper'
+import { describe } from 'vitest'
+
+const expectedPropOptions: ExpectedPropOptions<typeof AmeliproMessagingMenuBtn> = {
+	active: {
+		type: Boolean,
+		default: false,
+	},
+	href: {
+		type: String,
+		default: undefined,
+	},
+	icon: {
+		type: String,
+		default: undefined,
+	},
+	label: {
+		type: String,
+		default: undefined,
+	},
+	to: {
+		type: [Array, Object, String] as PropType<RouteLocationRaw>,
+		default: undefined,
+	},
+	uniqueId: {
+		type: String,
+		required: true,
+	},
+	unreadNumber: {
+		type: Number,
+		default: undefined,
+	},
+}
+
+// Values pour les props "required"
+const requiredPropValues = (): ComponentProps<typeof AmeliproMessagingMenuBtn> => ({ uniqueId: 'required-unique-id' })
+
+// Valeurs pour les props "modified"
+const modifiedPropValues = (): ComponentProps<typeof AmeliproMessagingMenuBtn> => ({
+	active: true,
+	href: '#modified',
+	icon: 'modified-plus',
+	label: 'Modified label',
+	to: '/modified-home',
+	uniqueId: 'modified-unique-id',
+	unreadNumber: 5,
+})
+
+const testHelper = new TestHelper(AmeliproMessagingMenuBtn)
+testHelper.setExpectedPropOptions(expectedPropOptions)
+	.setRequiredPropValues(requiredPropValues)
+	.setModifiedPropValues(modifiedPropValues)
 
 describe('AmeliproMessagingMenuBtn', () => {
-	it('render correctly', async () => {
-		const wrapper = mount(AmeliproMessagingMenuBtn, {
-			global: {
-				plugins: [vuetify],
-			},
-			props: {
-				label: 'label du bouton',
-				uniqueId: 'my-messaging-menu-btn-id',
-			},
-		})
-
-		expect(wrapper.html()).toMatchSnapshot()
+	describe('Snapshots', () => {
+		testHelper.snapshots()
 	})
+
+	describe('Properties', () => {
+		testHelper.properties()
+	})
+
+	// const wrapper = mount(AmeliproMessagingMenuBtn, {
+	//	props: modifiedPropValues(),
+	//	stubs: { AmeliproIcon },
+	// });
 })
