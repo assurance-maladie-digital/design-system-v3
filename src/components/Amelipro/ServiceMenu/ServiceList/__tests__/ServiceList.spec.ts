234a6c45822b52137971c5588ba3b4a140c4aa0e
Grégory Nicol <gregory.nicol@sogeti.com>
2025-07-08 11:47:07 +0200

    Add unit tests for ServiceList

--- src/components/Amelipro/ServiceMenu/ServiceList/__tests__/ServiceList.spec.ts
+++ src/components/Amelipro/ServiceMenu/ServiceList/__tests__/ServiceList.spec.ts
@@ -1,29 +1,116 @@
-import { mount } from '@vue/test-utils'
-import { expect, describe, it } from 'vitest'
+import { VueWrapper, shallowMount } from '@vue/test-utils'
+import { beforeEach, describe, expect, it } from 'vitest'
+import type { ComponentProps } from 'vue-component-type-helpers'
+import type { ExpectedPropOptions } from '@tests/types'
+import type { PropType } from 'vue'
+import type { Service } from '../../ServiceBtn/types'
+import ServiceBtn from '../../ServiceBtn/ServiceBtn.vue'
 import ServiceList from '../ServiceList.vue'
-import { vuetify } from '@tests/unit/setup'
+import TestHelper from '@tests/helpers/TestHelper'
+
+const expectedPropOptions: ExpectedPropOptions<typeof ServiceList> = {
+	items: {
+		type: Array as PropType<Service[]>,
+		required: true,
+	},
+	serviceContact: {
+		type: Boolean,
+		default: false,
+	},
+	servicePatient: {
+		type: Boolean,
+		default: false,
+	},
+	uniqueId: {
+		type: String,
+		required: true,
+	},
+}
+
+// Values pour les props "required"
+const requiredPropValues = (): ComponentProps<typeof ServiceList> => ({
+	items: [],
+	uniqueId: 'required-unique-id',
+})
+
+// Valeurs pour les props "modified"
+const modifiedPropValues = (): ComponentProps<typeof ServiceList> => ({
+	items: [
+		{
+			icon: 'modified-item-icon-1',
+			label: 'Modified item label 1',
+		},
+		{
+			icon: 'modified-item-icon-2',
+			label: 'Modified item label 2',
+		},
+	] as Service[],
+	serviceContact: true,
+	servicePatient: true,
+	uniqueId: 'modified-unique-id',
+})
+
+const testHelper = new TestHelper(ServiceList)
+testHelper.setExpectedPropOptions(expectedPropOptions)
+	.setRequiredPropValues(requiredPropValues)
+	.setModifiedPropValues(modifiedPropValues)
 
 describe('ServiceList', () => {
-	it('render correctly', async () => {
-		const wrapper = mount(ServiceList, {
-			global: {
-				plugins: [vuetify],
-			},
-			props: {
-				items: [
-					{
-						icon: 'releveHonoraires',
-						label: 'relevé honoraires',
-					},
-					{
-						icon: 'horairesCabinet',
-						label: 'horaires cabinet',
-					},
-				],
-				uniqueId: 'service-list-id',
-			},
+	describe('Snapshots', () => {
+		testHelper.snapshots()
+	})
+
+	describe('Properties', () => {
+		testHelper.properties()
+	})
+
+	describe('Setting props should update attributes of inner tags', () => {
+		let vueWrapper: VueWrapper<InstanceType<typeof ServiceList>>
+		beforeEach(() => {
+			vueWrapper = shallowMount(ServiceList, { props: requiredPropValues() })
 		})
 
-		expect(wrapper.html()).toMatchSnapshot()
+		it('prop uniqueId sets id attribute on root ul', async () => {
+			expect(vueWrapper.find('ul').attributes('id')).toBe(testHelper.default('uniqueId'))
+			const { uniqueId } = modifiedPropValues()
+			await vueWrapper.setProps({ uniqueId })
+			expect(vueWrapper.find('ul').attributes('id')).toBe(testHelper.modified('uniqueId'))
+		})
+	})
+
+	describe('Setting props should update props or attributes of inner components', () => {
+		let vueWrapper: VueWrapper<InstanceType<typeof ServiceList>>
+		beforeEach(() => {
+			vueWrapper = shallowMount(ServiceList, { props: modifiedPropValues(), global: { stubs: { ServiceBtn } } })
+		})
+
+		it('prop items sets item prop on ServiceBtn', () => {
+			const serviceBtns = vueWrapper.findAllComponents(ServiceBtn)
+			expect(serviceBtns.length).toBe(modifiedPropValues().items.length)
+			serviceBtns.forEach((btn, idx) => {
+				expect(btn.props('item')).toEqual(modifiedPropValues().items[idx])
+			})
+		})
+
+		it('prop uniqueId sets unique-id prop on ServiceBtn', () => {
+			const serviceBtns = vueWrapper.findAllComponents(ServiceBtn)
+			serviceBtns.forEach((btn, idx) => {
+				expect(btn.props('uniqueId')).toBe(`modified-unique-id-service-patient-btn-${idx}`)
+			})
+		})
+	})
+
+	describe('Other', () => {
+		it('renders correct number of ServiceBtn', () => {
+			const vueWrapper = shallowMount(ServiceList, { props: modifiedPropValues(), global: { stubs: { ServiceBtn } } })
+			expect(vueWrapper.findAllComponents(ServiceBtn).length).toBe(modifiedPropValues().items.length)
+		})
+
+		it('renders correct li ids', () => {
+			const vueWrapper = shallowMount(ServiceList, { props: modifiedPropValues(), global: { stubs: { ServiceBtn } } })
+			modifiedPropValues().items.forEach((_, idx) => {
+				expect(vueWrapper.find(`#${testHelper.modified('uniqueId')}-item-${idx}`).exists()).toBe(true)
+			})
+		})
 	})
 })
