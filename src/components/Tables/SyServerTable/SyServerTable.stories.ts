cd7dcf833930b26f5a2254efa68830b8f66f0431
adrien <adrien.charrier@outlook.fr>
2025-07-11 17:36:10 +0200

    Refactor ServerMultiSort story: improve user data structure and enhance multi-sort functionality

--- src/components/Tables/SyServerTable/SyServerTable.stories.ts
+++ src/components/Tables/SyServerTable/SyServerTable.stories.ts
@@ -665,20 +665,19 @@ export const ServerMultiSort: Story = {
 			  const total = items.length
 
 			  if (sortBy && sortBy.length > 0) {
-							// Apply multi-sort if there are multiple sort criteria
-							items = items.sort((a, b) => {
-								// Loop through all sort criteria
-								for (const sort of sortBy) {
-									const key = sort.key
-									const order = sort.order === 'asc' ? 1 : -1
-									
-									if (a[key] !== b[key]) {
-										return a[key] > b[key] ? order : -order
-									}
-								}
-								return 0 // If all criteria are equal
-							})
+					items.sort((a, b) => {
+						for (const sort of sortBy) {
+							const key = sort.key
+							const r = String(a[key]).localeCompare(String(b[key]))
+
+							const order = sort.order === 'asc' ? 1 : -1
+							if (r !== 0) {
+								return r * order
+							}
 						}
+						return 0
+					})
+				}
 
 			  if (itemsPerPage > 0) {
 				items = items.slice((page - 1) * itemsPerPage, page * itemsPerPage)
@@ -691,22 +690,42 @@ export const ServerMultiSort: Story = {
 
 		  const getUsers = (): User[] => {
 			return [
-							{ firstname: 'Virginie', lastname: 'Beauchesne', email: 'virginie.beauchesne@example.com' },
-							{ firstname: 'Simone', lastname: 'Bellefeuille', email: 'simone.bellefeuille@example.com' },
-							{ firstname: 'Étienne', lastname: 'Salois', email: 'etienne.salois@example.com' },
-							{ firstname: 'Bernadette', lastname: 'Langelier', email: 'bernadette.langelier@example.com' },
-							{ firstname: 'Agate', lastname: 'Roy', email: 'agate.roy@example.com' },
-							{ firstname: 'Louis', lastname: 'Denis', email: 'louis.denis@example.com' },
-							{ firstname: 'Édith', lastname: 'Cartier', email: 'edith.cartier@example.com' },
-							{ firstname: 'Alphonse', lastname: 'Bouvier', email: 'alphonse.bouvier@example.com' },
-							{ firstname: 'Eustache', lastname: 'Dubois', email: 'eustache.dubois@example.com' },
-							{ firstname: 'Rosemarie', lastname: 'Quessy', email: 'rosemarie.quessy@example.com' },
-							{ firstname: 'Serge', lastname: 'Rivard', email: 'serge.rivard@example.com' },
-							{ firstname: 'Jacques', lastname: 'Demers', email: 'jacques.demers@example.com' },
-							{ firstname: 'Aimée', lastname: 'Josseaume', email: 'aimee.josseaume@example.com' },
-							{ firstname: 'Delphine', lastname: 'Robillard', email: 'delphine.robillard@example.com' },
-							{ firstname: 'Alexandre', lastname: 'Lazure', email: 'alexandre.lazure@example.com' },
-						]
+				{
+					firstname: 'Virginie',
+					lastname: 'Beauchesne',
+					email: 'virginie.beauchesne@example.com',
+				},
+				{
+					firstname: 'Simone',
+					lastname: 'Bellefeuille',
+					email: 'simone.bellefeuille@example.com',
+				},
+				{
+					firstname: 'Étienne',
+					lastname: 'Salois',
+					email: 'etienne.salois@example.com',
+				},
+				{
+					firstname: 'Thierry',
+					lastname: 'Bobu',
+					email: 'thierry.bobu@example.com',
+				},
+				{
+					firstname: 'Bernadette',
+					lastname: 'Langelier',
+					email: 'bernadette.langelier@exemple.com'
+				},
+				{
+					firstname: 'Agate',
+					lastname: 'Roy',
+					email: 'agate.roy@exemple.com'
+				},
+				{
+					firstname: 'Agate',
+					lastname: 'Beauchesne',
+					email: 'agate.beauchesne@exemple.com'
+				}
+			]
 		  }
           
            // Initialize data
@@ -780,11 +799,17 @@ export const ServerMultiSort: Story = {
 						const total = items.length
 
 						if (sortBy && sortBy.length > 0) {
-							items = items.sort((a, b) => {
-								const key = sortBy[0].key
-								const order = sortBy[0].order === 'asc' ? 1 : -1
+							items.sort((a, b) => {
+								for (const sort of sortBy) {
+									const key = sort.key
+									const r = String(a[key]).localeCompare(String(b[key]))
 
-								return a[key] > b[key] ? order : -order
+									const order = sort.order === 'asc' ? 1 : -1
+									if (r !== 0) {
+										return r * order
+									}
+								}
+								return 0
 							})
 						}
 
@@ -799,21 +824,41 @@ export const ServerMultiSort: Story = {
 
 				const getUsers = (): User[] => {
 					return [
-						{ firstname: 'Virginie', lastname: 'Beauchesne', email: 'virginie.beauchesne@example.com' },
-						{ firstname: 'Simone', lastname: 'Bellefeuille', email: 'simone.bellefeuille@example.com' },
-						{ firstname: 'Étienne', lastname: 'Salois', email: 'etienne.salois@example.com' },
-						{ firstname: 'Bernadette', lastname: 'Langelier', email: 'bernadette.langelier@example.com' },
-						{ firstname: 'Agate', lastname: 'Roy', email: 'agate.roy@example.com' },
-						{ firstname: 'Louis', lastname: 'Denis', email: 'louis.denis@example.com' },
-						{ firstname: 'Édith', lastname: 'Cartier', email: 'edith.cartier@example.com' },
-						{ firstname: 'Alphonse', lastname: 'Bouvier', email: 'alphonse.bouvier@example.com' },
-						{ firstname: 'Eustache', lastname: 'Dubois', email: 'eustache.dubois@example.com' },
-						{ firstname: 'Rosemarie', lastname: 'Quessy', email: 'rosemarie.quessy@example.com' },
-						{ firstname: 'Serge', lastname: 'Rivard', email: 'serge.rivard@example.com' },
-						{ firstname: 'Jacques', lastname: 'Demers', email: 'jacques.demers@example.com' },
-						{ firstname: 'Aimée', lastname: 'Josseaume', email: 'aimee.josseaume@example.com' },
-						{ firstname: 'Delphine', lastname: 'Robillard', email: 'delphine.robillard@example.com' },
-						{ firstname: 'Alexandre', lastname: 'Lazure', email: 'alexandre.lazure@example.com' },
+						{
+							firstname: 'Virginie',
+							lastname: 'Beauchesne',
+							email: 'virginie.beauchesne@example.com',
+						},
+						{
+							firstname: 'Simone',
+							lastname: 'Bellefeuille',
+							email: 'simone.bellefeuille@example.com',
+						},
+						{
+							firstname: 'Étienne',
+							lastname: 'Salois',
+							email: 'etienne.salois@example.com',
+						},
+						{
+							firstname: 'Thierry',
+							lastname: 'Bobu',
+							email: 'thierry.bobu@example.com',
+						},
+						{
+							firstname: 'Bernadette',
+							lastname: 'Langelier',
+							email: 'bernadette.langelier@exemple.com',
+						},
+						{
+							firstname: 'Agate',
+							lastname: 'Roy',
+							email: 'agate.roy@exemple.com',
+						},
+						{
+							firstname: 'Agate',
+							lastname: 'Beauchesne',
+							email: 'agate.beauchesne@exemple.com',
+						},
 					]
 				}
 
