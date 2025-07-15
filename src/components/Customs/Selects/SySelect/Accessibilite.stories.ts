193d0bbd7f3625bd7d238fdd4466c1c505ce9e9f
virginieVachet <vachetvirginie@gmail.com>
2025-07-15 17:06:44 +0200

    add xlsx

--- src/components/Customs/Selects/SySelect/Accessibilite.stories.ts
+++ src/components/Customs/Selects/SySelect/Accessibilite.stories.ts
@@ -271,6 +271,14 @@ export const Legende: StoryObj = {
 					Problèmes relevés par Tanaguru
 				  </div>
 				</div>
+        <div class="mt-4">
+					  <p>Rapport d’audit manuel : <a href="/audits/SySelect.xlsx" style="color:#0C41BD;">Voir le
+						  rapport</a></p>
+					  <p style="color: grey; font-size: 14px">Correctifs associés (<a
+						  href="https://github.com/assurance-maladie-digital/design-system-v3/issues/787" target="_blank"
+						  style="color:#0C41BD;"
+					  >issue #787</a>)</p>
+				  </div>
 			  </div>
             `,
 		}
