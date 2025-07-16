23bf5691052ef43a711a4763c02ef0822e58a145
Grégory Nicol <gregory.nicol@sogeti.com>
2025-07-16 10:59:37 +0200

    Commentaires sur correctifs config

--- vite.config.ts
+++ vite.config.ts
@@ -120,49 +120,7 @@ export default defineConfig({
 			'@tests': fileURLToPath(new URL('./tests', import.meta.url)),
 		},
 	},
-
 	// test: {
-	// 	coverage: {
-	// 		enabled: true,
-	// 		exclude: [
-	// 			'src/**/*.spec.{js,vue,ts}',
-	// 			'src/**/*.stories.*',
-	// 			'src/**/*ExpertiseLevelEnum.ts*',
-	// 			'src/**/*AccessibiliteItems.ts*',
-	// 			'src/**/tests/data/*',
-	// 			'src/**/constants/*',
-	// 			'src/composables/index.ts*',
-	// 			'src/**/types.*',
-	// 			'src/main.ts',
-	// 			'src/components/index.ts',
-	// 			'src/components/TestA11y.vue',
-	// 			'src/components/customizableOptions.vue',
-	// 			'src/components/gridsTests.vue',
-	// 			'src/components/TestDesignTokensComponent/*',
-	// 			...coverageConfigDefaults.exclude,
-	// 		],
-	// 		include: [
-	// 			'src/components/**/*.{js,vue,ts}',
-	// 			'src/composables/**',
-	// 			'src/utils/**',
-	// 		],
-	// 		provider: 'v8',
-	// 		reportsDirectory: './tests/unit/coverage',
-	// 		/* thresholds: {
-	// 			branches: 80,
-	// 			functions: 80,
-	// 			lines: 80,
-	// 			statements: 80,
-	// 		}, */
-	// 	},
-	// 	environment: 'happy-dom',
-	// 	server: {
-	// 		deps: {
-	// 			inline: ['vuetify'],
-	// 		},
-	// 	},
-	// 	snapshotSerializers: [
-	// 		'./node_modules/vue3-snapshot-serializer/index.js',
-	// 	],
+	// ... déplacé dans vitest.config.ts
 	// },
 })
