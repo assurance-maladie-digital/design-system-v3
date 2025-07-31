eedf9a271b87cfe097c4724e6c5ef80f7e25f116
BarryLaFraise <raphael.gerard@capgemini.com>
2025-07-31 16:52:35 +0200

    Feat/821 ajout composant amelipro custom selector (#888)
    * Ajout composant AmeliproCustomSelector
    
    * correction lint
    
    * correction erreur variable scss
    
    * mise à jour story Custom selector

---
+++ src/components/Amelipro/AmeliproCustomSelector/__tests__/AmeliproCustomSelector.spec.ts
@@ -0,0 +1,50 @@
+import { mount } from '@vue/test-utils'
+import { expect, describe, it } from 'vitest'
+import AmeliproCustomSelector from '../AmeliproCustomSelector.vue'
+import { vuetify } from '@tests/unit/setup'
+
+describe('AmeliproCustomSelector', () => {
+	it('render correctly', async () => {
+		const wrapper = mount(AmeliproCustomSelector, {
+			global: {
+				plugins: [vuetify],
+			},
+			props: {
+				groupLabel: 'Libellé du groupe',
+				modelValue: [
+					{
+						disabled: true,
+						isChecked: true,
+						label: '1',
+						value: 'Valeur 1',
+					},
+					{
+						description: 'ma-tooltip',
+						disabled: true,
+						isChecked: false,
+						label: '2',
+						value: 'Valeur 2',
+					},
+					{
+						isChecked: false,
+						label: '3',
+						value: 'Valeur 3',
+					},
+					{
+						isChecked: false,
+						label: '4',
+						value: 'Valeur 4',
+					},
+					{
+						isChecked: false,
+						label: '5',
+						value: 'Valeur 5',
+					},
+				],
+				uniqueId: 'my-custom-selector-group-id',
+			},
+		})
+
+		expect(wrapper.html()).toMatchSnapshot()
+	})
+})
