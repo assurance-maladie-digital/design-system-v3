119586b455af8d129362be4523e2ec869f144eed
adrien charrier <adrien.charrier@outlook.fr>
2025-08-01 11:53:54 +0200

    test

--- src/vuetifyConfig.ts
+++ src/vuetifyConfig.ts
@@ -1,4 +1,5 @@
-import { createVuetify } from 'vuetify'
+import './assets/themes.scss'
+
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
@@ -21,7 +22,7 @@ import {
 import { createFlattenTheme, createFontVariables } from './designTokens/utils'
 import { fr } from 'vuetify/locale'
 
-import './assets/themes.scss'
+import { createVuetify } from 'vuetify'
 
 export const createVuetifyInstance = () => createVuetify({
 	components,
