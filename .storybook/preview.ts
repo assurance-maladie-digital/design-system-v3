9a417017949805d3eb1bb9b289236de5aeb0b3ca
David FYON <davidfyon@gmail.com>
2025-08-01 15:25:55 +0200

    fix

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,7 @@
+// Import Storybook-specific settings BEFORE vuetify/styles to ensure $layers: false is applied
 import './settings.scss'
-import 'vuetify/styles'
+// Note: Do not import 'vuetify/styles' here as it conflicts with our custom settings
+// The styles will be loaded through the settings.scss @use directive
 import { watch } from 'vue'
 import './storybook.css'
 import type { Preview } from '@storybook/vue3'
