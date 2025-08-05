d2a647081300e86025774e0f60dbc58e9aa55470
Julien Roche <6241568+JulienRoche@users.noreply.github.com>
2025-08-05 14:47:52 +0200

    Doc: add page on false positives Tanaguru

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,2 +1,129 @@
+<script setup lang="ts">
+	// import { VThemeProvider } from 'vuetify/components'
+	// import HeaderBar from '../src/components/HeaderBar/HeaderBar.vue'
+	// import HeaderToolbar from '../src/components/HeaderToolbar/HeaderToolbar.vue'
+	// import SubHeader from '../src/components/SubHeader/SubHeader.vue'
+	// import HeaderBurgerMenu from '../src/components/HeaderBar/HeaderBurgerMenu/HeaderBurgerMenu.vue'
+	// import HeaderMenuSection from '../src/components/HeaderBar/HeaderBurgerMenu/HeaderMenuSection/HeaderMenuSection.vue'
+	// import HeaderMenuItem from '../src/components/HeaderBar/HeaderBurgerMenu/HeaderMenuItem/HeaderMenuItem.vue'
+	// import HeaderSubMenu from '../src/components/HeaderBar/HeaderBurgerMenu/HeaderSubMenu/HeaderSubMenu.vue'
+	// import PhoneField from '../src/components/PhoneField/PhoneField.vue'
+	import SySelect from '../src/components/Customs/Selects/SySelect/SySelect.vue'
+	// import { VBtn } from 'vuetify/components'
+	import { ref } from 'vue'
+
+	const selectedOptions = ref([])
+	const options = [
+		{ text: 'Adrien', value: 'Adrien' },
+		{ text: 'Axel', value: 'Axel' },
+		{ text: 'Baptiste', value: 'Baptiste' },
+		{ text: 'Clement', value: 'Clement' },
+		{ text: 'Corentin', value: 'Corentin' },
+		{ text: 'Damien', value: 'Damien' },
+		{ text: 'David', value: 'David' },
+		{ text: 'Eloi', value: 'Eloi' },
+		{ text: 'Louis', value: 'Louis' },
+		{ text: 'Valentin', value: 'Valentin' },
+	]
+</script>
+
 <template>
+    <p id="error-code" aria-hidden="true"><span class="sr-only">Code d'erreur </span>500</p>
+    <h1 aria-describedby="error-code">Une erreur est survenue</h1>
+	<p>Test 1</p>
+	<p>Test 2</p>
+	<p>Test 3</p>
+    <SySelect
+		v-model="selectedOptions"
+		:items="options"
+		label="Options"
+		multiple
+	/>
+	<div class="mt-4">
+		Options sélectionnées: {{ selectedOptions }}
+	</div>
+    <!-- <HeaderToolbar />
+    <HeaderBar
+        service-title="Synapse"
+        service-subtitle="Design System"
+    >
+		<template #header-side="{ menuOpen }">
+            <div class="d-flex justify-center h-100 ga-4 pr-4">
+                <VBtn
+                    variant="text"
+                    :prepend-icon="mdiMagnify"
+                    color="primary"
+                >
+                    Rechercher
+                </VBtn>
+                <VBtn
+                    color="primary"
+                    :prepend-icon="mdiAccountCircleOutline"
+                >
+                    Login
+                </VBtn>
+            </div>
+        </template>
+		<template #append>
+            <SubHeader
+                title-text="Paul Dupont"
+				sub-title-text="1 69 08 75 125 456 75"
+				:data-list-group-items="items"
+				@click:list-item="updateInfo"
+            />
+        </template>
+	</HeaderBar>
+	<HeaderNavigationBar
+		:items="[
+			{ label: 'Home', href: '' },
+			{ label: 'About', href: '' },
+		]"
+	/> -->
+
+	<!-- <HeaderBar>
+		<template #menu>
+			<HeaderBurgerMenu>
+				<HeaderMenuSection>
+					<HeaderMenuItem>
+						<a href="/">Item</a>
+					</HeaderMenuItem>
+					<HeaderMenuItem>
+						<a href="/">Item</a>
+					</HeaderMenuItem>
+					<headerMenuItem>
+						<HeaderSubMenu>
+							<template #title>
+								Menu de premier niveau
+							</template>
+							<HeaderMenuItem>
+								<a href="/">Item</a>
+							</HeaderMenuItem>
+							<HeaderMenuItem>
+								<a href="/">Item</a>
+							</HeaderMenuItem>
+							<HeaderMenuItem>
+								<a href="/">Item</a>
+							</HeaderMenuItem>
+						</HeaderSubMenu>
+					</headerMenuItem>
+					<HeaderMenuItem>
+						<a href="/">Item</a>
+					</HeaderMenuItem>
+				</HeaderMenuSection>
+			</HeaderBurgerMenu>
+		</template>
+	</HeaderBar> -->
+
+    <!-- <div
+        style="height: 200vh; background-color: #f5f5f5; margin: auto; margin-top: 2rem; max-width: 1200px; padding: 1em;"
+    >Contenu de la page</div> -->
+    <!-- <VThemeProvider theme="dark" with-background class="pa-4">
+        SelectedValue: {{ selectedValueDark }}
+        <SySelect
+            v-model="selectedValueDark"
+            :items="itemsDark"
+            text-key="text"
+            required
+        />
+    </VThemeProvider> -->
 </template>
---
+++ src/stories/Accessibilite/KitDePreAudit/Outils/Tanaguru/FauxPositifs.stories.ts
@@ -0,0 +1,211 @@
+import type { StoryObj } from '@storybook/vue3'
+import { computed, ref } from 'vue'
+
+export default {
+	title: 'Accessibilité/Kit de pré-audit/Outils/Tanaguru/Faux positifs',
+}
+
+export const FauxPositifsCards: StoryObj = {
+	render: () => {
+		return {
+			setup() {
+				const search = ref('')
+				const tanaguruCategoryFilter = ref('all')
+				const page = ref(1)
+				const itemsPerPage = 8
+
+				const items = [
+					{
+						tanaguruCategory: 'mandatory-elements',
+						tanaguruIssue: 'Propriétés ARIA non autorisées sur leur élément.',
+						component: 'Dialog',
+						explanation: 'Une boîte de dialogue de type modale doit posséder la balise aria-modal="true" pour indiquer qu’elle est modale.',
+					},
+				]
+
+				const tanaguruCategories = {
+					'colors': {
+						label: 'Couleurs',
+						color: '#f0b323',
+					},
+					'forms': {
+						label: 'Formulaires',
+						color: '#f97316',
+					},
+					'images': {
+						label: 'Images',
+						color: '#ddd',
+					},
+					'information-presentation': {
+						label: 'Présentation de l’information',
+						color: '#555555',
+					},
+					'information-structure': {
+						label: 'Structure de l’information',
+						color: '#a05bb6',
+					},
+					'links': {
+						label: 'Liens',
+						color: '#3b82f6',
+					},
+					'mandatory-elements': {
+						label: 'Éléments obligatoires',
+						color: '#cc3e50',
+					},
+					'navigation': {
+						label: 'Navigation',
+						color: '#4caf50',
+					},
+					'scripts': {
+						label: 'Scripts',
+						color: '#2f3e46',
+					},
+				}
+
+				// Options de filtrage par catégorie Tanaguru
+				const tanaguruCategoryOptions = [
+					{ title: 'Toutes les catégories', value: 'all' },
+					...Object.entries(tanaguruCategories).map(
+						([value, { label }]) => ({ title: label, value }),
+					),
+				]
+
+				// Filtrer les items en fonction de la recherche et du statut
+				const filteredItems = computed(() => {
+					let result = [...items]
+
+					// Filtrage par statut
+					if (tanaguruCategoryFilter.value !== 'all') {
+						result = result.filter(({ tanaguruCategory }) => tanaguruCategory === tanaguruCategoryFilter.value)
+					}
+
+					// Filtrage par recherche
+					if (search.value) {
+						const searchTerm = search.value.toLowerCase()
+						result = result.filter(({ tanaguruIssue, component }) => tanaguruIssue.toLowerCase().includes(searchTerm) || component.toLowerCase().includes(searchTerm))
+					}
+
+					return result
+				})
+
+				const totalItems = computed(() => items.length)
+
+				// Calcul des éléments paginés
+				const paginatedItems = computed(() => {
+					const start = (page.value - 1) * itemsPerPage
+					const end = start + itemsPerPage
+					return filteredItems.value.slice(start, end)
+				})
+
+				// Permet de mettre en évidence les correspondances dans le texte
+				const highlightMatch = (text, search) => {
+					if (!search) return text
+
+					const escapedSearch = search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') // Échappe les caractères spéciaux
+					const regex = new RegExp(`(${escapedSearch})`, 'gi')
+
+					return text.replace(regex, '<mark>$1</mark>')
+				}
+
+				return { filteredItems, highlightMatch, items, itemsPerPage, page, paginatedItems, search, tanaguruCategories, tanaguruCategoryFilter, tanaguruCategoryOptions, totalItems }
+			},
+			template: `
+				<div class="audit-dashboard pa-4">
+					<div class="d-flex flex-wrap align-center justify-space-between mb-6">
+						<div class="d-flex flex-wrap">
+							<v-select
+								v-model="tanaguruCategoryFilter"
+								:items="tanaguruCategoryOptions"
+								item-title="title"
+								item-value="value"
+								label="Filtrer par catégorie Tanaguru"
+								hide-details
+								class="mr-2 mb-2"
+								style="max-width: 250px; min-width: 250px"
+								density="comfortable"
+								variant="outlined"
+							></v-select>
+
+							<v-text-field
+								v-model="search"
+								label="Rechercher (composant, problème Tanaguru)"
+								append-inner-icon="mdi-magnify"
+								hide-details
+								class="mb-2"
+								style="max-width: 350px; min-width: 350px"
+								density="comfortable"
+								variant="outlined"
+							></v-text-field>
+						</div>
+					</div>
+
+					<div class="component-grid mb-6">
+
+						<v-row>
+							<v-col
+								v-for="item in paginatedItems"
+								:key="item.component"
+								cols="12"
+								sm="12"
+								md="6"
+							>
+								<v-card
+									class="component-card"
+									elevation="2"
+									height="100%"
+								>
+									<v-card-item>
+										<v-card-title
+											:aria-label="'Composant : ' + item.component"
+											:title="'Composant : ' + item.component"
+											role="heading"
+											aria-level="3"
+											class="text-truncate"
+											v-html="highlightMatch(item.component, search)"
+										></v-card-title>
+									</v-card-item>
+
+									<v-card-text>
+										<v-chip
+											:color="tanaguruCategories[item.tanaguruCategory].color"
+											variant="flat"
+											class="mb-2"
+											:aria-label="'Catégorie Tanaguru : ' + tanaguruCategories[item.tanaguruCategory].label"
+											:title="'Catégorie Tanaguru : ' + tanaguruCategories[item.tanaguruCategory].label"
+										>
+											{{ tanaguruCategories[item.tanaguruCategory].label }}
+										</v-chip>
+
+										<dl class="mt-1">
+											<dt class="text-subtitle-3 font-weight-bold">Problème Tanaguru :</dt>
+											<dd class="text-body-3 mb-3" v-html="highlightMatch(item.tanaguruIssue, search)"></dd>
+											<dt class="text-subtitle-3 font-weight-bold">Explication :</dt>
+											<dd class="text-body-3">{{ item.explanation }}</dd>
+										</dl>
+									</v-card-text>
+								</v-card>
+							</v-col>
+						</v-row>
+
+						<div class="d-flex justify-center mt-4">
+							<v-pagination
+								v-model="page"
+								:length="Math.ceil(filteredItems.length / itemsPerPage)"
+								total-visible="7"
+								rounded
+							></v-pagination>
+						</div>
+
+						<div class="d-flex pa-2 justify-space-between">
+							<div class="text-caption">Total : {{ totalItems }} </div>
+							<div class="text-caption text-right">
+								Dernière mise à jour: 05/08/2025
+							</div>
+						</div>
+					</div>
+				</div>
+			`,
+		}
+	},
+	tags: ['!dev'],
+}
