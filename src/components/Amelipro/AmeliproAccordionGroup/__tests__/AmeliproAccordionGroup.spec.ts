e5d7c3a8f6004d3f205ce09ca8140e501ba13de8
RAPHAEL GERARD <raphael.gerard@capgemini.com>
2025-08-11 09:18:08 +0200

    correction lint

--- src/components/Amelipro/AmeliproAccordionGroup/__tests__/AmeliproAccordionGroup.spec.ts
+++ src/components/Amelipro/AmeliproAccordionGroup/__tests__/AmeliproAccordionGroup.spec.ts
@@ -4,34 +4,34 @@ import { vuetify } from '@tests/unit/setup'
 import AmeliproAccordionGroup from '../AmeliproAccordionGroup.vue'
 
 describe('AmeliproAccordionGroup', () => {
-    it('render correctly', async () => {
-        const wrapper = mount(AmeliproAccordionGroup, {
-            global: {
-                plugins: [vuetify],
-            },
-            props: {
-               items: [
-                    {
-                        id: 'test-1',
-                        title: 'Exemple 1',
-                    },
-                    {
-                        id: 'test-2',
-                        title: 'Exemple 2',
-                    },
-                    {
-                        id: 'test-3',
-                        title: 'Exemple 3',
-                    },
-                    {
-                        id: 'test-4',
-                        title: 'Exemple 4',
-                    },
-                ],
-                uniqueId: 'amelipro-accordion-group-unique-id',
-            },
-        })
+	it('render correctly', async () => {
+		const wrapper = mount(AmeliproAccordionGroup, {
+			global: {
+				plugins: [vuetify],
+			},
+			props: {
+				items: [
+					{
+						id: 'test-1',
+						title: 'Exemple 1',
+					},
+					{
+						id: 'test-2',
+						title: 'Exemple 2',
+					},
+					{
+						id: 'test-3',
+						title: 'Exemple 3',
+					},
+					{
+						id: 'test-4',
+						title: 'Exemple 4',
+					},
+				],
+				uniqueId: 'amelipro-accordion-group-unique-id',
+			},
+		})
 
-        expect(wrapper.html()).toMatchSnapshot()
-    })
+		expect(wrapper.html()).toMatchSnapshot()
+	})
 })
