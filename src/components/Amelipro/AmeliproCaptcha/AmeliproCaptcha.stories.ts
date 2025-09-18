ee7fcb9863f7ebe74988a3ede74d4ff587a01369
Grégory NICOL <gnicol-bzh@users.noreply.github.com>
2025-09-18 09:33:43 +0200

    Feat/1111 amelipro mise a jour exemples amelipro captcha customselector (#1116)
    * Ajout d'exemples pour AmeliproCaptcha
    
    * Correction plusieurs boutons sur la même ligne
    
    * Ajout d'exemples pour AmeliproCustomSelector
    
    * AmeliproCustomSelector : correction exemple info-bulle
    
    * AmeliproIllustratedRadioGroup: ajout d'exemples
    
    * AmeliproRadioGroup : ajout d'exemples
    
    * AmeliproSelect : ajout d'exemples
    
    * Mise à jour des snapshots

--- src/components/Amelipro/AmeliproCaptcha/AmeliproCaptcha.stories.ts
+++ src/components/Amelipro/AmeliproCaptcha/AmeliproCaptcha.stories.ts
@@ -85,3 +85,270 @@ export const Default: Story = {
 		`,
 	}),
 }
+
+export const AudioParDefaut: Story = {
+	args: {
+		modelValue: {
+			audioSrc: '/amelipro/sound/captcha.wav',
+			imgSrc: '/amelipro/img/captcha.png',
+			inputValue: '',
+		},
+		uniqueId: 'amelipro-captcha-audio-default',
+		defaultAudio: true,
+	},
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `<template>
+  <p>Le captcha s'affiche en mode audio par défaut grâce à la prop <code>defaultAudio</code>.</p>
+  <AmeliproCaptcha
+    v-model="model"
+    unique-id="amelipro-captcha-audio-default"
+    default-audio
+  />
+</template>`,
+			},
+			{
+				name: 'Script',
+				code: `<script setup lang="ts">
+  import { AmeliproCaptcha } from '@cnamts/synapse'
+  import { ref } from 'vue'
+  const model = ref({
+    audioSrc: '/amelipro/sound/captcha.wav',
+    imgSrc: '/amelipro/img/captcha.png',
+    inputValue: '',
+  })
+</script>`,
+			},
+		],
+	},
+	render: args => ({
+		components: { AmeliproCaptcha },
+		setup() {
+			const model = ref(args.modelValue)
+			watch(() => args.modelValue, (newValue) => {
+				model.value = newValue
+			})
+			return { args, model }
+		},
+		template: `<p class="mb-2">Le captcha s'affiche en mode audio par défaut grâce à la prop <code>defaultAudio</code>.</p>
+<AmeliproCaptcha v-bind="args" v-model="model" />`,
+	}),
+}
+
+export const LabelsPersonnalises: Story = {
+	args: {
+		modelValue: {
+			audioSrc: '/amelipro/sound/captcha.wav',
+			imgSrc: '/amelipro/img/captcha.png',
+			inputValue: '',
+		},
+		uniqueId: 'amelipro-captcha-labels',
+		captchaInputLabelAudio: 'Code audio personnalisé :',
+		captchaInputLabelImg: 'Code image personnalisé :',
+		captchaTitle: 'Titre personnalisé',
+	},
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `<template>
+  <p>Les labels et le titre sont personnalisés via les props <code>captchaInputLabelAudio</code>, <code>captchaInputLabelImg</code> et <code>captchaTitle</code>.</p>
+  <AmeliproCaptcha
+    v-model="model"
+    unique-id="amelipro-captcha-labels"
+    captcha-input-label-audio="Code audio personnalisé :"
+    captcha-input-label-img="Code image personnalisé :"
+    captcha-title="Titre personnalisé"
+  />
+</template>`,
+			},
+			{
+				name: 'Script',
+				code: `<script setup lang="ts">
+  import { AmeliproCaptcha } from '@cnamts/synapse'
+  import { ref } from 'vue'
+  const model = ref({
+    audioSrc: '/amelipro/sound/captcha.wav',
+    imgSrc: '/amelipro/img/captcha.png',
+    inputValue: '',
+  })
+</script>`,
+			},
+		],
+	},
+	render: args => ({
+		components: { AmeliproCaptcha },
+		setup() {
+			const model = ref(args.modelValue)
+			watch(() => args.modelValue, (newValue) => {
+				model.value = newValue
+			})
+			return { args, model }
+		},
+		template: `<p class="mb-2">Les labels et le titre sont personnalisés via les props <code>captchaInputLabelAudio</code>, <code>captchaInputLabelImg</code> et <code>captchaTitle</code>.</p>
+<AmeliproCaptcha v-bind="args" v-model="model" />`,
+	}),
+}
+
+export const ImagePersonnalisee: Story = {
+	args: {
+		modelValue: {
+			audioSrc: '/amelipro/sound/captcha.wav',
+			imgSrc: '/amelipro/img/captcha.png',
+			inputValue: '',
+		},
+		uniqueId: 'amelipro-captcha-img',
+		imgAlt: 'Captcha personnalisé',
+		imgWidth: '200px',
+		imgMaxWidth: '80%',
+	},
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `<template>
+  <p>L’image du captcha est personnalisée via les props <code>imgAlt</code>, <code>imgWidth</code> et <code>imgMaxWidth</code>.</p>
+  <AmeliproCaptcha
+    v-model="model"
+    unique-id="amelipro-captcha-img"
+    img-alt="Captcha personnalisé"
+    img-width="200px"
+    img-max-width="80%"
+  />
+</template>`,
+			},
+			{
+				name: 'Script',
+				code: `<script setup lang="ts">
+  import { AmeliproCaptcha } from '@cnamts/synapse'
+  import { ref } from 'vue'
+  const model = ref({
+    audioSrc: '/amelipro/sound/captcha.wav',
+    imgSrc: '/amelipro/img/captcha.png',
+    inputValue: '',
+  })
+</script>`,
+			},
+		],
+	},
+	render: args => ({
+		components: { AmeliproCaptcha },
+		setup() {
+			const model = ref(args.modelValue)
+			watch(() => args.modelValue, (newValue) => {
+				model.value = newValue
+			})
+			return { args, model }
+		},
+		template: `<p class="mb-2">L’image du captcha est personnalisée via les props <code>imgAlt</code>, <code>imgWidth</code> et <code>imgMaxWidth</code>.</p>
+<AmeliproCaptcha v-bind="args" v-model="model" />`,
+	}),
+}
+
+export const BoutonsDeMiseAJour: Story = {
+	args: {
+		modelValue: {
+			audioSrc: '/amelipro/sound/captcha.wav',
+			imgSrc: '/amelipro/img/captcha.png',
+			inputValue: '',
+		},
+		uniqueId: 'amelipro-captcha-btns',
+		updateCaptchaLabelAudio: 'Nouveau son',
+		updateCaptchaLabelImg: 'Nouvelle image',
+	},
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `<template>
+  <p>Les libellés des boutons de mise à jour (à gauche) sont personnalisés via les props <code>updateCaptchaLabelAudio</code> et <code>updateCaptchaLabelImg</code>.</p>
+  <AmeliproCaptcha
+    v-model="model"
+    unique-id="amelipro-captcha-btns"
+    update-captcha-label-audio="Nouveau son"
+    update-captcha-label-img="Nouvelle image"
+  />
+</template>`,
+			},
+			{
+				name: 'Script',
+				code: `<script setup lang="ts">
+  import { AmeliproCaptcha } from '@cnamts/synapse'
+  import { ref } from 'vue'
+  const model = ref({
+    audioSrc: '/amelipro/sound/captcha.wav',
+    imgSrc: '/amelipro/img/captcha.png',
+    inputValue: '',
+  })
+</script>`,
+			},
+		],
+	},
+	render: args => ({
+		components: { AmeliproCaptcha },
+		setup() {
+			const model = ref(args.modelValue)
+			watch(() => args.modelValue, (newValue) => {
+				model.value = newValue
+			})
+			return { args, model }
+		},
+		template: `<p class="mb-2">Les libellés des boutons de mise à jour (à gauche) sont personnalisés via les props <code>updateCaptchaLabelAudio</code> et <code>updateCaptchaLabelImg</code>.</p>
+<AmeliproCaptcha v-bind="args" v-model="model" />`,
+	}),
+}
+
+export const BoutonsDeBascule: Story = {
+	args: {
+		modelValue: {
+			audioSrc: '/amelipro/sound/captcha.wav',
+			imgSrc: '/amelipro/img/captcha.png',
+			inputValue: '',
+		},
+		uniqueId: 'amelipro-captcha-switch',
+		switchCaptchaTypeLabelAudio: 'Basculer vers image',
+		switchCaptchaTypeLabelImg: 'Basculer vers audio',
+	},
+	parameters: {
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `<template>
+  <p>Les libellés des boutons de bascule (à droite) sont personnalisés via les props <code>switchCaptchaTypeLabelAudio</code> et <code>switchCaptchaTypeLabelImg</code>.</p>
+  <AmeliproCaptcha
+    v-model="model"
+    unique-id="amelipro-captcha-switch"
+    switch-captcha-type-label-audio="Basculer vers image"
+    switch-captcha-type-label-img="Basculer vers audio"
+  />
+</template>`,
+			},
+			{
+				name: 'Script',
+				code: `<script setup lang="ts">
+  import { AmeliproCaptcha } from '@cnamts/synapse'
+  import { ref } from 'vue'
+  const model = ref({
+    audioSrc: '/amelipro/sound/captcha.wav',
+    imgSrc: '/amelipro/img/captcha.png',
+    inputValue: '',
+  })
+</script>`,
+			},
+		],
+	},
+	render: args => ({
+		components: { AmeliproCaptcha },
+		setup() {
+			const model = ref(args.modelValue)
+			watch(() => args.modelValue, (newValue) => {
+				model.value = newValue
+			})
+			return { args, model }
+		},
+		template: `<p class="mb-2">Les libellés des boutons de bascule (à droite) sont personnalisés via les props <code>switchCaptchaTypeLabelAudio</code> et <code>switchCaptchaTypeLabelImg</code>.</p>
+<AmeliproCaptcha v-bind="args" v-model="model" />`,
+	}),
+}
