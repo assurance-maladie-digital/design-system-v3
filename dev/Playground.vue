16eebfa4299da12b44ce1319854f8dfcc780d814
adrien <adrien.charrier@assurance-maladie.fr>
2025-09-30 10:21:32 +0200

    fix/remove-playground

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,20 +1,2 @@
-<script setup lang="ts">
-	import RatingPicker from '@/components/RatingPicker/RatingPicker.vue';
-	import { ref } from 'vue'
-
-	const ratingStar = ref(-1)
-	const disabled = ref(false)
-</script>
-
 <template>
-	<RatingPicker
-		v-model="ratingStar"
-		label="Êtes-vous satisfait de ce service ?"
-		type="stars"
-		:readonly="disabled"
-	/>
-
-	<button @click="disabled = !disabled">
-		{{ disabled ? 'Activer' : 'Désactiver' }} la notation
-	</button>
 </template>
