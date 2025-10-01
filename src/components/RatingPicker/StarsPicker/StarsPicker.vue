faaeee85eac71876d55270b88bf529aa66816342
Adrien <65124433+4dr1en@users.noreply.github.com>
2025-10-01 11:03:30 +0200

    RatingPicker fixes post Audit rgaa (#1159)
    * feat: enhance RatingPicker component with accessibility improvements
    
    * refactor: remove console logs from useRating function
    
    * feat: add ripple effect to rating elements in EmotionPicker and NumberPicker components
    
    * fix: disabled
    
    * feat: add accessibility improvements and audit report link to RatingPicker component
    
    * fix: correct CSS property value and improve hover focus styles for disabled items
    
    * fix/tanaguru-label
    
    * fix/lint
    
    * fix/remove-playground
    
    * fix: update aria attributes for accessibility and remove redundant code in RatingPicker components
    
    ---------
    
    Co-authored-by: AdmPMF-107501-32221 <AdmPMF-107501-32221@cnamts.local>
    Co-authored-by: Adrien Charrier <adrien.charrier@assurance-maladie.fr>

--- src/components/RatingPicker/StarsPicker/StarsPicker.vue
+++ src/components/RatingPicker/StarsPicker/StarsPicker.vue
@@ -2,6 +2,8 @@
 	import { ref, type PropType, onMounted } from 'vue'
 	import { RatingEnum, useRating } from '../Rating'
 	import { mdiStarOutline, mdiStar } from '@mdi/js'
+	import SyIcon from '@/components/Customs/SyIcon/SyIcon.vue'
+	import { locales } from '../locales'
 
 	const props = defineProps({
 		label: {
@@ -30,74 +32,108 @@
 	const hoverIndex = ref<number | null>(-1)
 
 	function isActive(index: number): boolean {
-		return props.modelValue - 1 === index
+		return props.modelValue === index
 	}
 
 	function isFilled(index: number): boolean {
-		const isHovered = hoverIndex.value !== null && hoverIndex.value >= index
-		const isActive = props.modelValue - 1 >= index
-		return isHovered || isActive
+		const isHovered = hoverIndex.value !== null && hoverIndex.value >= index && !props.readonly
+		const isActive = props.modelValue >= index
+		return (isHovered && !hasAnswered.value) || isActive
+	}
+
+	const ratingElement = ref<HTMLDivElement[]>([])
+	function focusNextElement(index: number) {
+		const currentIndex = ratingElement.value?.findIndex(el => el === ratingElement.value[index]) ?? -1
+		const nextIndex = currentIndex < (props.length - 1) ? currentIndex + 1 : props.length - 1
+		const nextElem = ratingElement.value?.[nextIndex]
+		nextElem?.focus()
+	}
+
+	function focusPrevElement(index: number) {
+		const currentIndex = ratingElement.value?.findIndex(el => el === ratingElement.value[index]) ?? -1
+		const prevIndex = currentIndex > 0 ? currentIndex - 1 : 0
+		const prevElem = ratingElement.value?.[prevIndex]
+		prevElem?.focus()
+	}
+
+	function setFocus(index: number) {
+		ratingElement.value.forEach((el, i) => {
+			if (i === index) {
+				el?.setAttribute('tabindex', '0')
+			}
+			else {
+				el?.setAttribute('tabindex', '-1')
+			}
+		})
 	}
 
 	onMounted(() => {
-		const hiddenInputs = document.querySelectorAll('.v-rating__hidden')
-		hiddenInputs.forEach((input) => {
-			(input as HTMLElement).setAttribute('aria-hidden', 'true')
-		})
+		ratingElement.value[0]?.setAttribute('tabindex', '0')
+		for (let i = 1; i < ratingElement.value.length; i++) {
+			ratingElement.value[i]?.setAttribute('tabindex', '-1')
+		}
 	})
 </script>
 
 <template>
-	<fieldset class="vd-stars-picker">
+	<fieldset class="sy-stars-picker">
 		<legend class="text-h6 mb-6">
 			<slot name="label">
 				{{ props.label }}
 			</slot>
 		</legend>
 
-		<VRating
-			:model-value="props.modelValue"
-			:length="props.length"
-			:readonly="props.readonly || hasAnswered"
-			class="d-flex flex-wrap max-width-none mx-n3"
-			@update:model-value="(value) => emit('update:modelValue', value)"
+		<div
+			role="radiogroup"
+			class="d-flex max-width-none mx-n1 mx-sm-n2"
 		>
-			<template #item="{ index }">
-				<button
-					:disabled="props.readonly"
-					:aria-pressed="isActive(index)"
-					:aria-label="`${index + 1} star`"
-					@mouseover="hoverIndex = index"
-					@focus="hoverIndex = index"
-					@mouseleave="hoverIndex = -1"
-					@blur="hoverIndex = -1"
-					@click="emitInputEvent(index + 1)"
-				>
-					<VIcon
-						:class="
-							isFilled(index)
-								? 'text-primary'
-								: 'text-blue-lighten'
-						"
-						size="36px"
-						class="py-0 px-2"
-					>
-						{{ isFilled(index) ? starIcon : starOutlineIcon }}
-					</VIcon>
-				</button>
-			</template>
-		</VRating>
+			<div
+				v-for="index in props.length"
+				:key="index"
+				ref="ratingElement"
+				class="sy-stars-picker__item d-flex align-center justify-center"
+				role="radio"
+				:aria-disabled="(props.readonly || hasAnswered) ? 'true' : undefined"
+				:aria-checked="isActive(index) ? 'true' : undefined"
+				@mouseover="hoverIndex = index"
+				@focus="hoverIndex = index"
+				@mouseleave="hoverIndex = -1"
+				@blur="hoverIndex = -1"
+				@click="emitInputEvent(index); setFocus(index - 1)"
+				@keyup.enter="emitInputEvent(index); setFocus(index - 1)"
+				@keyup.space="emitInputEvent(index); setFocus(index - 1)"
+				@keyup.right="focusNextElement(index - 1)"
+				@keyup.left="focusPrevElement(index - 1)"
+			>
+				<span class="d-sr-only">{{ locales.etoiles(index) }}</span>
+				<SyIcon
+					:icon="isFilled(index) ? starIcon : starOutlineIcon"
+					:class="
+						isFilled(index)
+							? 'text-primary'
+							: 'text-blue-lighten'
+					"
+					size="36px"
+					class="py-0 px-2"
+					decorative
+				/>
+			</div>
+		</div>
 	</fieldset>
 </template>
 
 <style lang="scss" scoped>
 @use '@/assets/tokens';
 
-.vd-stars-picker {
+.sy-stars-picker {
 	border: 0;
 }
 
-.v-rating .v-icon {
+.sy-stars-picker__item:not([disabled]) {
+	cursor: pointer;
+}
+
+.sy-stars-picker__item .v-icon {
 	width: 52px !important;
 	height: 36px !important;
 
