1c1b11741acd025083fd9438bbe26029fd949851
RAPHAEL GERARD <raphael.gerard@capgemini.com>
2025-10-06 15:37:36 +0200

    corrections lint

--- src/components/Amelipro/AmeliproPatientLogin/AmeliproPatientLoginForm/__tests__/AmeliproPatientLoginForm.spec.ts
+++ src/components/Amelipro/AmeliproPatientLogin/AmeliproPatientLoginForm/__tests__/AmeliproPatientLoginForm.spec.ts
@@ -4,43 +4,43 @@ import AmeliproPatientLoginForm from '../AmeliproPatientLoginForm.vue'
 import { vuetify } from '@tests/unit/setup'
 
 describe('AmeliproPatientLoginForm', () => {
-    it('render correctly', async () => {
-        const wrapper = mount(AmeliproPatientLoginForm, {
-            global: {
-                plugins: [vuetify],
-            },
-            props: {
-                autoCompleteCardItems: [
-                    {
-                        title: 'Carte 1',
-                        value: '1',
-                    },
-                    {
-                        title: 'Carte 2',
-                        value: '2',
-                    },
-                    {
-                        title: 'Carte 3',
-                        value: '3',
-                    },
-                ],
-                disablePatientLoginFormAppVitalCard: true,
-                disablePatientLoginFormNir: true,
-                disablePatientLoginFormVitalCard: true,
-                errorMessageAppVitalCard: true,
-                errorMessageNir: true,
-                errorMessageVitalCard: true,
-                loading: true,
-                modelValue: {
-                    fieldValue: 'test',
-                    autoCompleteValue: '2',
-                },
-                noAppVitalCard: false,
-                noNir: false,
-                noVitalCard: false,
-                uniqueId: 'test-id',
-            },
-        })
-        expect(wrapper.html()).toMatchSnapshot()
-    })
+	it('render correctly', async () => {
+		const wrapper = mount(AmeliproPatientLoginForm, {
+			global: {
+				plugins: [vuetify],
+			},
+			props: {
+				autoCompleteCardItems: [
+					{
+						title: 'Carte 1',
+						value: '1',
+					},
+					{
+						title: 'Carte 2',
+						value: '2',
+					},
+					{
+						title: 'Carte 3',
+						value: '3',
+					},
+				],
+				disablePatientLoginFormAppVitalCard: true,
+				disablePatientLoginFormNir: true,
+				disablePatientLoginFormVitalCard: true,
+				errorMessageAppVitalCard: true,
+				errorMessageNir: true,
+				errorMessageVitalCard: true,
+				loading: true,
+				modelValue: {
+					fieldValue: 'test',
+					autoCompleteValue: '2',
+				},
+				noAppVitalCard: false,
+				noNir: false,
+				noVitalCard: false,
+				uniqueId: 'test-id',
+			},
+		})
+		expect(wrapper.html()).toMatchSnapshot()
+	})
 })
--- src/components/Amelipro/AmeliproPatientLogin/__tests__/AmeliproPatientLogin.spec.ts
+++ src/components/Amelipro/AmeliproPatientLogin/__tests__/AmeliproPatientLogin.spec.ts
@@ -4,46 +4,46 @@ import AmeliproPatientLogin from '../AmeliproPatientLogin.vue'
 import { vuetify } from '@tests/unit/setup'
 
 describe('AmeliproPatientLogin', () => {
-    it('render correctly', async () => {
-        const wrapper = mount(AmeliproPatientLogin, {
-            global: {
-                plugins: [vuetify],
-            },
-            props: {
-                autoCompleteCardItems: [
-                    {
-                        title: 'Carte 1',
-                        value: '1',
-                    },
-                    {
-                        title: 'Carte 2',
-                        value: '2',
-                    },
-                    {
-                        title: 'Carte 3',
-                        value: '3',
-                    },
-                ],
-                disableBtnAppVitalCard: true,
-                disableBtnNir: true,
-                disableBtnVitalCard: true,
-                errorMessageAppVitalCard: true,
-                errorMessageNir: true,
-                errorMessageVitalCard: true,
-                loading: true,
-                modelValue: {
-                    dialog: false,
-                    formValue: {
-                        fieldValue: 'test',
-                        autoCompleteValue: '2',
-                    },
-                },
-                noAppVitalCard: false,
-                noNir: false,
-                noVitalCard: false,
-                uniqueId: 'test-id',
-            },
-        })
-        expect(wrapper.html()).toMatchSnapshot()
-    })
+	it('render correctly', async () => {
+		const wrapper = mount(AmeliproPatientLogin, {
+			global: {
+				plugins: [vuetify],
+			},
+			props: {
+				autoCompleteCardItems: [
+					{
+						title: 'Carte 1',
+						value: '1',
+					},
+					{
+						title: 'Carte 2',
+						value: '2',
+					},
+					{
+						title: 'Carte 3',
+						value: '3',
+					},
+				],
+				disableBtnAppVitalCard: true,
+				disableBtnNir: true,
+				disableBtnVitalCard: true,
+				errorMessageAppVitalCard: true,
+				errorMessageNir: true,
+				errorMessageVitalCard: true,
+				loading: true,
+				modelValue: {
+					dialog: false,
+					formValue: {
+						fieldValue: 'test',
+						autoCompleteValue: '2',
+					},
+				},
+				noAppVitalCard: false,
+				noNir: false,
+				noVitalCard: false,
+				uniqueId: 'test-id',
+			},
+		})
+		expect(wrapper.html()).toMatchSnapshot()
+	})
 })
