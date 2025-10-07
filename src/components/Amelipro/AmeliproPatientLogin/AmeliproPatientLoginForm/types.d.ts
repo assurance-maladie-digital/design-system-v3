2875afb9e108c907979ad2ee6fa8c32932bf70b5
BarryLaFraise <raphael.gerard@capgemini.com>
2025-10-07 09:39:17 +0200

    Feat/1223 ajout composant amelipro patient login (#1241)
    * Ajout du composant AmeliproPatientLogin + correction anomalie mineure AmeliproErrorTemplate
    
    * corrections lint
    
    * changement syntaxe
    
    * ajout de condition sur les observe()
    
    * ajout actions à la story

---
+++ src/components/Amelipro/AmeliproPatientLogin/AmeliproPatientLoginForm/types.d.ts
@@ -0,0 +1,6 @@
+import { AutoCompleteItem } from '../../AmeliproAutoCompleteField/types'
+
+export interface IPatientLoginForm {
+	fieldValue?: string | number
+	autoCompleteValue?: AutoCompleteItem | string
+}
