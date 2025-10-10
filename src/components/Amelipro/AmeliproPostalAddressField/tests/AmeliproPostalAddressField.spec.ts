93e1b9893beba52388dc78bab632c53e00a9a9e0
RAPHAEL GERARD <raphael.gerard@capgemini.com>
2025-10-10 14:59:18 +0200

    Ajout composant AmeliproPostalAddressField + correction mineure style AmeliproRadioGroup

---
+++ src/components/Amelipro/AmeliproPostalAddressField/tests/AmeliproPostalAddressField.spec.ts
@@ -0,0 +1,27 @@
+import { mount } from '@vue/test-utils'
+import { expect, describe, it } from 'vitest'
+import AmeliproPostalAddressField from '../AmeliproPostalAddressField.vue'
+import { vuetify } from '@tests/unit/setup'
+
+describe('AmeliproPostalAddressField', () => {
+    it('render correctly', async () => {
+        const wrapper = mount(AmeliproPostalAddressField, {
+            global: {
+                plugins: [vuetify],
+            },
+            props: {
+                ariaRequired: true,
+                uniqueId: 'the-groupId',
+                groupLabel: 'the-groupLabel',
+                modelValue: {
+                    additionalInfo: 'the-additionalInfo',
+                    address: 'the-address',
+                    postalCode: 'the-postalCode',
+                    city: 'the-city',
+                },
+            },
+        })
+
+        expect(wrapper.html()).toMatchSnapshot()
+    })
+})
