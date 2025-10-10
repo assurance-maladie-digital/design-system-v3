e7a1d2a68593e2e3593836642c0256bdf3f3f6c7
Adrien Charrier <adrien.charrier@assurance-maladie.fr>
2025-10-10 10:16:56 +0200

    feat(RangeField): enhance accessibility and add new localization strings

--- src/components/FilterInline/locales.ts
+++ src/components/FilterInline/locales.ts
@@ -1,4 +1,8 @@
 export const locales = {
 	badgeLabel: (count: number): string =>
 		`${count} filtre${count > 1 ? 's' : ''}`,
+	filterActivatedStatus: (count: number, values: string, field: string): string =>
+		count > 1
+			? `Les filtres "${values}" pour "${field}" ont été activés. Pensez à soumettre le formulaire pour appliquer les filtres sur les recherches.`
+			: `Le filtre "${values}" pour "${field}" a été activé. Pensez à soumettre le formulaire pour appliquer le filtre sur les recherches.`,
 } as const
