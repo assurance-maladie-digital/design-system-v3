8fff11c4dc9392005b82cb22c602fa1d154dd821
David FYON <davidfyon@gmail.com>
2025-10-10 10:48:17 +0200

    lint

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,23 +1,2 @@
-<script setup lang="ts">
-	import DialogBox from '@/components/DialogBox/DialogBox.vue'
-	import { ref } from 'vue'
-
-	const dialogOpen = ref(false)
-</script>
-
 <template>
-	<VBtn
-		color="primary"
-		@click="dialogOpen = !dialogOpen"
-	>
-		Toggle DialogBox
-	</VBtn>
-	<DialogBox
-		v-model="dialogOpen"
-		title="DialogBox title"
-		@confirm="dialogOpen = false"
-		@cancel="dialogOpen = false"
-	>
-		DialogBox content
-	</DialogBox>
 </template>
