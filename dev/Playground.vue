5132c4ecbb64a3645934ca71e1e3cf928749eba0
David FYON <davidfyon@gmail.com>
2025-10-17 15:03:58 +0200

    fix story

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,2 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import NirField from '@/components/NirField/NirField.vue'
+
+const value = ref('')
+const nirField = ref()
+
+const onSubmit = async () => {
+  const isValid = await nirField.value?.validateOnSubmit()
+
+  if (isValid) {
+    alert('Formulaire soumis avec succès !')
+  }
+  else {
+    alert('Formulaire non soumis !')
+  }
+}
+</script>
+
 <template>
+  <form @submit.prevent="onSubmit">
+  <NirField
+      v-model="value"
+      label="NirField"
+      required
+      showSuccessMessages
+      :displayKey="false"
+      ref="nirField"
+  />
+  <v-btn
+      type="submit"
+      color="primary"
+      class="mt-4"
+  >
+    Valider
+  </v-btn>
+</form>
 </template>
