2648f9df1f95bbd56f67b8f6796d36b9a45ba64e
David FYON <davidfyon@gmail.com>
2025-10-17 16:25:05 +0200

    add ex

---
+++ dev/CustomPlaygrounds/Demo/NirFieldHideDetails.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+	import NirField from '@/components/NirField/NirField.vue'
+	import { ref } from 'vue'
+
+	const value = ref('')
+</script>
+
+<template>
+	<NirField
+		v-model="value"
+		number-label="Numéro de sécurité sociale"
+		key-label="Clé"
+		:hide-details="true"
+	/>
+</template>
