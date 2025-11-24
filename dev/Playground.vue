18f8522ea0ba26e7fb55c80906dcb2031b36a409
VachetVirginie <vachetvirginie@gmail.com>
2025-11-24 11:59:32 +0100

    remove playground

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,22 +1,2 @@
-<script lang="ts" setup>
-	import { ref } from 'vue'
-	import DatePicker from '@/components/DatePicker/CalendarMode/DatePicker.vue'
-
-	const dateSaisine = ref('2023-01-11')
-</script>
-
 <template>
-	<div style="padding: 20px; max-width: 400px;">
-        {{  dateSaisine }}
-		<DatePicker
-			v-model="dateSaisine"
-			placeholder="Sélectionner une période"
-        format="DD/MM/YYYY"
-        display-range
-        use-combined-mode
-		/>
-		<div style="margin-top: 20px; font-family: monospace;">
-			Valeur: {{ dateSaisine || 'Non définie' }}
-		</div>
-	</div>
 </template>
