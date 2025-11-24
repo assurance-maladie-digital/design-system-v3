8a4219a63d6094dfa354709e16420bb5aec16ebe
David FYON <davidfyon@gmail.com>
2025-11-24 10:39:19 +0100

    delet ePlayground

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,21 +1,2 @@
-<script lang="ts" setup>
-	import { ref } from 'vue'
-	import DatePicker from '@/components/DatePicker/CalendarMode/DatePicker.vue'
-
-	const dateSaisine = ref('2023-01-11')
-</script>
-
 <template>
-	<div style="padding: 20px; max-width: 400px;">
-        {{  dateSaisine }}
-		<DatePicker
-			v-model="dateSaisine"
-			label="Date de saisine"
-			use-combined-mode
-			date-format-return="YYYY-MM-DD"
-		/>
-		<div style="margin-top: 20px; font-family: monospace;">
-			Valeur: {{ dateSaisine || 'Non définie' }}
-		</div>
-	</div>
 </template>
