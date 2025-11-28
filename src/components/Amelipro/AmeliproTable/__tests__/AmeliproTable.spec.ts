a386327f8b7774b382aa11a77756df96d51e2c7c
Grégory Nicol <gregory.nicol@sogeti.com>
2025-11-28 11:12:12 +0100

    TU pour AmeliproTable (wip)

--- src/components/Amelipro/AmeliproTable/__tests__/AmeliproTable.spec.ts
+++ src/components/Amelipro/AmeliproTable/__tests__/AmeliproTable.spec.ts
@@ -96,12 +96,25 @@ const requiredPropValues = (): ComponentProps<typeof AmeliproTable> => ({
 const modifiedPropValues = (): ComponentProps<typeof AmeliproTable> => ({
 	counterLabel: 'Modified counter label',
 	dataList: [
-		{ email: 'modified.jean.bernard@gmail.com', firstname: 'Modified Jean', id: 10, name: 'Modified Bernard' },
-		{ email: 'modified.simon.pierre@gmail.com', firstname: 'Modified Simon', id: 11, name: 'Modified Pierre' },
+		{
+			email: 'modified.jean.bernard@gmail.com',
+			firstname: 'Modified Jean',
+			id: 10,
+			name: 'Modified Bernard',
+		},
+		{
+			email: 'modified.simon.pierre@gmail.com',
+			firstname: 'Modified Simon',
+			id: 11,
+			name: 'Modified Pierre',
+		},
 	],
 	headers: [
 		{
 			align: 'right',
+			headerClasses: 'custom-header-class',
+			cellsClasses: 'custom-cells-class',
+			descriptionId: 'modified-description-id',
 			maxWidth: '30%',
 			minWidth: '10%',
 			name: 'name',
@@ -169,7 +182,13 @@ testHelper
 	.setExpectedPropOptions(expectedPropOptions)
 	.setRequiredPropValues(requiredPropValues)
 	.setModifiedPropValues(modifiedPropValues)
-	.setMountOptions({ global: { stubs: { AmeliproSelect: AmeliproSelectMock } } })
+	.setMountOptions({
+		global: {
+			stubs: {
+				AmeliproSelect: AmeliproSelectMock,
+			},
+		},
+	})
 
 describe('AmeliproTable', () => {
 	describe('Snapshots', () => {
@@ -233,46 +252,222 @@ describe('AmeliproTable', () => {
 			})
 		})
 
-		it('prop title sets aria-label on table', async () => {
-			const table = wrapper.find(`#${testHelper.default('uniqueId')}-table`)
-			expect(table.attributes('aria-label')).toBe(testHelper.default('title'))
-			await wrapper.setProps({ title: testHelper.modified('title'), uniqueId: testHelper.modified('uniqueId') })
-			const tableMod = wrapper.find(`#${testHelper.modified('uniqueId')}-table`)
-			expect(tableMod.attributes('aria-label')).toBe(testHelper.modified('title'))
-		})
-	})
+		describe('Desktop wrapper', () => {
+			it('prop uniqueId sets attribute id', async () => {
+				expect(wrapper.find('.amelipro-table__wrapper--desktop').attributes('id')).toBe(`${testHelper.default('uniqueId')}-desktop`)
 
-	describe.skip('Setting props should update props or attributes of inner components', () => {
-		let wrapper: VueWrapper<InstanceType<typeof AmeliproTable>>
-
-		beforeEach(() => {
-			wrapper = shallowMount(AmeliproTable, { props: requiredPropValues() })
-		})
-
-		it('prop headers sets table header cells', async () => {
-			const headerCell = wrapper.find(`#${testHelper.default('uniqueId')}-table-header-cell-0`)
-			expect(headerCell.text()).toContain(testHelper.default('headers')[0].title)
-			await wrapper.setProps({ headers: testHelper.modified('headers'), uniqueId: testHelper.modified('uniqueId') })
-			const headerCellMod = wrapper.find(`#${testHelper.modified('uniqueId')}-table-header-cell-0`)
-			expect(headerCellMod.text()).toContain(testHelper.modified('headers')[0].title)
-		})
-
-		it('prop dataList sets table rows', async () => {
-			const row = wrapper.find(`#${testHelper.default('uniqueId')}-table-row-0`)
-			expect(row.exists()).toBe(true)
-			await wrapper.setProps({ dataList: testHelper.modified('dataList'), uniqueId: testHelper.modified('uniqueId') })
-			const rowMod = wrapper.find(`#${testHelper.modified('uniqueId')}-table-row-0`)
-			expect(rowMod.exists()).toBe(true)
-		})
-	})
-
-	describe.skip('Slots', () => {
-		it('displays slot content', () => {
-			const wrapper = shallowMount(AmeliproTable, {
-				props: requiredPropValues(),
-				slots: { default: '<div id="slot-content">Slot Content</div>' },
+				const { uniqueId } = modifiedPropValues()
+				await wrapper.setProps({ uniqueId })
+				expect(wrapper.find('.amelipro-table__wrapper--desktop').attributes('id')).toBe(`${testHelper.modified('uniqueId')}-desktop`)
 			})
-			expect(wrapper.find('#slot-content').text()).toBe('Slot Content')
+		})
+
+		describe('Desktop wrapper > div', () => {
+			it('prop tableMaxWidth & tableWidth set attribute style', async () => {
+				expect(wrapper.find('.amelipro-table__wrapper--desktop > div').attributes('style')).toBe(`max-width: ${testHelper.default('tableMaxWidth')}; width: ${testHelper.default('tableWidth')};`)
+
+				const { tableMaxWidth, tableWidth } = modifiedPropValues()
+				await wrapper.setProps({ tableMaxWidth, tableWidth })
+				expect(wrapper.find('.amelipro-table__wrapper--desktop > div').attributes('style')).toBe(`max-width: ${testHelper.modified('tableMaxWidth')}; width: ${testHelper.modified('tableWidth')};`)
+			})
+		})
+
+		describe('Desktop table', () => {
+			it('prop uniqueId sets attribute id', async () => {
+				expect(wrapper.find('.amelipro-table__table--desktop').attributes('id')).toBe(`${testHelper.default('uniqueId')}-table`)
+
+				const { uniqueId } = modifiedPropValues()
+				await wrapper.setProps({ uniqueId })
+				expect(wrapper.find('.amelipro-table__table--desktop').attributes('id')).toBe(`${testHelper.modified('uniqueId')}-table`)
+			})
+
+			it('prop title sets attribute aria-label', async () => {
+				expect(wrapper.find('.amelipro-table__table--desktop').attributes('aria-label')).toBe(testHelper.default('title'))
+
+				const { title } = modifiedPropValues()
+				await wrapper.setProps({ title })
+				expect(wrapper.find('.amelipro-table__table--desktop').attributes('aria-label')).toBe(testHelper.modified('title'))
+			})
+
+			it('prop verticalBorder sets attribute class', async () => {
+				expect(wrapper.find('.amelipro-table__table--desktop').classes('vertical-border')).toBe(testHelper.default('verticalBorder'))
+
+				const { verticalBorder } = modifiedPropValues()
+				await wrapper.setProps({ verticalBorder })
+				expect(wrapper.find('.amelipro-table__table--desktop').classes('vertical-border')).toBe(testHelper.modified('verticalBorder'))
+			})
+		})
+
+		describe('Desktop table header', () => {
+			it('prop uniqueId sets attribute id', async () => {
+				expect(wrapper.find('.amelipro-table__table--desktop thead tr').attributes('id')).toBe(`${testHelper.default('uniqueId')}-table-header`)
+
+				const { uniqueId } = modifiedPropValues()
+				await wrapper.setProps({ uniqueId })
+				expect(wrapper.find('.amelipro-table__table--desktop thead tr').attributes('id')).toBe(`${testHelper.modified('uniqueId')}-table-header`)
+			})
+		})
+
+		describe('Desktop table header cells', () => {
+			// Ici on a besoin d'avoir des lignes à afficher pour que les headers puissent être contrôlés
+			beforeEach(() => {
+				wrapper = shallowMount(AmeliproTable, {
+					props: {
+						...requiredPropValues(),
+						dataList: [
+							{
+								email: 'test@example.com', firstname: 'Test', id: 1, name: 'User',
+							},
+						],
+						headers: [{
+							align: 'right',
+							maxWidth: '50%',
+							minWidth: '20%',
+							name: 'test',
+							title: 'header test',
+							width: '50%',
+						}],
+					},
+				})
+			})
+
+			it('prop headers sets number of header cells', async () => {
+				expect(wrapper.findAll('.amelipro-table__table--desktop thead th').length).toBe(1)
+
+				const { headers } = modifiedPropValues()
+				await wrapper.setProps({ headers })
+				expect(wrapper.findAll('.amelipro-table__table--desktop thead th').length).toBe(testHelper.modified('headers').length)
+			})
+
+			it('prop uniqueId sets attribute id', async () => {
+				expect(wrapper.find('.amelipro-table__table--desktop thead th').attributes('id')).toBe(`${testHelper.default('uniqueId')}-table-header-cell-0`)
+
+				const { uniqueId } = modifiedPropValues()
+				await wrapper.setProps({ uniqueId })
+				expect(wrapper.find('.amelipro-table__table--desktop thead th').attributes('id')).toBe(`${testHelper.modified('uniqueId')}-table-header-cell-0`)
+			})
+
+			it('prop headers sets attribute class', async () => {
+				expect(wrapper.find('.amelipro-table__table--desktop thead th').classes('custom-header-class')).toBe(false)
+
+				const { headers } = modifiedPropValues()
+				await wrapper.setProps({ headers })
+				expect(wrapper.find('.amelipro-table__table--desktop thead th').classes('custom-header-class')).toBe(true)
+			})
+
+			it('prop headers sets attribute style', async () => {
+				expect(wrapper.find('.amelipro-table__table--desktop thead th').attributes('style')).toBe('min-width: 20%; width: 50%; max-width: 50%; text-align: right;')
+
+				const { headers } = modifiedPropValues()
+				await wrapper.setProps({ headers })
+				expect(wrapper.find('.amelipro-table__table--desktop thead th').attributes('style')).toBe('min-width: 10%; width: 30%; max-width: 30%; text-align: right;')
+			})
+
+			// th > p
+			it('prop headers sets attributes style & class and content', async () => {
+				expect(wrapper.find('.amelipro-table__table--desktop thead th p').attributes('aria-describedby')).toBeUndefined()
+				expect(wrapper.find('.amelipro-table__table--desktop thead th p').classes('d-flex')).toBe(false)
+				expect(wrapper.find('.amelipro-table__table--desktop thead th p').text()).toBe('header test')
+
+				const { headers } = modifiedPropValues()
+				await wrapper.setProps({ headers })
+				expect(wrapper.find('.amelipro-table__table--desktop thead th p').attributes('aria-describedby')).toBe('modified-description-id')
+				expect(wrapper.find('.amelipro-table__table--desktop thead th p').classes('d-flex')).toBe(true)
+				expect(wrapper.find('.amelipro-table__table--desktop thead th p').text()).toBe('Modified Nom')
+			})
+		})
+
+		describe('Desktop table body rows', () => {
+			beforeEach(() => {
+				wrapper = shallowMount(AmeliproTable, {
+					props: {
+						...requiredPropValues(),
+						dataList: [
+							{
+								email: 'test@example.com', firstname: 'Test', id: 1, name: 'User',
+							},
+						],
+						headers: [{
+							align: 'right',
+							maxWidth: '50%',
+							minWidth: '20%',
+							name: 'test',
+							title: 'header test',
+							width: '50%',
+						}],
+					},
+				})
+			})
+
+			it('prop dataList sets number of body rows', async () => {
+				expect(wrapper.findAll('.amelipro-table__table--desktop tbody tr').length).toBe(1)
+
+				const { dataList } = modifiedPropValues()
+				await wrapper.setProps({ dataList })
+				expect(wrapper.findAll('.amelipro-table__table--desktop tbody tr').length).toBe(testHelper.modified('dataList').length)
+			})
+
+			it('prop uniqueId sets attribute id', async () => {
+				expect(wrapper.find('.amelipro-table__table--desktop tbody tr').attributes('id')).toBe(`${testHelper.default('uniqueId')}-table-row-0`)
+
+				const { uniqueId } = modifiedPropValues()
+				await wrapper.setProps({ uniqueId })
+				expect(wrapper.find('.amelipro-table__table--desktop tbody tr').attributes('id')).toBe(`${testHelper.modified('uniqueId')}-table-row-0`)
+			})
+		})
+
+		describe('Desktop table body cells', () => {
+			beforeEach(() => {
+				wrapper = shallowMount(AmeliproTable, {
+					props: {
+						...requiredPropValues(),
+						dataList: [
+							{
+								email: 'test@example.com', firstname: 'Test', id: 1, name: 'User',
+							},
+						],
+						headers: [{
+							align: 'right',
+							maxWidth: '50%',
+							minWidth: '20%',
+							name: 'test',
+							title: 'header test',
+							width: '50%',
+						}],
+					},
+				})
+			})
+
+			it('prop headers sets number of body cells', async () => {
+				expect(wrapper.findAll('.amelipro-table__table--desktop tbody td').length).toBe(1)
+
+				const { headers } = modifiedPropValues()
+				await wrapper.setProps({ headers })
+				expect(wrapper.findAll('.amelipro-table__table--desktop tbody td').length).toBe(testHelper.modified('headers').length)
+			})
+
+			it('prop uniqueId sets attribute id', async () => {
+				expect(wrapper.find('.amelipro-table__table--desktop tbody td').attributes('id')).toBe(`${testHelper.default('uniqueId')}-table-row-0-cell-0`)
+
+				const { uniqueId } = modifiedPropValues()
+				await wrapper.setProps({ uniqueId })
+				expect(wrapper.find('.amelipro-table__table--desktop tbody td').attributes('id')).toBe(`${testHelper.modified('uniqueId')}-table-row-0-cell-0`)
+			})
+
+			it('prop dataList sets attribute class', async () => {
+				expect(wrapper.find('.amelipro-table__table--desktop tbody td').classes()).toEqual([])
+
+				// Il faut aussi modifier headers pour que cell.value soit correctement mise à jour
+				const { dataList, headers } = modifiedPropValues()
+				await wrapper.setProps({ dataList, headers })
+				expect(wrapper.find('.amelipro-table__table--desktop tbody td').classes()).toEqual(['custom-cells-class'])
+			})
 		})
 	})
+
+	// TODO:
+	// describe.skip('Setting props should update props or attributes of inner components', () => {	})
+
+	// TODO:
+	// describe.skip('Slots', () => {	})
 })
