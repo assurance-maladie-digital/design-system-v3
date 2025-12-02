ba84167cf140eadfacabb1e964cb7873d565a889
Sebastian <sebastian.czech@assurance-maladie.fr>
2025-12-02 11:40:55 +0100

    Update component to displaying pdf preview on firefox

--- src/components/Amelipro/AmeliproFilePreview/AmeliproFilePreview.stories.ts
+++ src/components/Amelipro/AmeliproFilePreview/AmeliproFilePreview.stories.ts
@@ -8,10 +8,10 @@ const meta = {
 		fileName: { description: 'Nom du fichier PDF' },
 		fileSrc: { description: 'Url du fichier PDF' },
 		foldable: { description: 'Transforme la carte en carte dépliante' },
-		iframeTitle: { description: 'Titre de l’iframe contenant l’aperçu du fichier' },
+		pdfDisplayTitle: { description: 'Titre pour l’élément <object> affichant l’aperçu du fichier PDF' },
 		isOpen: { description: 'Statut par défaut de la carte dépliante' },
 		linkTitle: { description: 'Titre du lien de téléchargement du fichier, il doit reprendre l’intitulé du bouton et informer si possible sur le format du fichier à télécharger ainsi que sur sa taille (exemple : Télécharger le fichier au format PDF - 3Mo)' },
-		previewHeight: { description: 'Hauteur de l’iframe' },
+		previewHeight: { description: `Hauteur de l'aperçu PDF` },
 		uniqueId: { description: 'Identifiant unique' },
 	},
 	component: AmeliproFilePreview,
@@ -28,7 +28,7 @@ export const Default: Story = {
 		fileName: 'Nom du fichier',
 		fileSrc: '/amelipro/pdf/charte-pour-pdf.pdf',
 		foldable: false,
-		iframeTitle: 'Aperçu du fichier PDF',
+		pdfDisplayTitle: 'Aperçu du fichier PDF',
 		isOpen: false,
 		linkTitle: 'Télécharger le fichier au Format PDF',
 		previewHeight: 550,
@@ -45,7 +45,7 @@ export const Default: Story = {
 		file-name="Nom du fichier"
 		file-src="/amelipro/pdf/charte-pour-pdf.pdf"
 		:foldable="false"
-		iframe-title="Aperçu du fichier PDF"
+		pdfDisplayTitle="Aperçu du fichier PDF"
 		:is-open="false"
 		link-title="Télécharger le fichier au Format PDF"
 		:preview-height="550"
