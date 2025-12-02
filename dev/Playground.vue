7ba5f0aa3451b85cb4d832b87a539bb5fac8944f
Verushka Rossi <verushkarossi@gmail.com>
2025-12-02 15:13:05 +0100

    modif en cours

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,2 +1,26 @@
+<script lang="ts" setup>
+	import ThemeSwitcher from './ThemeSwitcher.vue'
+</script>
+
 <template>
+	<div>
+		<ThemeSwitcher class="theme-switcher-position" />
+
+		<VSheet
+			color="primary"
+			class="pa-4"
+		>
+			start developing here
+		</VSheet>
+	</div>
 </template>
+
+<style scoped>
+/* Position the theme switcher as needed */
+.theme-switcher-position {
+  position: fixed;
+  bottom: 16px;
+  right: 16px;
+  z-index: 100;
+}
+</style>
---
+++ src/components/Customs/SyRadioGroup/types.d.ts
@@ -0,0 +1,6 @@
+export interface SyRadioGroupItem {
+	disabled?: boolean
+	isChecked: boolean
+	label: string
+	value: string
+}
