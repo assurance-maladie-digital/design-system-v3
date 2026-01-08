4c46022ead24a0392ab504f365aa34fb559e8bcb
Verushka Rossi <verushkarossi@gmail.com>
2026-01-08 12:12:01 +0100

    correction background custom theme

--- src/components/FooterBar/FooterBar.vue
+++ src/components/FooterBar/FooterBar.vue
@@ -252,11 +252,14 @@ a {
 	border: 0;
 }
 
+.vd-footer-bar.v-theme--dark {
+	--v-theme-background-surface: 47, 56, 77; // #2F384D
+}
+
 // Fix footer bar height in SK
 .v-footer {
 	flex-grow: 0 !important;
 	justify-content: center;
-	background-color: rgb(var(--v-theme-background-surface));
 
 	.container {
 		width: 100%;
