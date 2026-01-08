4bc45a0fd88aaa67e4ef0cb1bed933d8a1f233b2
David FYON <davidfyon@gmail.com>
2026-01-08 11:14:44 +0100

    playground

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,17 +1,2 @@
-	<script lang="ts" setup >
-    import SyTextField from '@/components/Customs/SyTextField/SyTextField.vue'
-	import { ref } from 'vue'
-
-	const inputValue = ref<null>(null)
-</script>
-
 <template>
-	<SyTextField
-		v-model="inputValue"
-		label="Playground TextField"
-		placeholder="Type something..."
-		:counter="20"
-		:display-persistent-counter="true"
-	/>
-	<p>Input Value: {{ inputValue }}</p>
 </template>
