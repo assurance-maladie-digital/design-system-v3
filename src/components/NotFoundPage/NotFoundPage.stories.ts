048ddd745bd3b6f9fcdf218c1b24e1047fcde420
David FYON <davidfyon@gmail.com>
2026-01-12 11:21:03 +0100

    add custom illustation stories

--- src/components/NotFoundPage/NotFoundPage.stories.ts
+++ src/components/NotFoundPage/NotFoundPage.stories.ts
@@ -105,3 +105,59 @@ export const WithErrorCode: Story = {
 		() => ({ template: '<div style="padding: 20px; background: rgb(231, 236, 245)"><story /></div>' }),
 	],
 }
+
+export const CustomIllustration: Story = {
+	args: {
+		btnText: 'Retour à l’accueil',
+		btnHref: '/',
+	},
+	parameters: {
+		query: {
+			support_id: '',
+		},
+		sourceCode: [
+			{
+				name: 'Template',
+				code: `
+				<template>
+					<div style="padding: 20px; background: rgb(231, 236, 245)">
+						<NotFoundPage btn-text="Retour à l’accueil" btn-href="/">
+							<template #illustration>
+								<div style="width: 260px; height: 200px; display: grid; place-items: center; border-radius: 12px; background: white; border: 2px dashed #0D419A;">
+									<span style="font-weight: 700; color: #0D419A;">Illustration</span>
+								</div>
+							</template>
+						</NotFoundPage>
+					</div>
+				</template>
+				`,
+			},
+			{
+				name: 'Script',
+				code: `
+				<script setup lang="ts">
+					import { NotFoundPage } from '@cnamts/synapse'
+				</script>
+				`,
+			},
+		],
+	},
+	decorators: [
+		() => ({ template: '<div style="padding: 20px; background: rgb(231, 236, 245)"><story /></div>' }),
+	],
+	render: args => ({
+		components: { NotFoundPage },
+		setup() {
+			return { args }
+		},
+		template: `
+			<NotFoundPage v-bind="args">
+				<template #illustration>
+					<div style="width: 260px; height: 200px; display: grid; place-items: center; border-radius: 12px; background: white; border: 2px dashed #0D419A;">
+						<span style="font-weight: 700; color: #0D419A;">Illustration</span>
+					</div>
+				</template>
+			</NotFoundPage>
+		`,
+	}),
+}
