7c2dc62cc46c2bee97f01ac62d9d7a59873140c7
adrien <adrien.charrier@assurance-maladie.fr>
2026-01-14 11:56:36 +0100

    refactor: remove About and Home components, update router to use Playground as main component

--- dev/router.ts
+++ dev/router.ts
@@ -6,12 +6,7 @@ export const router = createRouter({
 		{
 			name: 'home',
 			path: '/',
-			component: () => import('./Home.vue'),
-		},
-		{
-			name: 'about',
-			path: '/about',
-			component: () => import('./About.vue'),
+			component: () => import('./Playground.vue'),
 		},
 	],
 })
