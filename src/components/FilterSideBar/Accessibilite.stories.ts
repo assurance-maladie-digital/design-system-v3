0e6be85734946c535a22e45b773b7e9b4b7c92e4
Adrien <65124433+4dr1en@users.noreply.github.com>
2026-01-15 09:51:17 +0100

    feat: enhance accessibility and usability of FilterSideBar component (#1704)

--- src/components/FilterSideBar/Accessibilite.stories.ts
+++ src/components/FilterSideBar/Accessibilite.stories.ts
@@ -209,6 +209,10 @@ export const Legende: StoryObj = {
 				  </div>
 				</div>
 			  </div>
+			  <div class="mt-4">
+				  <p>Rapport d’audit manuel : <a href="/audits/FilterSideBar.xlsx" style="color:#0C41BD;">Voir le rapport</a></p>
+				  <p style="color: grey; font-size: 14px">Correctifs associés (<a href="https://github.com/assurance-maladie-digital/design-system-v3/issues/913" target="_blank" style="color:#0C41BD;">issue #913</a>)</p>
+			  </div>
             `,
 		}
 	},
--- src/components/FilterSideBar/FilterSideBar.vue
+++ src/components/FilterSideBar/FilterSideBar.vue
@@ -1,17 +1,21 @@
 <script lang="ts" setup>
-	import { computed, ref, toRef, watch } from 'vue'
-	import { locales as defaultLocales } from './locales'
-	import { mdiFilterVariant } from '@mdi/js'
-	import ChipList from '../ChipList/ChipList.vue'
 	import type { FilterProp } from '@/composables/useFilterable/useFilterable'
 	import useFilterable from '@/composables/useFilterable/useFilterable'
+	import { mdiFilterVariant } from '@mdi/js'
+	import { computed, onMounted, ref, toRef, watch } from 'vue'
 	import { type VBtn, type VNavigationDrawer } from 'vuetify/components'
+	import ChipList from '../ChipList/ChipList.vue'
+	import SyIcon from '../Customs/SyIcon/SyIcon.vue'
+	import { locales as defaultLocales } from './locales'
+	import vLockFocus from '@/directives/lockFocus'
 
 	const props = withDefaults(defineProps<{
 		modelValue?: FilterProp
+		modale?: boolean
 		locales?: typeof defaultLocales
 	}>(), {
 		modelValue: () => [],
+		modale: false,
 		locales: () => defaultLocales,
 	})
 
@@ -58,13 +62,20 @@
 			drawerBtnRef.value!.$el.focus()
 		}
 	})
+
+	onMounted(() => {
+		const filterIconSVG = drawerRef!.value!.$el.nextElementSibling.querySelectorAll('svg[role="img"]')
+		filterIconSVG.forEach((svg) => {
+			svg.removeAttribute('role')
+		})
+	})
 </script>
 
 <template>
 	<div class="sy-filters-side-bar">
 		<VBtn
 			ref="drawerBtnRef"
-			class="px-4 py-3"
+			class="sy-filters-side-bar__open-btn px-4 py-3"
 			:class="{ 'v-btn--active': drawer }"
 			color="primary"
 			variant="text"
@@ -82,18 +93,19 @@
 
 			{{ locales.filterBtnLabel }}
 
-			<VIcon
+			<SyIcon
 				class="ml-2"
+				:icon="mdiFilterVariant"
 				size="small"
-			>
-				{{ mdiFilterVariant }}
-			</VIcon>
+				decorative
+			/>
 		</VBtn>
 		<Teleport
 			to="body"
 		>
 			<VNavigationDrawer
 				ref="drawerRef"
+				tag="div"
 				:model-value="drawer"
 				sticky
 				temporary
@@ -105,83 +117,106 @@
 				disable-resize-watcher
 				tabindex="0"
 				class="sy-filters-side-bar__content elevation-6 bg-white"
+				role="dialog"
+				:aria-hidden="drawer ? undefined : 'true'"
+				:aria-modal="props.modale"
+				:aria-label="locales.modaleLabel"
+				@keydown.escape.prevent="drawer = false"
 			>
-				<VExpansionPanels variant="accordion">
-					<VExpansionPanel
-						v-for="filter in filters"
-						:key="filter.name"
-						:class="`vd-filter-${filter.name}`"
-						tile
-						elevation="0"
+				<form
+					v-lock-focus="$props.modale"
+					@submit.prevent="applyFilters"
+				>
+					<VExpansionPanels
+						variant="accordion"
+						tag="ul"
 					>
-						<VExpansionPanelTitle class="text-subtitle-2">
-							{{ filter.title }}
-
-							<span
-								v-if="getFilterCount(filter)"
-								class="ml-1"
+						<VExpansionPanel
+							v-for="filter in filters"
+							:key="filter.name"
+							:class="`vd-filter-${filter.name}`"
+							tile
+							elevation="0"
+							tag="li"
+						>
+							<VExpansionPanelTitle
+								class="text-subtitle-2"
 							>
-								({{ getFilterCount(filter) }})
-							</span>
-						</VExpansionPanelTitle>
+								{{ filter.title }}
 
-						<VExpansionPanelText>
-							<ChipList
-								:items="getChips(filter)"
-								:overflow-limit="filter.chipOverflowLimit"
-								class="mb-5"
-								:vuetify-options="{ chip: { color: 'secondary' }, icon: { color: 'white'} }"
-								@remove="removeChip(filter, $event)"
-								@reset="resetFilter(filter)"
-							/>
+								<span
+									v-if="getFilterCount(filter)"
+									class="ml-1"
+								>
+									({{ getFilterCount(filter) }})
+								</span>
+							</VExpansionPanelTitle>
 
-							<slot
-								:name="`${formatFilterName(filter.name)}`"
-								:props="{
-									modelValue: filter.value as any,
-									'onUpdate:modelValue': (value: unknown) =>
-										(filter.value = value),
-								}"
-							/>
-						</VExpansionPanelText>
-					</VExpansionPanel>
-				</VExpansionPanels>
+							<VExpansionPanelText>
+								<ChipList
+									:aria-label="locales.badgeListLabel(filter.name)"
+									:items="getChips(filter)"
+									:overflow-limit="filter.chipOverflowLimit"
+									class="mb-5"
+									:vuetify-options="{ chip: { color: 'secondary' }, icon: { color: 'white'} }"
+									@remove="removeChip(filter, $event)"
+									@reset="resetFilter(filter)"
+								/>
 
-				<VSpacer />
+								<slot
+									:name="`${formatFilterName(filter.name)}`"
+									:props="{
+										modelValue: filter.value as any,
+										'onUpdate:modelValue': (value: unknown) =>{
+											(filter.value = value)},
+									}"
+								/>
+							</VExpansionPanelText>
+						</VExpansionPanel>
+					</VExpansionPanels>
 
-				<div class="px-4 pb-4 pt-10">
-					<VBtn
-						color="primary"
-						block
-						size="large"
-						variant="outlined"
-						class="mb-4"
-						@click="drawer = false"
-					>
-						{{ locales.close }}
-					</VBtn>
+					<VSpacer />
 
-					<VBtn
-						color="primary"
-						block
-						size="large"
-						variant="outlined"
-						class="mb-4"
-						@click.stop="resetAllFilters"
-					>
-						{{ locales.reset }}
-					</VBtn>
+					<div class="px-4 pb-4 pt-10">
+						<VBtn
+							color="primary"
+							block
+							size="large"
+							variant="outlined"
+							class="sy-filters-side-bar__close-btn mb-4"
+							:aria-label="locales.closeAriaLabel"
+							@click="drawer = false"
+						>
+							{{ locales.close }}
+						</VBtn>
 
-					<VBtn
-						color="primary"
-						block
-						size="large"
-						variant="elevated"
-						@click.stop="applyFilters"
-					>
-						{{ locales.apply }}
-					</VBtn>
-				</div>
+						<VBtn
+							color="primary"
+							block
+							size="large"
+							variant="outlined"
+							class="sy-filters-side-bar__reset-btn mb-4"
+							type="reset"
+							:aria-label="locales.resetAriaLabel"
+							@click.stop="resetAllFilters"
+						>
+							{{ locales.reset }}
+						</VBtn>
+
+						<VBtn
+							color="primary"
+							block
+							size="large"
+							class="sy-filters-side-bar__apply-btn"
+							variant="elevated"
+							type="submit"
+							:aria-label="locales.applyAriaLabel"
+							@click.stop="applyFilters"
+						>
+							{{ locales.apply }}
+						</VBtn>
+					</div>
+				</form>
 			</VNavigationDrawer>
 		</Teleport>
 	</div>
@@ -209,4 +244,32 @@
 	}
 }
 
+.sy-filters-side-bar__close-btn,
+.sy-filters-side-bar__reset-btn,
+.sy-filters-side-bar__apply-btn,
+.sy-filters-side-bar__open-btn {
+	&:focus-visible {
+		outline: 2px solid rgb(var(--v-theme-primary));
+		outline-offset: 2px;
+
+		:deep(.v-btn__overlay) {
+			display: none;
+		}
+
+		&::after {
+			display: none;
+		}
+	}
+}
+
+.v-expansion-panel-title {
+	&:focus-visible {
+		outline: 2px solid rgb(var(--v-theme-primary));
+		outline-offset: -2px;
+
+		:deep(.v-expansion-panel-title__overlay) {
+			display: none;
+		}
+	}
+}
 </style>
