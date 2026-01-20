3da6ab4bdf70b549f0d74152bdd94a0ed34cb3f8
adrien <adrien.charrier@assurance-maladie.fr>
2026-01-20 16:08:44 +0100

    restor temp folder

---
+++ src/temp/customizableOptions.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+	import useCustomizableOptions, { type CustomizableOptions } from '@/composables/useCustomizableOptions'
+	import defaultOptions from './options.json'
+
+	const props = defineProps<CustomizableOptions & {
+		title: string
+	}>()
+
+	const options = useCustomizableOptions(defaultOptions, props)
+</script>
+
+<template>
+	<div>
+		<VBtn v-bind="options.btn">
+			{{ title }}
+		</VBtn>
+	</div>
+</template>
