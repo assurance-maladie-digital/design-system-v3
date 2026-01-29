66c2734534dd0f6977b0ca6b616af5f6c8e68e49
David FYON <davidfyon@gmail.com>
2026-01-29 11:51:14 +0100

    fix focus ring

--- src/components/Customs/Selects/SySelect/SySelect.vue
+++ src/components/Customs/Selects/SySelect/SySelect.vue
@@ -1150,6 +1150,12 @@
 	:deep(.v-input__append > .v-icon__svg) {
 		fill: rgb(0 0 0 / 70%);
 	}
+  :deep(.v-input__prepend .v-icon:focus-visible),
+  :deep(.v-input__append .v-icon:focus-visible) {
+    outline: 2px solid tokens.$primary-base;
+    outline-offset: 2px;
+    opacity: 1;
+  }
 }
 
 .v-field {
