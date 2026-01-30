c3265eb88d01ee106e4db7f1a7d28cd960e48bb4
David FYON <davidfyon@gmail.com>
2026-01-30 12:33:02 +0100

    SySelect: add slots/tooltips (#1767)
    * add slots + tests + stories
    
    * fox onMounted
    
    * fix dropdown display on multiple
    
    * fix icon colors
    
    * fix lint
    
    * add comment
    
    * fix nav
    
    * fix
    
    * fix focus ring
    
    * fix focus ring
    
    * fix roles
    
    * fix
    
    * redo svg fix
    
    * lint
    
    * fix
    
    * fix img errors
    
    * lint
    
    * fix tanaguru
    
    * fix tanaguru
    
    * undo  directive change
    
    * fix keydown
    
    * add IconSlot
    
    * fix types

--- src/components/Customs/Selects/SySelect/types.d.ts
+++ src/components/Customs/Selects/SySelect/types.d.ts
@@ -1,6 +1,3 @@
-export type VariantType = 'text' | 'flat' | 'elevated' | 'tonal' | 'outlined' | 'plain'
-export type DensityType = 'comfortable' | 'compact'
-export type locationType = 'start' | 'end' | 'top' | 'bottom' | 'start center' | 'end center' | 'top center' | 'bottom center'
 export type IconType = 'info' | 'success' | 'warning' | 'error' | 'close' | 'calendar' | undefined
 export type VariantStyle = 'outlined' | 'plain' | 'underlined' | 'filled' | 'solo' | 'solo-inverted' | 'solo-filled'
 export type ColorType = 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'error'
