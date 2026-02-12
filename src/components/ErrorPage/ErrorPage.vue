0f084cf5c8bb34b3c88c918e788303c50d3bd0cf
Valentino Suma <valentino.suma@octo.com>
2026-02-12 15:35:17 +0100

    modify errorPage to be customed

--- src/components/ErrorPage/ErrorPage.vue
+++ src/components/ErrorPage/ErrorPage.vue
@@ -3,10 +3,22 @@
 	import StatusPage from '../StatusPage/StatusPage.vue'
 	import { useTheme } from 'vuetify'
 	import { computed } from 'vue'
+	import type { RouteRecordRaw } from 'vue-router'
+
+	const props = defineProps<{
+		pageTitle?: string
+		message?: string
+		code?: string
+		codeErrorText?: string
+		btnText?: string
+		btnHref?: string
+		btnLink?: RouteRecordRaw | string
+		hideBtn?: boolean
+	}>()
 
 	const vuetifyTheme = useTheme()
 
-	const currentLocales = computed(() => {
+	const themeLocales = computed(() => {
 		if (vuetifyTheme.name.value === 'cnam') {
 			return locales.cnam
 		}
@@ -17,22 +29,43 @@
 			return locales.default
 		}
 	})
+
+	// Utiliser les props de l'utilisateur en priorité, sinon les locales
+	const pageTitle = computed(() => props.pageTitle ?? themeLocales.value.pageTitle)
+	const message = computed(() => props.message ?? themeLocales.value.message)
+	const code = computed(() => props.code ?? themeLocales.value.code)
+	const src = computed(() => themeLocales.value.src)
+
+	// Props qui peuvent être customisées
+	const codeErrorText = computed(() => props.codeErrorText)
+	const btnText = computed(() => props.btnText)
+	const btnHref = computed(() => props.btnHref)
+	const btnLink = computed(() => props.btnLink)
+	const hideBtn = computed(() => props.hideBtn)
+
+	// Vérifier s'il y a une image à afficher
+	const hasImage = computed(() => src.value !== '')
 </script>
 
 <template>
 	<StatusPage
-		:page-title="currentLocales.pageTitle"
-		:message="currentLocales.message"
-		code="500"
-		:hide-btn="true"
+		:page-title="pageTitle"
+		:message="message"
+		:code="code"
+		:code-error-text="codeErrorText"
+		:btn-text="btnText"
+		:btn-href="btnHref"
+		:btn-link="btnLink"
+		:hide-btn="hideBtn"
 	>
 		<template
-			v-if="currentLocales.src"
+			v-if="hasImage || $slots.illustration"
 			#illustration
 		>
 			<slot name="illustration">
 				<img
-					:src="currentLocales.src"
+					v-if="hasImage"
+					:src="src"
 					alt=""
 					aria-hidden="true"
 				>
@@ -43,6 +76,6 @@
 
 <style lang="scss" scoped>
 img {
-	max-height: 290px;
+  max-height: 290px;
 }
 </style>
--- src/components/ErrorPage/locales.ts
+++ src/components/ErrorPage/locales.ts
@@ -5,25 +5,19 @@ export const locales = {
 		pageTitle: 'Une erreur est survenue',
 		message: 'Une erreur est survenue de notre côté, veuillez réessayer plus tard.',
 		src: '',
-		supportIdMessage: 'Votre identifiant de support\xa0:',
-		btnText: 'Retour à l’accueil',
-		errorCodeText: 'Code d\'erreur\xa0: ',
+		code: '500',
 	},
 	cnam: {
 		pageTitle: 'Une erreur est survenue',
 		message: 'Une erreur est survenue de notre côté, veuillez réessayer plus tard. Si le problème persiste veuillez nous contacter par téléphone au 3646',
 		src: '',
-		supportIdMessage: 'Votre identifiant de support\xa0:',
-		btnText: 'Retour à l’accueil',
-		errorCodeText: 'Code d\'erreur\xa0: ',
+		code: '500',
 	},
 	ap: {
 		pageTitle: 'Les services amelipro sont indisponibles !',
 		message:
       'Nous mettons en œuvre les mesures nécessaires pour rétablir rapidement votre accès. Veuillez nous excuser pour la gêne occasionnée.',
 		src: imgUrlAp,
-		supportIdMessage: 'Votre identifiant de support\xa0:',
-		btnText: 'Retour à l’accueil',
-		errorCodeText: 'Code d\'erreur\xa0: ',
+		code: '500',
 	},
 }
