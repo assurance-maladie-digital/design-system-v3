bea156c2d49476829dfd1d6fdc541e3eeedf525e
Valentino Suma <valentino.suma@octo.com>
2026-02-23 10:42:40 +0100

    fix locales

--- src/components/NotFoundPage/locales.ts
+++ src/components/NotFoundPage/locales.ts
@@ -6,7 +6,7 @@ export const locales = {
 		code: '404',
 		pageTitle: 'Page non trouvée',
 		message: 'Cette page n’existe pas ou a été déplacée.',
-		src: '',
+		src: imgUrlCnam,
 	},
 	cnam: {
 		code: '404',
