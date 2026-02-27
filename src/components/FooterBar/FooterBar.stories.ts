654fa4378eaee30160ca826488e43f056d72011f
Sebastian <sebastian.czech@assurance-maladie.fr>
2026-02-27 09:13:19 +0100

    change name

--- src/components/FooterBar/FooterBar.stories.ts
+++ src/components/FooterBar/FooterBar.stories.ts
@@ -995,7 +995,7 @@ export const customSocialMediaLinks: Story = {
 				name: 'Script',
 				code: `<script setup lang="ts">
 	import { FooterBar } from '@cnamts/synapse'
-	import { mdiFacebook, mdiTwitter, mdiLinkedin, mdiYoutube } from '@mdi/js'
+	import { mdiFacebook, mdiLinkedin, mdiYoutube } from '@mdi/js'
 	
 	const xIcon = 'M14.234 10.162 22.977 0h-2.072l-7.591 8.824L7.251 0H.258l9.168 13.343L.258 24H2.33l8.016-9.318L16.749 24h6.993zm-2.837 3.299-.929-1.329L3.076 1.56h3.182l5.965 8.532.929 1.329 7.754 11.09h-3.182z'
 
@@ -1276,9 +1276,9 @@ export const BackOffice: Story = {
 				href: 'https://www.facebook.com',
 			},
 			{
-				icon: mdiTwitter,
-				name: 'Twitter',
-				href: 'https://www.twitter.com',
+                icon: xIcon,
+                name: 'X',
+                href: 'https://x.com',
 			},
 			{
 				icon: mdiLinkedin,
