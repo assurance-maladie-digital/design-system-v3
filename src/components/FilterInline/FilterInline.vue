bed73d1b8d787fad993616b53cb168205249d340
VachetVirginie <vachetvirginie@gmail.com>
2026-03-04 15:34:11 +0100

    fix: remove aria-controls from FilterInline button when menu is closed

--- src/components/FilterInline/FilterInline.vue
+++ src/components/FilterInline/FilterInline.vue
@@ -45,6 +45,7 @@
 			<template #activator="{ props: menuProps, isActive }">
 				<VBtn
 					v-bind="menuProps"
+					:aria-controls="isActive ? menuProps['aria-controls'] : undefined"
 					:class="`sy-filter-${filter.name}`"
 					:variant="getFilterCount(filter) ? undefined : 'outlined'"
 					color="secondary"
