5aaf6ff110a269fdfe538ffd0ceab7b95efb02ce
David FYON <davidfyon@gmail.com>
2026-03-04 09:17:28 +0100

    playground

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,20 +1,2 @@
-<script setup lang="ts">
-  import {onMounted, ref} from 'vue'
-  import DatePicker from '@/components/DatePicker/CalendarMode/DatePicker.vue'
-  const date = ref('2022-10-18')
-
-  onMounted(() => {
-    console.log(date.value)
-  })
-</script>
-
 <template>
-  <DatePicker
-      v-model="date"
-      placeholder="Sélectionner une date"
-      format="DD/MM/YYYY"
-      date-format-return="YYYY-MM-DD"
-      label="Sélectionner une date"
-  />
-  {{ date }}
 </template>
