ffc1f0158e58720088ecbb0f5d8a1acea800440d
Valentino Suma <valentino.suma@octo.com>
2026-03-09 16:03:14 +0100

    add component syIconButton

---
+++ src/components/Customs/SyIconButton/SyIconButton.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+	import type { IconValue } from 'vuetify/lib/composables/icons.mjs'
+	import SyIcon from '../SyIcon/SyIcon.vue'
+
+	const props = defineProps<{
+		icon: IconValue
+		label: string
+		color?: string
+		size?: string
+		disabled?: boolean
+	}>()
+
+	defineEmits<{
+		'click-icon-button'
+	}>()
+</script>
+
+<template>
+	<v-btn
+		:disabled="props.disabled"
+		:label="props.label"
+		class="rounded-xl"
+		:size="props.size"
+		variant="text"
+		icon
+		@click="$emit('click-icon-button')"
+	>
+		<SyIcon
+			:label="props.label"
+			:icon="props.icon"
+			:size="props.size"
+		/>
+	</v-btn>
+</template>
