5a5b5f5133c247de33640d5558ab0bcb6acebadd
David FYON <davidfyon@gmail.com>
2026-03-10 11:32:10 +0100

    PhoneField: fix errors display (#1928)
    * fix errors display
    
    * fix success message display + add test
    
    * fix counter + maxlength
    
    * lint
    
    * fix indicatif ZA
    
    * add accessibility
    
    * fix a11y test
    
    * fix link
    
    * set default indicatif
    
    * fix events on blur
    
    * fix stories
    
    * fix doc
    
    * fix EG mask
    
    * fix indicatif change error display
    
    * fix comments
    
    * refacto
    
    * fix story
    
    * improve doc

--- src/components/Customs/SyTextField/SyTextField.vue
+++ src/components/Customs/SyTextField/SyTextField.vue
@@ -90,6 +90,7 @@
 			disableClickButton?: boolean
 			autocomplete?: string
 			helpText?: string
+			maxlength?: string | number
 		}>(),
 		{
 			modelValue: undefined,
@@ -159,6 +160,7 @@
 			disableClickButton: true,
 			autocomplete: 'off',
 			helpText: '',
+			maxlength: undefined,
 		},
 	)
 
@@ -173,6 +175,8 @@
 
 	const emit = defineEmits([
 		'update:modelValue',
+		'input',
+		'keydown',
 		'clear',
 		'prepend-icon-click',
 		'append-icon-click',
@@ -305,7 +309,11 @@
 		}
 	})
 
-	const hasError = computed(() => validation.hasError.value || props.hasError)
+	const hasError = computed(() =>
+		validation.hasError.value
+		|| (props.errorMessages?.length ?? 0) > 0
+		|| props.hasError,
+	)
 	const hasWarning = computed(() => validation.hasWarning.value || props.hasWarning)
 	const hasSuccess = computed(() => ((validation.hasSuccess.value && !hasError.value && !hasWarning.value) || props.hasSuccess) && props.showSuccessMessages)
 
@@ -615,6 +623,7 @@
 			:hint="showHelpTextAsMessage ? props.helpText : props.hint"
 			:label="labelWithAsterisk"
 			:loading="props.loading"
+			:maxlength="props.maxlength"
 			:max-errors="props.maxErrors"
 			:max-width="props.maxWidth"
 			:messages="hasError ? errors : (hasWarning ? warnings : (hasSuccess && props.showSuccessMessages ? successes : []))"
@@ -645,6 +654,8 @@
 				'basic-field': !hasError && !hasWarning && !hasSuccess
 			}"
 			@blur="checkErrorOnBlur"
+			@input="(e: Event) => emit('input', e)"
+			@keydown="(e: KeyboardEvent) => emit('keydown', e)"
 		>
 			<!-- Prepend -->
 			<template
