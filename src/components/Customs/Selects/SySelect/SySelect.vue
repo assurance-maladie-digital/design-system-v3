11364606242de0fc2d9f38ba92a50eb5003e9255
David FYON <davidfyon@gmail.com>
2026-03-18 17:05:49 +0100

    fix chips and multiple display

--- src/components/Customs/Selects/SySelect/SySelect.vue
+++ src/components/Customs/Selects/SySelect/SySelect.vue
@@ -414,6 +414,11 @@
 			return ''
 		}
 
+		// If inline labels are shown, return empty string to hide input text
+		if (hasMultipleSelections.value) {
+			return ''
+		}
+
 		// For multiple mode, show default option text when nothing is selected
 		if (props.multiple) {
 			if (!selectedItem.value || (Array.isArray(selectedItem.value) && selectedItem.value.length === 0)) {
@@ -455,6 +460,10 @@
 
 	const hasChips = computed(() => {
 		return props.chips && props.multiple && Array.isArray(selectedItem.value) && selectedItem.value.length > 0
+	})
+
+	const hasMultipleSelections = computed(() => {
+		return !props.chips && props.multiple && Array.isArray(selectedItem.value) && (selectedItem.value as unknown[]).length > 0
 	})
 
 	const hasSelectionToClear = computed(() => {
@@ -920,7 +929,7 @@
 						:rules="isRequired && !props.disableErrorHandling ? ['Le champ est requis.'] : []"
 						:bg-color="props.bgColor"
 						:density="props.density"
-						:active="hasChips || isOpen"
+						:active="hasChips || hasMultipleSelections || isOpen"
 						readonly
 						:hide-details="props.hideMessages && !showHelpTextAsMessage"
 						:hint="showHelpTextAsMessage ? props.helpText : ''"
@@ -961,6 +970,15 @@
 								{{ getChipText(item) }}
 							</VChip>
 						</div>
+					<template v-else-if="hasMultipleSelections">
+						<span
+							v-for="item in (selectedItem as unknown[])"
+							:key="props.returnObject && item ? (item as ItemType)[props.valueKey] : (item as string | number)"
+							class="sy-select__label"
+						>
+							{{ getChipText(item) }}
+						</span>
+					</template>
 						<!-- Prepend -->
 						<template
 							v-if="$slots.prepend || props.prependIcon || props.prependTooltip"
@@ -1247,6 +1265,18 @@
 
 .v-chip {
 	margin: 2px;
+}
+
+.sy-select__label {
+	align-self: center;
+	white-space: nowrap;
+	flex-shrink: 0;
+	font-size: inherit;
+
+	&:not(:last-of-type)::after {
+		content: ',';
+		margin-right: 4px;
+	}
 }
 
 .sy-select :deep(.v-field__input) {
