341bbe2ae8d9f030edc5922fa2f8895e61cf7e67
ValentinoSumaOcto <valentino.suma@octo.com>
2026-04-02 11:11:49 +0200

    Feat/components vuetify VBtn (#2054)
    * add introduction and v-btn
    
    * clean playground
    
    * modify component from V-Btn to VBtn
    
    * fix hover color

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -147,6 +147,7 @@ const preview: Preview = {
 						'Filtres', ['FiltersInline', 'FiltersSideBar', 'FilterModule'],
 						'Données', ['Logo', 'LogoBrandSection', 'CollapsibleList', 'ChipList', 'DataList', 'DataListGroup', 'FilePreview', 'FileList'],
 						'Feedback', ['SyAlert', 'DialogBox', 'NotificationBar', 'CookieBanner', 'RatingPicker'],
+						'Composants Vuetify', ['Introduction', 'VBtn'],
 					],
 					'Templates', ['Vue d\'ensemble', 'StatusPage', 'ErrorPage', 'MaintenancePage', 'NotFoundPage'],
 					'Guide Du Dev', [
