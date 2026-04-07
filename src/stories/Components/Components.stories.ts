b004c57392ba12a260bae1d80db3b9e6599f4019
Sebastian <sebastian.czech@assurance-maladie.fr>
2026-04-07 13:19:02 +0200

    linter

--- src/stories/Components/Components.stories.ts
+++ src/stories/Components/Components.stories.ts
@@ -245,13 +245,13 @@ export const ComponentsList: StoryObj = {
 						img: '/components/select-btn-field.svg',
 						category: 'Formulaires',
 					},
-                    {
-                        title: 'SyRadioGroup',
-                        description: 'Utilisé pour permettre à l\'utilisateur de choisir une option parmi plusieurs..',
-                        link: '/?path=/docs/composants-formulaires-syradiogroup--docs',
-                        img: '/components/range-field.svg',
-                        category: 'Formulaires',
-                    },
+					{
+						title: 'SyRadioGroup',
+						description: 'Utilisé pour permettre à l\'utilisateur de choisir une option parmi plusieurs..',
+						link: '/?path=/docs/composants-formulaires-syradiogroup--docs',
+						img: '/components/range-field.svg',
+						category: 'Formulaires',
+					},
 					{
 						title: 'PaginatedTable',
 						description: 'Utilisé pour afficher une VDataTable de Vuetify avec une pagination persistante.',
@@ -380,7 +380,7 @@ export const ComponentsList: StoryObj = {
 					'SyPagination',
 					'FranceConnectBtn',
 					'SkipLink',
-                    'SyRadioGroup'
+					'SyRadioGroup',
 				]
 
 				const shouldDisplayComponent = (component: { category: string, title: string }, category: string) => {
