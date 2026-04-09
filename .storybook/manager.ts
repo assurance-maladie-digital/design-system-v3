c6072b9bd215d88d042f7bb43c1542cecb2e6f34
ValentinoSumaOcto <valentino.suma@octo.com>
2026-04-09 14:55:03 +0200

    HeaderBar: Ajout de la story appendSlot dans le theme ameliPro (#2074)
    * wip
    
    * add appendSlot story for ameliPro
    
    * hide prepend slot story
    
    * wip
    
    * add appendSlot story for ameliPro
    
    * hide prepend slot story
    
    * hide header tollbar story in ap
    
    * display more child of stories function condition
    
    * remove usage story
    
    ---------
    
    Co-authored-by: Sebastian <sebastian.czech@assurance-maladie.fr>

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -30,7 +30,7 @@ const apOnlyStories = [
 const apComponents = [
 	'composants-vue-d-ensemble--docs',
 	'composants-structure-footerbar',
-	'composants-structure-headerbar',
+    'composants-structure-headerbar',
 	'composants-structure-headerloading',
 	'composants-layout-pagecontainer',
 	'composants-navigation-skiplink',
@@ -43,9 +43,9 @@ const apComponents = [
 	'composants-formulaires-sycheckboxgroup',
 	'composants-formulaires-sycheckbox',
 	'composants-composants-vuetify-vtooltip--docs',
-    'composants-formulaires-selects-syselect',
-    'composants-formulaires-selects-syautocomplete',
-    'composants-formulaires-syradiogroup'
+	'composants-formulaires-selects-syselect',
+	'composants-formulaires-selects-syautocomplete',
+	'composants-formulaires-syradiogroup',
 ]
 // Get stored theme or default to CNAM
 const storedTheme = typeof window !== 'undefined' ? localStorage.getItem('storybook-theme') : 'cnam'
@@ -57,7 +57,11 @@ const isParentOfAllowedComponent = (itemId: string, stories: string[]) =>
 	stories.some(story => story.startsWith(`${itemId}-`))
 
 const isChildOfAllowedComponent = (itemId: string, stories: string[]) =>
-	stories.some(story => itemId.startsWith(`${story}--`))
+    stories.some(
+        story =>
+            itemId.startsWith(`${story}--`) ||
+            itemId.startsWith(`${story}-`)
+    )
 
 const shouldShowApComponent = (item, itemId, theme) => {
 	const isAp2026 = theme === 'ap2026'
@@ -72,6 +76,10 @@ const shouldShowApComponent = (item, itemId, theme) => {
 		'guide-du-dev-guide-technique-système-de-validation-règles--docs',
 		'guide-du-dev-migration',
 		'guide-du-dev-installation--docs',
+		'composants-structure-headerbar--prepend-slot',
+		'composants-structure-headerbar--with-header-toolbar',
+        'composants-structure-headerbar-headernavigationbar',
+        'composants-structure-headerbar--usages'
 	])
 
 	if (itemId === 'composants-amelipro') {
@@ -96,6 +104,7 @@ const shouldShowApComponent = (item, itemId, theme) => {
 			item.style.display = ''
 			return
 		}
+        console.log(itemId, apComponents)
 
 		const shouldShow = isExactMatch(itemId, apComponents)
 			|| isParentOfAllowedComponent(itemId, apComponents)
