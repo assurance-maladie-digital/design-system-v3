e23fe156f996eab23d2ea62f73e75f4198466248
David FYON <davidfyon@gmail.com>
2026-04-14 14:39:12 +0200

    fix types

--- src/designTokens/utils/convertSemanticsToken.ts
+++ src/designTokens/utils/convertSemanticsToken.ts
@@ -14,19 +14,16 @@ type ThemeVariables = {
 export const generateThemeVariables = (tokens: Tokens): ThemeVariables => {
 	const themeVariables: ThemeVariables = {}
 
-	// Parcourir les catégories de couleurs
-	for (const category in tokens.colors) {
-		if (Object.prototype.hasOwnProperty.call(tokens.colors, category)) {
-			const categoryTokens = tokens.colors[category]
-
-			// Parcourir les tokens de chaque catégorie
-			for (const token in categoryTokens) {
-				if (Object.prototype.hasOwnProperty.call(categoryTokens, token)) {
-					const variableName = `on${category.charAt(0).toUpperCase() + category.slice(1)}${token.charAt(0).toUpperCase() + token.slice(1)}`
-					themeVariables[variableName] = categoryTokens[token]!
-				}
+	for (const [category, categoryTokens] of Object.entries(tokens.colors)) {
+		for (const [token, tokenValue] of Object.entries(categoryTokens)) {
+			if (tokenValue === undefined) {
+				throw new Error(`Missing semantic token "${token}" in category "${category}"`)
 			}
+
+			const variableName = `on${category.charAt(0).toUpperCase() + category.slice(1)}${token.charAt(0).toUpperCase() + token.slice(1)}`
+			themeVariables[variableName] = tokenValue
 		}
 	}
+
 	return themeVariables
 }
