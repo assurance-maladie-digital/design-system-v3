4891ee73b14d2df34f843f5a8f358e5db311b96f
David FYON <davidfyon@gmail.com>
2026-04-16 16:04:29 +0200

    delete unused files

--- src/designTokens/utils/index.ts
+++ src/designTokens/utils/index.ts
@@ -1,5 +1,4 @@
 import { createFlattenTheme } from './createFlattenTheme'
-import { convertGaps } from './convertGaps'
 import { createFontVariables } from './createFontVariables'
 
-export { createFlattenTheme, convertGaps, createFontVariables }
+export { createFlattenTheme, createFontVariables }
