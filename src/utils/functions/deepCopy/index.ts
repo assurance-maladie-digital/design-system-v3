d999614ed6fbc018f9b43d81650f425302966c20
adrien <adrien.charrier@assurance-maladie.fr>
2026-06-05 10:52:10 +0200

    test

--- src/utils/functions/deepCopy/index.ts
+++ src/utils/functions/deepCopy/index.ts
@@ -1,17 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { toRaw } from 'vue'
+
 type UnknownValue = any
 
 /** Deep copy an object or an array without reference */
 export function deepCopy<T = any>(o: UnknownValue): T {
+	const value = toRaw(o)
 	let copy = o
 	let k
 
-	if (o && typeof o === 'object') {
-		copy = Object.prototype.toString.call(o) === '[object Array]' ? [] : {}
+	if (value && typeof value === 'object') {
+		copy = Object.prototype.toString.call(value) === '[object Array]' ? [] : {}
 
-		for (k in o) {
-			if (o[k] !== undefined) {
-				copy[k] = deepCopy(o[k])
+		for (k in value) {
+			if (value[k] !== undefined) {
+				copy[k] = deepCopy(value[k])
 			}
 		}
 	}
