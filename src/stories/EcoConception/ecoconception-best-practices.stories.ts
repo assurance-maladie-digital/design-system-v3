aecf8c890299546ff1d8daf033f3c23f69af2d2f
VerushkaRossi <39561014+verushkarossi@users.noreply.github.com>
2026-06-10 09:41:48 +0200

    ECO-CONCEPTION: 20 pratiques essentielles (#2213)
    * page en cours
    
    * page en cours
    
    * page doc ok
    
    * pb lint
    
    * pb lint
    
    * pb lint
    
    * page doc pratiques accessibilite
    
    * page doc pratiques eco-conception
    
    * mis a jour emplacement storybook
    
    * correction css, ajout pop up pr action
    
    * correction lint
    
    * mis a jour page bonne pratique sessentielles
    
    * derniers changements ok
    
    * derniers changements ok
    
    * ajout introduction de midori

---
+++ src/stories/EcoConception/ecoconception-best-practices.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import EcoBestPracticesDoc from './EcoBestPracticesDoc.vue'
+
+export default {
+	title: 'Éco-conception',
+	component: EcoBestPracticesDoc,
+} as Meta
+
+export const BonnesPratiquesEssentielles: StoryObj = {
+	render: () => ({
+		components: {
+			EcoBestPracticesDoc,
+		},
+		template: '<EcoBestPracticesDoc />',
+	}),
+	parameters: {
+		layout: 'fullscreen',
+	},
+	tags: ['!dev'],
+}
