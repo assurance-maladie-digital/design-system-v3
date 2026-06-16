0c9ba957cac14ad3bc6498f56d775b36c0b6e0b4
SebastianCzechDev <sebastian.czech@assurance-maladie.fr>
2026-06-16 15:42:49 +0200

    Décommissionnement du composant `AmeliproNumberedCard` (#2278)
    * Deprecate AmeliproNumberedCard and add example story
    
    * linter

--- src/stories/GuideDuDev/Amelipro.stories.ts
+++ src/stories/GuideDuDev/Amelipro.stories.ts
@@ -522,17 +522,19 @@ export const AmeliproComponents: StoryObj = {
 							url: 'https://p2-design-system-dev.app.ge-4.digital.ramage/composants/amelipro-multiple-folding-card',
 						},
 					},
-					// {
-					// 	title: 'AmeliproNumberedCard',
-					// 	synapse: {
-					// 		name: 'Composant synapse',
-					// 		url: '',
-					// 	},
-					// 	amelipro: {
-					// 		name: 'AmeliproNumberedCard',
-					// 		url: 'https://p2-design-system-dev.app.ge-4.digital.ramage/composants/amelipro-numbered-card',
-					// 	},
-					// },
+					{
+						title: 'AmeliproNumberedCard',
+						usedComponents: [
+							{
+								name: 'VCard',
+								type: 'composants vuetify',
+							},
+						],
+						amelipro: {
+							name: 'AmeliproNumberedCard',
+							url: 'https://p2-design-system-dev.app.ge-4.digital.ramage/composants/amelipro-numbered-card',
+						},
+					},
 					{
 						title: 'AmeliproOnboarding',
 						synapse: {
