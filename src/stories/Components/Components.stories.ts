f39cbac55e5d0287dd53f2ad7c21961a6e0e449f
Sebastian <sebastian.czech@assurance-maladie.fr>
2026-06-29 16:22:34 +0200

    add thumbnail

--- src/stories/Components/Components.stories.ts
+++ src/stories/Components/Components.stories.ts
@@ -270,7 +270,7 @@ export const ComponentsList: StoryObj = {
 						title: 'SyBtnMenu',
 						description: 'Utilisé pour proposer une sélection d\'options avec un bouton personnalisé, conforme au style du Design System et qui respecte les règles d\'accessibilité RGAA. Il est basé sur un v-btn.',
 						link: '/?path=/docs/composants-navigation-sybtnMenu--docs',
-						img: `/components/sy-btn-select${isAp ? '-ap' : ''}.svg`,
+						img: `/components/sy-btn-menu${isAp ? '-ap' : ''}.svg`,
 						category: 'Formulaires',
 					},
 					{
