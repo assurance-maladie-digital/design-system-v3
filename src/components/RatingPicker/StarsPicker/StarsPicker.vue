1b378bc33c68e77730b8603f00d803b40b0b6303
SebastianCzechDev <sebastian.czech@assurance-maladie.fr>
2026-07-06 16:11:10 +0200

    Feat/migration rating picker (#2352)
    * migration rating picker
    
    * migration rating picker
    
    * update snapshot
    
    * fix star focus color
    
    * linter

--- src/components/RatingPicker/StarsPicker/StarsPicker.vue
+++ src/components/RatingPicker/StarsPicker/StarsPicker.vue
@@ -126,7 +126,7 @@
 					:class="
 						isFilled(index)
 							? 'text-primary'
-							: 'text-blue-lighten'
+							: 'text-lighten'
 					"
 					size="36px"
 					class="py-0 px-2"
@@ -154,12 +154,18 @@
 		color: rgb(var(--v-theme-primary)) !important;
 	}
 
-	&.text-blue-lighten {
-		color: rgb(var(--v-theme-blue-lighten60)) !important;
+	&.text-lighten {
+		color: rgb(var(--v-theme-primary), 0.4) !important;
 	}
 
-	&--disabled.text-blue-lighten {
-		color: rgb(var(--v-theme-blue-lighten60)) !important;
+	&--disabled.text-lighten {
+		color: rgb(var(--v-theme-primary), 0.4) !important;
 	}
+}
+
+.sy-stars-picker__item:focus-visible {
+	outline: none;
+	box-shadow: inset 0 0 0 2px rgb(var(--v-theme-primary));
+	border-radius: var(--radius-md);
 }
 </style>
