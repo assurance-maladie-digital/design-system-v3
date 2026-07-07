4d4bee7db90d75e5fabec97b8ed43991cf3cfaef
David FYON <davidfyon@gmail.com>
2026-07-07 15:23:08 +0200

    FilePreview: add readonly + mandatory + pdf.js (#2344)
    * add mandatory option
    
    * lint
    
    * add readonly prop
    
    * add tests
    
    * improve test
    
    * revokeFileURL
    
    * fix conflicts
    
    * renforce readonly
    
    * improve render pdf
    
    * improve loaded doc
    
    * fix build
    
    * lint
    
    * add comment
    
    * fix fileURL
    
    * add a11y doc

--- src/components/FilePreview/locales.ts
+++ src/components/FilePreview/locales.ts
@@ -1,4 +1,6 @@
 export const locales = {
 	previewNotAvailable: 'Impossible de prévisualiser le fichier.',
 	previewTypeNotAvailable: 'Impossible de prévisualiser ce type de fichier.',
+	loadingDocument: 'Chargement du document…',
+	documentError: 'Le document n\'a pas pu être affiché.',
 }
