7c3d57e4f679870e915dfd8514877c9854af2c5f
Valentino Suma <valentino.suma@octo.com>
2026-07-27 11:32:45 +0200

    fix month picker

--- src/components/MonthPicker/MonthPickerVisual/VisualPickerFooter.vue
+++ src/components/MonthPicker/MonthPickerVisual/VisualPickerFooter.vue
@@ -18,43 +18,27 @@
 
 <template>
 	<div class="month-picker-footer">
-		<button
-			class="month-picker-footer__current-month-btn"
-			type="button"
+		<VBtn
+			color="primary"
+			variant="text"
 			:aria-label="btnLabel"
 			:title="btnLabel"
 			@click="selectCurrentMonth"
 		>
 			<SyIcon
+				class="mr-2"
 				:icon="mdiCalendarMonthOutline"
 				decorative
 			/>
 			{{ btnText }}
-		</button>
+		</VBtn>
 	</div>
 </template>
 
 <style scoped lang="scss">
 .month-picker-footer {
-	padding: 0 12px 12px;
-}
-
-.month-picker-footer__current-month-btn {
 	display: flex;
-	gap: 8px;
-	margin: auto;
-	margin-block: 8px;
-	padding: 8px;
-	text-align: center;
-	font-weight: bold;
-	font-size: var(--v-typography-body2-font-size, 1rem);
-	border-radius: 99px;
-	color: rgb(var(--v-theme-primary, 12, 65, 154));
-	margin-bottom: 2px;
-
-	&:focus-visible {
-		/* stylelint-disable-next-line custom-property-pattern */
-		outline: 2px solid rgb(var(--v-theme-primary, 12, 65, 154));
-	}
+	justify-content: center;
+	padding: 12px;
 }
 </style>
