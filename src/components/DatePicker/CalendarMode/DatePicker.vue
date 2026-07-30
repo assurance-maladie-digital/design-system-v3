ad3b035f6b06ac1b85f725c41f550b86ad22f19f
Valentino Suma <valentino.suma@octo.com>
2026-07-30 12:08:53 +0200

    add variant outlined

--- src/components/DatePicker/CalendarMode/DatePicker.vue
+++ src/components/DatePicker/CalendarMode/DatePicker.vue
@@ -1149,6 +1149,7 @@
 									type="button"
 									size="x-small"
 									color="primary"
+									variant="outlined"
 									:title="todayButtonLabel"
 									:aria-label="todayButtonLabel"
 									class="date-picker__today-button my-2 pa-2 mt-2"
