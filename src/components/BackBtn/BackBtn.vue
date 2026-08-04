24fe54829b2139542f72413f7dbc19d46ff30c94
Sebastian <sebastian.czech@assurance-maladie.fr>
2026-08-04 13:33:26 +0200

    update interaction on backBtn

--- src/components/BackBtn/BackBtn.vue
+++ src/components/BackBtn/BackBtn.vue
@@ -1,54 +1,33 @@
 <script lang="ts" setup>
 	import { mdiArrowLeft } from '@mdi/js'
-	import { computed } from 'vue'
-	import { locales as defaultLocales } from './locales'
+	import { locales } from './locales'
 	import SyIcon from '@/components/Customs/SyIcon/SyIcon.vue'
-	import { useLocales } from '@/composables/useLocales'
-	import type { DeepPartial } from '@/utils/locales/mergeLocales'
+	import { computed } from 'vue'
 
-	const props = withDefaults(defineProps<{
+	const props = (defineProps<{
 		hideBackIcon?: boolean
 		dark?: boolean
-		color?: string
-		backgroundColor?: string
-		locales?: DeepPartial<typeof defaultLocales>
-	}>(), {
-		backgroundColor: 'white',
-		locales: () => ({}),
-		color: undefined,
-	})
+	}>())
 
-	const locales = useLocales(defaultLocales, () => props.locales)
-
-	const isDark = computed(() => props.dark ?? false)
-	const color = computed(() => props.color || (isDark.value ? 'white' : 'primary'))
-	const buttonVariant = computed(() => isDark.value ? 'outlined' : 'text')
-	const buttonTheme = computed(() => isDark.value ? 'dark' : undefined)
-	const buttonBgColor = computed(() => isDark.value ? 'transparent' : props.backgroundColor)
-
-	const buttonClasses = computed(() => ({
-		'px-0': !isDark.value,
-		'pr-1': !isDark.value && !props.hideBackIcon,
-	}))
+	const buttonTheme = computed(() => props.dark ? 'dark' : undefined)
 
 </script>
 
 <template>
 	<VBtn
 		v-bind="$attrs"
-		:variant="buttonVariant"
+		variant="outlined"
 		:theme="buttonTheme"
-		:color="color"
-		:class="['sy-back-btn', 'text-none', buttonClasses]"
-		:style="{ backgroundColor: buttonBgColor }"
+		:class="[
+			'sy-back-btn',
+			'text-none'
+		]"
 	>
 		<slot name="icon">
 			<SyIcon
 				v-if="!props.hideBackIcon"
 				:icon="mdiArrowLeft"
 				decorative
-				:color="color"
-				:class="{ 'ml-n1': isDark }"
 				class="mr-1"
 			/>
 		</slot>
@@ -60,11 +39,13 @@
 </template>
 
 <style lang="scss" scoped>
-// Désactiver l'état de hover sur le thème clair
-.v-btn:deep() {
-	.v-btn__underlay,
-	.v-btn__overlay {
-		display: none;
+.sy-back-btn {
+	border-color: rgb(var(--v-theme-primary));
+	color: rgb(var(--v-theme-primary));
+
+	&.v-theme--dark {
+		border-color: rgb(var(--v-theme-onPrimary));
+		color: rgb(var(--v-theme-onPrimary));
 	}
 }
 </style>
