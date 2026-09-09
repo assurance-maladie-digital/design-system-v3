8860351462d5de9406584424dd0a52125d8f30cf
VachetVirginie <vachetvirginie@gmail.com>
2026-09-09 16:53:53 +0200

    test(SyTextArea): add comment explaining flushPromises usage for blur validation watcher in useCustomValidation

--- src/components/SyTextArea/tests/SyTextArea.spec.ts
+++ src/components/SyTextArea/tests/SyTextArea.spec.ts
@@ -177,6 +177,12 @@ describe('SyTextArea', () => {
 		await textarea.trigger('focus')
 		await textarea.setValue('content\ncontent\ncontent\ncontent\ncontent\ncontent')
 		await textarea.trigger('blur')
+		// Ajout pour useCustomValidation.ts:195-199 : watch(focused, (newVal) => {
+		//     if (isValidateOnBlur.value && !newVal && !disableErrorHandling.value) {
+		//         validate()
+		//     }
+		// })
+		// Pour eviter de demander à chaque composant de connaître et d'appeler validate() explicitement.
 		await flushPromises()
 
 		expect(wrapper.text()).toContain('Ce champ ne peut pas dépasser 5 lignes')
